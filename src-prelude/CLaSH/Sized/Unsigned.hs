2ceb35ff5e9c74ce038d32bb0f7b7d415776a1d9
Christiaan Baaij <christiaan.baaij@gmail.com>
2012-10-03 18:04:59 +0200

    Start on Num type-class

--- src-prelude/CLaSH/Sized/Unsigned.hs
+++ src-prelude/CLaSH/Sized/Unsigned.hs
@@ -1,9 +1,43 @@
 {-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE ExplicitForAll        #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
 module CLaSH.Sized.Unsigned where
 
+import CLaSH.Class.Num
+import CLaSH.Promoted.Ord
+
+import qualified Prelude
+import Prelude (Integer,undefined,Show,(.))
 import GHC.TypeLits
 
-newtype Unsigned (n :: Nat) = U Integer
+newtype Unsigned (n :: Nat) = U Integer deriving Show
+
+instance Add (Unsigned n) where
+  type AResult (Unsigned n) = Unsigned (n + 1)
+  (U a) + (U b) = U (a Prelude.+ b)
+  (U a) - (U b) = U (a Prelude.- b)
+
+instance Mult (Unsigned n) where
+  type MResult (Unsigned n) = Unsigned (n + n)
+  (U a) * (U b) = U (a Prelude.* b)
+
+resize :: Unsigned n -> Unsigned m
+resize (U n) = (U n)
+
+dot = (.).(.)
+
+u1 :: Unsigned 4
+u1 = (U 2)
+
+u2 :: Unsigned 4
+u2 = (U 4)
+
+u3 :: Unsigned 5
+u3 = u1 + u2
