3e35fb51c9155914f34c10548cdaf2d6d0af42a4
Christiaan Baaij <christiaan.baaij@gmail.com>
2013-04-24 13:22:21 +0200

    Add support for (+>>)

--- examples/FIR.hs
+++ examples/FIR.hs
@@ -5,7 +5,10 @@ import CLaSH.Prelude
 
 dotp as bs = vfoldl (+) 0 (vzipWith (*) as bs)
 
+fir hs pxs x = (x +>> pxs, dotp pxs hs)
+
 topEntity :: Vec 4 (Signed 16)
-          -> Vec 4 (Signed 16)
           -> Signed 16
-topEntity as bs = dotp as bs
+          -> (Vec 4 (Signed 16),Signed 16)
+topEntity pxs x = fir (2 :> 3 :> (-2) :> 8 :> Nil) pxs x
+
