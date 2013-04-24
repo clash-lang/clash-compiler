f4f52f9096ec12b52a6d935e57e554226c82e40d
Christiaan Baaij <christiaan.baaij@gmail.com>
2013-04-24 12:04:14 +0200

    Add code to run GHC simplifier, but don't actually run it

--- src/CLaSH/GHC/Compat/DynFlags.hs
+++ src/CLaSH/GHC/Compat/DynFlags.hs
@@ -22,3 +22,17 @@ dopt_unset = DynFlags.gopt_unset
 #else
 dopt_unset = DynFlags.dopt_unset
 #endif
+
+dopt_set ::
+  DynFlags
+#if __GLASGOW_HASKELL__ >= 707
+  -> GeneralFlag
+#else
+  -> DynFlag
+#endif
+  -> DynFlags
+#if __GLASGOW_HASKELL__ >= 707
+dopt_set = DynFlags.gopt_set
+#else
+dopt_set = DynFlags.dopt_set
+#endif
--- src/CLaSH/GHC/LoadModules.hs
+++ src/CLaSH/GHC/LoadModules.hs
@@ -11,10 +11,12 @@ import qualified GHC.Paths
 -- GHC API
 -- import qualified CorePrep
 import qualified CoreSyn
+import           DynFlags (GeneralFlag(..))
 import qualified DynFlags
-import           CLaSH.GHC.Compat.DynFlags (dopt_unset)
+import           CLaSH.GHC.Compat.DynFlags (dopt_unset,dopt_set)
 import qualified GHC
 import           CLaSH.GHC.Compat.GHC (defaultErrorHandler)
+-- import qualified HscMain
 import qualified HscTypes
 import qualified HsImpExp
 import qualified MonadUtils
@@ -41,11 +43,14 @@ loadModules ::
 loadModules modName = defaultErrorHandler $
   GHC.runGhc (Just GHC.Paths.libdir) $ do
     dflags <- GHC.getSessionDynFlags
-    let dflags' = foldl DynFlags.xopt_set
+    let dflags1 = foldl DynFlags.xopt_set
                     (dflags
-                      {GHC.simplPhases = 0, DynFlags.ctxtStkDepth = 1000})
+                      { DynFlags.ctxtStkDepth = 1000
+                      , DynFlags.optLevel = 2
+                      } )
                     [DynFlags.Opt_TemplateHaskell,DynFlags.Opt_Arrows]
-    _ <- GHC.setSessionDynFlags dflags'
+    let dflags2 = wantedOptimizationFlags dflags1
+    _ <- GHC.setSessionDynFlags dflags2
     target <- GHC.guessTarget modName Nothing
     GHC.setTargets [target]
     ldRes <- GHC.load GHC.LoadAllTargets
@@ -71,20 +76,18 @@ loadModules modName = defaultErrorHandler $
                                      ; tcMod <- GHC.typecheckModule pMod
                                      ; dsMod <- fmap GHC.coreModule $ GHC.desugarModule tcMod
                                      ; hsc_env <- GHC.getSession
+                                     -- ; simpl_guts <- MonadUtils.liftIO $ HscMain.hscSimplify hsc_env dsMod
                                      ; (tidy_guts,_) <- MonadUtils.liftIO $ TidyPgm.tidyProgram hsc_env dsMod
-                                     -- ; dflags'' <- GHC.getSessionDynFlags
                                      ; let tycons     = HscTypes.cg_tycons tidy_guts
-                                     -- ; let dataTyCons = filter TyCon.isDataTyCon tycons
                                      ; let pgm        = HscTypes.cg_binds tidy_guts
+                                     -- ; let dataTyCons = filter TyCon.isDataTyCon tycons
+                                     -- ; dflags'' <- GHC.getSessionDynFlags
                                      -- ; prepBinders <- MonadUtils.liftIO $ CorePrep.corePrepPgm dflags'' hsc_env pgm dataTyCons
                                      ; return (CoreSyn.flattenBinds pgm,tycons)
                                      }
                              ) modGraph'
 
         let (binders,tyCons) = (concat >< concat) (unzip tidiedMods)
-
-        -- let (binders,tyCons) = flattenModules
-        --               $ map flattenDesugaredModule desugardMods
 
         (externalBndrs,dfuns,clsOps,unlocatable) <- loadExternalExprs
                                                 (map snd binders)
@@ -105,6 +108,44 @@ disableOptimizationsFlags :: GHC.ModSummary -> GHC.ModSummary
 disableOptimizationsFlags ms@(GHC.ModSummary {..})
   = ms {GHC.ms_hspp_opts = dflags}
   where
-    dflags = dopt_unset (ms_hspp_opts
-              {DynFlags.optLevel = 0, DynFlags.ctxtStkDepth = 1000})
-              DynFlags.Opt_EnableRewriteRules
+    dflags = wantedOptimizationFlags (ms_hspp_opts
+              {DynFlags.optLevel = 2, DynFlags.ctxtStkDepth = 1000})
+
+wantedOptimizationFlags :: GHC.DynFlags -> GHC.DynFlags
+wantedOptimizationFlags df = foldl dopt_unset (foldl dopt_set df wanted) unwanted
+  where
+    wanted = [ Opt_Strictness -- [Wanted?] don't care about strictness
+             , Opt_CSE -- CSE
+             , Opt_FullLaziness -- Floats let-bindings outside enclosing lambdas
+             , Opt_Specialise -- Specialise on types, specialise type-class-overloaded function defined in this module for the types
+             , Opt_DoLambdaEtaExpansion -- We need eta-expansion anyway, so the more GHC does, the better
+             , Opt_CaseMerge -- We want fewer case-statements
+             , Opt_DictsCheap -- Makes dictionaries seem cheap to optimizer: hopefully inline
+             , Opt_IrrefutableTuples -- User might enable, doesn't affect normalization, don't disable
+             , Opt_SimpleListLiterals -- Avoids 'build' rule
+             , Opt_ExposeAllUnfoldings -- We need all the unfoldings we can get
+             , Opt_ForceRecomp -- Force recompilation: never bad
+             ]
+
+    unwanted = [ Opt_FloatIn -- Moves let-bindings inwards: defeats the normal-form with a single top-level let-binding
+               , Opt_StaticArgumentTransformation -- [Wanted?] Turn on the static argument transformation, which turns a recursive function into a non-recursive one with a local recursive loop.
+               , Opt_LiberateCase -- Perform unrolling of recursive RHS: avoid
+               , Opt_SpecConstr -- Creates local-functions: avoid
+               , Opt_IgnoreAsserts -- We don't care about assertions
+               , Opt_DoEtaReduction -- We want eta-expansion
+               , Opt_UnboxStrictFields -- Unboxed types are not handled properly: avoid
+               , Opt_UnboxSmallStrictFields -- Unboxed types are not handled properly: avoid
+               , Opt_EnableRewriteRules -- Intermediate data-structures take up no space
+               , Opt_Vectorise -- Don't care
+               , Opt_VectorisationAvoidance -- Don't care
+               , Opt_RegsGraph -- Don't care
+               , Opt_RegsGraph -- Don't care
+               , Opt_PedanticBottoms -- Stops eta-expansion through case: avoid
+               , Opt_LlvmTBAA -- Don't care
+               , Opt_CmmSink -- Don't care
+               , Opt_CmmElimCommonBlocks -- Don't care
+               , Opt_OmitYields -- Don't care
+               , Opt_IgnoreInterfacePragmas -- We need all the unfoldings we can get
+               , Opt_OmitInterfacePragmas -- We need all the unfoldings we can get
+               , Opt_SimplPreInlining -- Does inlining, which destroys function hierarchy: avoid
+               ]
