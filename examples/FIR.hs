fc4a98812e96c5f2b1e63e16c822537718723e4e
Christiaan Baaij <christiaan.baaij@gmail.com>
2013-05-06 10:00:56 +0200

    Cleanup FIR filter

--- examples/FIR.hs
+++ examples/FIR.hs
@@ -5,13 +5,10 @@ import CLaSH.Prelude
 
 dotp as bs = vfoldl (+) 0 (vzipWith (*) as bs)
 
-fir :: Vec 4 (Sync (Signed 16))
-          -> Sync (Signed 16)
-          -> Sync (Signed 16)
-fir hs x = y
+fir coeffs x_t = y_t
   where
-    y  = dotp hs xs
-    xs = rememberN x
+    y_t = dotp coeffs xs
+    xs  = window x_t
 
 topEntity :: Sync (Signed 16) -> Sync (Signed 16)
 topEntity = fir (0 :> 1 :> 2 :> 3 :> Nil)
