56dc77e3f2e301fe62d2d147cf80f383934879e3
Christiaan Baaij <christiaan.baaij@gmail.com>
2013-06-06 10:05:39 +0200

    wobble examples

--- examples/Calculator.hs
+++ examples/Calculator.hs
@@ -20,12 +20,12 @@ pu alu (op1,op2,cnt) (dmem,Pop)  = ((dmem,op1,cnt-1),(cnt,Nothing))
 pu alu (op1,op2,cnt) (dmem,Push) = ((op1,op2,cnt+1) ,(cnt,Nothing))
 pu alu (op1,op2,cnt) (dmem,opc)  = ((op1,op2,cnt)   ,(cnt,alu opc op1 op2))
 
-datamem mem (addr,Nothing)  = (mem                  ,vindex mem addr)
-datamem mem (addr,Just val) = (vreplace mem addr val,vindex mem addr)
+datamem mem (addr,Nothing)  = (mem                  ,mem ! addr)
+datamem mem (addr,Just val) = (vreplace mem addr val,mem ! addr)
 
 topEntity :: Sync (OPC Word) -> Sync (Maybe Word)
 topEntity i = val
   where
     (addr,val) = (pu alu <^> (0,0,0 :: Unsigned 3)) (mem,i)
     mem        = (datamem <^> initMem) (addr,val)
-    initMem    = vcopyE (sing :: Sing 8) 0
+    initMem    = vcopy (sing :: Sing 8) 0
