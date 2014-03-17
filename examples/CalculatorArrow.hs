794df7b67771c7e5b3a127ac5410d8c356eb1d14
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-03-17 09:08:33 +0100

    Add type signatures to examples for documentation purposes

--- examples/CalculatorArrow.hs
+++ examples/CalculatorArrow.hs
@@ -10,15 +10,27 @@ data OPC a = ADD | MUL | Imm a | Pop | Push
 
 infixr 9 .:
 
+alu :: Num a => OPC a -> a -> a -> Maybe a
 alu ADD     = Just .: (+)
 alu MUL     = Just .: (*)
 alu (Imm i) = const . const (Just i)
 alu _       = const . const Nothing
 
+pu :: (Num a, Num b)
+   => (OPC a -> a -> a -> Maybe a)
+   -> (a, a, b)       -- Current state
+   -> (a, OPC a)      -- Input
+   -> ( (a, a, b)     -- New state
+      , (b, Maybe a)  -- Output
+      )
 pu alu (op1,op2,cnt) (dmem,Pop)  = ((dmem,op1,cnt-1),(cnt,Nothing))
 pu alu (op1,op2,cnt) (dmem,Push) = ((op1,op2,cnt+1) ,(cnt,Nothing))
 pu alu (op1,op2,cnt) (dmem,opc)  = ((op1,op2,cnt)   ,(cnt,alu opc op1 op2))
 
+datamem :: (KnownNat n, Integral i)
+        => Vec n a       -- Current state
+        -> (i, Maybe a)  -- Input
+        -> (Vec n a, a)  -- (New state, Output)
 datamem mem (addr,Nothing)  = (mem                  ,mem ! addr)
 datamem mem (addr,Just val) = (vreplace mem addr val,mem ! addr)
 
--- examples/FIR.hs
+++ examples/FIR.hs
@@ -2,8 +2,14 @@ module FIR where
 
 import CLaSH.Prelude
 
+dotp :: Num a
+     => Vec n a
+     -> Vec n a
+     -> a
 dotp as bs = vfoldl (+) 0 (vzipWith (*) as bs)
 
+fir :: (Default a, KnownNat (n + 1), Num a)
+    => Vec ((n + 1) + 1) (Signal a) -> Signal a -> Signal a
 fir coeffs x_t = y_t
   where
     y_t = dotp coeffs xs
