39aa55fcce14d8ee4ac8cc726611915dbf55514c
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-03-30 10:45:48 +0200

    adapt test to new prelude

--- tests/shouldwork/Basic/NestedPrimitives.hs
+++ tests/shouldwork/Basic/NestedPrimitives.hs
@@ -3,7 +3,7 @@ module NestedPrimitives where
 import CLaSH.Prelude
 
 topEntity :: Vec 3 (Signed 16)
-topEntity = vmap resizeS o
+topEntity = vmap resize o
   where
     o :: Vec 3 (Signed 32)
     o = vcopy d3 (bit 31)
