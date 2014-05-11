70d2c13e5971becdec2df5302fdda1b871ce6c7a
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-05-11 17:34:56 +0200

    Add showPprDebug to GHC.Compat.Outputable

--- src-ghc/CLaSH/GHC/Compat/Outputable.hs
+++ src-ghc/CLaSH/GHC/Compat/Outputable.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE CPP #-}
 module CLaSH.GHC.Compat.Outputable
-  ( showPpr, showSDoc )
+  ( showPpr, showSDoc, showPprDebug )
 where
 
 #if __GLASGOW_HASKELL__ >= 707
@@ -8,12 +8,15 @@ import qualified DynFlags   (unsafeGlobalDynFlags)
 #elif __GLASGOW_HASKELL__ >= 706
 import qualified DynFlags   (tracingDynFlags)
 #endif
-import qualified Outputable (Outputable, SDoc, showPpr, showSDoc)
+import qualified Outputable (Outputable (..), SDoc, showPpr, showSDoc, showSDocDebug)
 
 showSDoc :: Outputable.SDoc -> String
 #if __GLASGOW_HASKELL__ >= 707
 showSDoc = Outputable.showSDoc DynFlags.unsafeGlobalDynFlags
 #endif
+
+showPprDebug :: Outputable.Outputable a => a -> String
+showPprDebug = Outputable.showSDocDebug DynFlags.unsafeGlobalDynFlags . Outputable.ppr
 
 showPpr :: (Outputable.Outputable a) => a -> String
 #if __GLASGOW_HASKELL__ >= 707
