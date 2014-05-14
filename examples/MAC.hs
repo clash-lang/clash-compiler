9ee09688fe902db263b651d379d69765fe6e6e90
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-05-14 16:56:36 +0200

    Change MAC example to the example in the tutorial

--- examples/MAC.hs
+++ examples/MAC.hs
@@ -2,7 +2,20 @@ module MAC where
 
 import CLaSH.Prelude
 
-topEntity :: Signal (Signed 16) -> Signal (Signed 16) -> Signal (Signed 16)
-topEntity x y = acc
+ma acc (x,y) = acc + x * y
+
+macT acc (x,y) = (acc',o)
   where
-    acc = register 3 (x*y + acc)
+    acc' = ma acc (x,y)
+    o    = acc
+
+mac = macT <^> 0
+
+topEntity :: (Signal (Signed 9),Signal (Signed 9)) -> Signal (Signed 9)
+topEntity = mac
+
+testInput :: Signal (Signed 9,Signed 9)
+testInput = stimuliGenerator $(v [(1,1) :: (Signed 9,Signed 9),(2,2),(3,3),(4,4)])
+
+expectedOutput :: Signal (Signed 9) -> Signal Bool
+expectedOutput = outputVerifier $(v [0 :: Signed 9,1,5,14])
