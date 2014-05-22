0ef05f71b05fc5db275b731c42c749baf18aa645
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-05-22 18:22:26 +0200

    Actually use type synonym in test for #17

--- tests/shouldwork/FixedNumber/FixedS.hs
+++ tests/shouldwork/FixedNumber/FixedS.hs
@@ -4,7 +4,7 @@ import CLaSH.Prelude
 
 type SF = SFixed 16 20
 
-co = $$(fLit 4.578) :: SFixed 16 20
+co = $$(fLit 4.578) :: SF
 
-topEntity :: SFixed 16 20 -> SFixed 16 20
+topEntity :: SF -> SF
 topEntity x = x + co
