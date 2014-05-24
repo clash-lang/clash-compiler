99698bcef41087966f2c8523c85fb31ae2403a3b
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-05-24 10:42:12 +0200

    Adapt 'FixedS.hs' test so that it also tests #19

--- tests/shouldwork/FixedNumber/FixedS.hs
+++ tests/shouldwork/FixedNumber/FixedS.hs
@@ -2,9 +2,15 @@ module FixedS where
 
 import CLaSH.Prelude
 
-type SF = SFixed 16 20
+type SF = SFixed 4 20
 
 co = $$(fLit 4.578) :: SF
 
 topEntity :: SF -> SF
 topEntity x = x + co
+
+testInput :: Signal SF
+testInput = stimuliGenerator $ $(v ([$$(fLit 1.0), $$(fLit 2.0), $$(fLit 4.0)] :: [SFixed 4 20] ))
+
+expectedOutput :: Signal SF -> Signal Bool
+expectedOutput = outputVerifier $ $(v ([$$(fLit 5.578), $$(fLit 6.578), $$(fLit 8.578)] :: [SFixed 4 20]))
