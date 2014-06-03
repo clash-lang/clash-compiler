ebc40a6ac6e34caf94b268e3427b604f6146926c
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-06-03 10:38:30 +0200

    Add second test for #18

---
+++ tests/shouldwork/Vector/VecOfSum.hs
@@ -0,0 +1,10 @@
+module VecOfSum where
+
+import CLaSH.Prelude
+
+data Sum = Empty
+         | Half Integer Integer Integer Integer
+         | Full (Unsigned 4) (Signed 9) Bool Integer Bit (Maybe (Bool,Signed 3)) (Unsigned 3, Signed 4)
+
+topEntity :: Vec 3 Sum -> Vec 5 Sum
+topEntity xs = (Full 4 12 True 12 H (Just (False, 1)) (2,7)) :> Empty :> xs
