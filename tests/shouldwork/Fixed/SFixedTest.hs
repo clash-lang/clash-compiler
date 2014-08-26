76caaa8245dd673e7ce65b66525de42b0020c1e3
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-08-26 18:04:18 +0200

    generate correct VHDL for minBound and maxBound

--- tests/shouldwork/Fixed/SFixedTest.hs
+++ tests/shouldwork/Fixed/SFixedTest.hs
@@ -2,7 +2,13 @@ module SFixedTest where
 
 import CLaSH.Prelude
 
-type SF = SFixed 20 18
+type SF = SFixed 4 18
 
 topEntity :: SF -> SF
-topEntity x = x * 3.56
+topEntity x = x * 2.56
+
+testInput :: Signal SF
+testInput = stimuliGenerator $ $(v ([1.2, 1.8, 3.5] :: [SFixed 4 18] ))
+
+expectedOutput :: Signal SF -> Signal Bool
+expectedOutput = outputVerifier $ $(v ([3.07199, 4.607991, 8.96] :: [SFixed 4 18]))
