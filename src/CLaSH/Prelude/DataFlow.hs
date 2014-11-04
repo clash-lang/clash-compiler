4917315dffe5af3fa3cf227b763aad6f433c4acb
Christiaan Baaij <christiaan.baaij@gmail.com>
2014-11-04 10:17:19 +0100

    Add Arrow instances to DataFlow type

--- src/CLaSH/Prelude/DataFlow.hs
+++ src/CLaSH/Prelude/DataFlow.hs
@@ -1,8 +1,12 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleInstances #-}
 module CLaSH.Prelude.DataFlow where
 
-import Control.Applicative (pure)
-import GHC.TypeLits        (KnownNat, KnownSymbol)
+import Control.Applicative   (Applicative (..), (<$>))
+import Control.Arrow         (Arrow (..), ArrowChoice (..), ArrowLoop (..))
+import Control.Category      (Category (..))
+import GHC.TypeLits          (KnownNat, KnownSymbol)
+import Prelude               hiding (id, (.))
 
 import CLaSH.Signal          ((.&&.))
 import CLaSH.Signal.Bundle   (Bundle (..))
@@ -28,81 +32,80 @@ newtype DataFlow clk iEn oEn i o
 type DataFlow' iEn oEn i o = DataFlow SystemClock iEn oEn i o
 
 -- | Sequential composition of two 'DataFlow' circuits.
-(>>>) :: DataFlow clk aEn bEn a b
+seqDF :: DataFlow clk aEn bEn a b
       -> DataFlow clk bEn cEn b c
       -> DataFlow clk aEn cEn a c
-(DF f) >>> (DF g) = DF (\a cEn -> let (b,aEn) = f a bEn
-                                      (c,bEn) = g b cEn
-                                  in  (c,aEn))
+(DF f) `seqDF` (DF g) = DF (\a cEn -> let (b,aEn) = f a bEn
+                                          (c,bEn) = g b cEn
+                                      in  (c,aEn))
 
 -- | Apply the circuit to the first halve of the communication channels, leave
 -- the second halve unchanged.
-first :: (KnownSymbol nm, KnownNat rate)
+firstDF :: (KnownSymbol nm, KnownNat rate)
       => DataFlow (Clk nm rate) aEn bEn a b
       -> DataFlow (Clk nm rate) (aEn,cEn) (bEn,cEn) (a,c) (b,c)
-first (DF f) = DF (\ac en -> let clk       = sclock
-                                 (acD,acV) = unbundle clk ac
-                                 (a,c)     = unbundle clk acD
-                                 (aV,cV)   = unbundle clk acV
-                                 (bRd,cRd) = unbundle clk en
-                                 (b,aRd)   = f (bundle clk (a,aV)) bRd
-                                 (bD,bV)   = unbundle clk b
-                                 bcD       = bundle clk (bD,c)
-                                 bcV       = bundle clk (bV,cV)
-                                 dt        = bundle clk (bcD,bcV)
-                                 rd        = bundle clk (aRd,cRd)
-                             in  (dt,rd)
-
-                  )
+firstDF (DF f) = DF (\ac en -> let clk       = sclock
+                                   (acD,acV) = unbundle clk ac
+                                   (a,c)     = unbundle clk acD
+                                   (aV,cV)   = unbundle clk acV
+                                   (bRd,cRd) = unbundle clk en
+                                   (b,aRd)   = f (bundle clk (a,aV)) bRd
+                                   (bD,bV)   = unbundle clk b
+                                   bcD       = bundle clk (bD,c)
+                                   bcV       = bundle clk (bV,cV)
+                                   dt        = bundle clk (bcD,bcV)
+                                   rd        = bundle clk (aRd,cRd)
+                               in  (dt,rd)
+                    )
 
 -- | Swap the two communication channels.
-swap :: (KnownSymbol nm, KnownNat rate)
-     => DataFlow (Clk nm rate) (aEn,bEn) (bEn,aEn) (a,b) (b,a)
-swap = DF (\ab en -> let clk       = sclock
-                         (abD,abV) = unbundle clk ab
-                         (a,b)     = unbundle clk abD
-                         (aV,bV)   = unbundle clk abV
-                         (bEn,aEn) = unbundle clk en
-                         baD       = bundle clk (b,a)
-                         baV       = bundle clk (bV,aV)
-                         ba        = bundle clk (baD,baV)
-                         en'       = bundle clk (aEn,bEn)
-                     in  (ba,en')
-          )
+swapDF :: (KnownSymbol nm, KnownNat rate)
+       => DataFlow (Clk nm rate) (aEn,bEn) (bEn,aEn) (a,b) (b,a)
+swapDF = DF (\ab en -> let clk       = sclock
+                           (abD,abV) = unbundle clk ab
+                           (a,b)     = unbundle clk abD
+                           (aV,bV)   = unbundle clk abV
+                           (bEn,aEn) = unbundle clk en
+                           baD       = bundle clk (b,a)
+                           baV       = bundle clk (bV,aV)
+                           ba        = bundle clk (baD,baV)
+                           en'       = bundle clk (aEn,bEn)
+                       in  (ba,en')
+            )
 
 -- | Apply the circuit to the second halve of the communication channels, leave
 -- the first halve unchanged.
-second :: (KnownSymbol nm, KnownNat rate)
-       => DataFlow (Clk nm rate) aEn bEn a b
-       -> DataFlow (Clk nm rate) (cEn,aEn) (cEn,bEn) (c,a) (c,b)
-second f = swap >>> first f >>> swap
+secondDF :: (KnownSymbol nm, KnownNat rate)
+         => DataFlow (Clk nm rate) aEn bEn a b
+         -> DataFlow (Clk nm rate) (cEn,aEn) (cEn,bEn) (c,a) (c,b)
+secondDF f = swapDF `seqDF` firstDF f `seqDF` swapDF
 
 -- | Compose two 'DataFlow' circuits in parallel.
-(***) :: (KnownSymbol nm, KnownNat rate)
+parDF :: (KnownSymbol nm, KnownNat rate)
       => DataFlow (Clk nm rate) aEn bEn a b
       -> DataFlow (Clk nm rate) cEn dEn c d
       -> DataFlow (Clk nm rate) (aEn,cEn) (bEn,dEn) (a,c) (b,d)
-f *** g = first f >>> second g
+f `parDF` g = firstDF f `seqDF` secondDF g
 
 -- | Feed back the second halve of the communication channel.
-loop :: (KnownSymbol nm, KnownNat rate)
-     => DataFlow (Clk nm rate) Bool Bool (a,d) (b,d)
-     -> DataFlow (Clk nm rate) Bool Bool a     b
-loop (DF f) = DF (\a bEn -> let clk       = sclock
-                                (aD,aV)   = unbundle clk a
-                                and1      = aV   .&&. and3
-                                and2      = adEn .&&. and3
-                                and3      = bdV  .&&. bEn
-                                and4      = bdV  .&&. and6
-                                and5      = bEn  .&&. and6
-                                and6      = adEn .&&. aV
-                                adD       = bundle clk (aD,dD)
-                                ad        = bundle clk (adD,and1)
-                                (bd,adEn) = f ad and5
-                                (bdD,bdV) = unbundle clk bd
-                                (bD,dD)   = unbundle clk bdD
-                            in  (bundle clk (bD,and4),and2)
-                 )
+loopDF :: (KnownSymbol nm, KnownNat rate)
+       => DataFlow (Clk nm rate) Bool Bool (a,d) (b,d)
+       -> DataFlow (Clk nm rate) Bool Bool a     b
+loopDF (DF f) = DF (\a bEn -> let clk       = sclock
+                                  (aD,aV)   = unbundle clk a
+                                  and1      = aV   .&&. and3
+                                  and2      = adEn .&&. and3
+                                  and3      = bdV  .&&. bEn
+                                  and4      = bdV  .&&. and6
+                                  and5      = bEn  .&&. and6
+                                  and6      = adEn .&&. aV
+                                  adD       = bundle clk (aD,dD)
+                                  ad        = bundle clk (adD,and1)
+                                  (bd,adEn) = f ad and5
+                                  (bdD,bdV) = unbundle clk bd
+                                  (bD,dD)   = unbundle clk bdD
+                              in  (bundle clk (bD,and4),and2)
+                    )
 
 -- | Change the synchronisation granularity, but leave the data unchanged.
 mapDF :: (KnownSymbol nm, KnownNat rate)
@@ -115,3 +118,42 @@ mapDF f g = DF (\din rb -> let clk    = sclock
                                ra'    = fmap g rb
                            in  (bundle clk (d,rb'),ra')
                )
+
+instance Category (DataFlow clk Bool Bool) where
+  id              = DF (\d r -> (d,r))
+  (DF g) . (DF f) = DF (\a cR -> let (b,aR) = f a bR
+                                     (c,bR) = g b cR
+                                 in  (c,aR)
+                       )
+
+instance (KnownSymbol nm, KnownNat rate) =>
+  Arrow (DataFlow (Clk nm rate) Bool Bool) where
+  arr f        = DF (\a r -> (fmap (first f) a,r))
+  first (DF f) = DF (\bd r -> let clk     = sclock
+                                  (bdD,v) = unbundle clk bd
+                                  (b,d)   = unbundle clk bdD
+                                  (c,bR)  = f (bundle clk (b,v)) r
+                                  (cD,cV) = unbundle clk c
+                                  cdD     = bundle clk (cD,d)
+                                  v'      = cV .&&. v
+                                  cd      = bundle clk (cdD,v')
+                                  r'      = bR .&&. r
+                              in  (cd,r')
+                    )
+
+instance (KnownSymbol nm, KnownNat rate) =>
+  ArrowLoop (DataFlow (Clk nm rate) Bool Bool) where
+  loop = loopDF
+
+instance (KnownSymbol nm, KnownNat rate) =>
+  ArrowChoice (DataFlow (Clk nm rate) Bool Bool) where
+  left (DF f) =
+    DF (\ebd r -> let clk              = sclock
+                      (c,fR)           = f b rIn
+                      (ecd,rOut,b,rIn) = unbundle clk (route <$> ebd <*> r
+                                                             <*> c   <*> fR)
+                  in  (ecd,rOut)
+        )
+    where
+      route (Left b,v)  r (c,cV) bR = ((Left c,cV),bR,(b,v),r)
+      route (Right d,v) r _      _  = ((Right d,v),r ,(undefined,False),False)
