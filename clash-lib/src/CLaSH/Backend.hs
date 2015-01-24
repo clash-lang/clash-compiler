dad689cc5ac9663ea5c86c4f642f42f4bfa0bbba
John Ericson <Ericson2314@Yahoo.com>
2015-01-23 20:19:42 -0500

    Copy docs into Backend type class

--- clash-lib/src/CLaSH/Backend.hs
+++ clash-lib/src/CLaSH/Backend.hs
@@ -8,12 +8,19 @@ import CLaSH.Netlist.Types
 
 class Backend state where
   init             :: state
+  -- | Generate VHDL for a Netlist component
   genVHDL          :: Component    -> State state (String, Doc)
+  -- | Generate a VHDL package containing type definitions for the given HWTypes
   mkTyPackage      :: [HWType]     -> State state Doc
+  -- | Convert a Netlist HWType to a VHDL type
   vhdlType         :: HWType       -> State state Doc
+  -- | Convert a Netlist HWType to an error VHDL value for that type
   vhdlTypeErrValue :: HWType       -> State state Doc
+  -- | Convert a Netlist HWType to the root of a VHDL type
   vhdlTypeMark     :: HWType       -> State state Doc
+  -- | Turn a Netlist Declaration to a VHDL concurrent block
   inst             :: Declaration  -> State state (Maybe Doc)
+  -- | Turn a Netlist expression into a VHDL expression
   expr             :: Bool -> Expr -> State state Doc
   extractTypes     :: state -> HashSet HWType
   --expr             :: Declaration  -> State state Doc
