b3b91310a04cf959f8ad1948561c3513b59434b7
John Ericson <Ericson2314@Yahoo.com>
2015-02-01 23:49:55 -0500

    Make IR for verilog backend, stencil out verilog backend

---
+++ clash-verilog/src/CLaSH/Backend/Verilog/Bore.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE LambdaCase #-}
+module CLaSH.Backend.Verilog.Bore where
+
+import           Control.Applicative
+
+import Data.Text.Lazy                              (Text)
+
+import qualified CLaSH.Netlist.Types as N
+import           CLaSH.Util                        (curLoc)
+
+import           CLaSH.Backend.Verilog.BoringTypes
+
+
+-- | Clash Conponent converted to somethign approximating Verilog's module
+component :: N.Component -> Component Text
+component (N.Component name hports inpts outpt decls) =
+  Component name (hwtype <$$> hports) (hwtype <$$> inpts) (hwtype <$> outpt) (declaration <$> decls)
+
+
+-- | Clash Decleration converted to somethign approximating Verilog's decleration
+declaration :: N.Declaration -> Declaration Text
+declaration = \case
+  N.Assignment i e            -> Assignment [i] $ expr e
+  N.CondAssignment i e branches -> CondAssignment i (expr e) $ custMap expr <$> branches
+    where custMap f (x , y) = (f <$> x , f y)
+  N.InstDecl i1 i2 portAssigns -> InstDecl i1 i2 $ expr <$$> portAssigns
+  N.BlackBoxD bb               -> BlackBoxD bb
+  N.NetDecl i t me             -> NetDecl i (hwtype t) $ expr <$> me
+
+
+-- | Clash Hardware type converted to somethign approximating Verilog's fixed
+-- types
+hwtype :: N.HWType -> HWType
+hwtype = \case
+  N.Void         -> Bits [(0, undefined)] -- try this
+  N.Bool         -> Bits [(1, False)]
+
+  N.Integer      -> Integer
+  N.BitVector n  -> Bits [(n, False)]
+  N.Signed    n  -> Bits [(n, True)]
+
+  -- TODO could be improoved ?
+  N.Index     _  -> error $ $(curLoc) ++ "index types unsupported"
+
+  N.Unsigned  n  -> Bits [(n, False)]
+  N.Vector  n t  -> Bits $ concat $ replicate n lst
+    where lst = case hwtype t of
+            Integer  -> error $ $(curLoc) ++ "cannot vector integer"
+            Bits lst' -> lst'
+
+
+  N.Sum     _ _  -> error $ $(curLoc) ++ "sum types unsupported"
+  N.Product _ ts -> Bits $ concat $ bitsOnly <$> hwtype <$> ts
+
+  N.SP      _ _  -> error $ $(curLoc) ++ "sum of products unsupported"
+
+  N.Clock _      -> hwtype N.Bool
+  N.Reset _      -> hwtype N.Bool
+
+  where bitsOnly :: HWType -> [(N.Size, Bool)]
+        bitsOnly = \case
+          Integer  -> error $ $(curLoc) ++ "Cannot tuple Integers"
+          Bits lst -> lst
+
+
+expr :: N.Expr -> Expr Text
+expr = \case
+  N.DataCon _ _ _    -> error $ $(curLoc) ++ "Data constructors unsupported"
+  N.DataTag _ _      -> error $ $(curLoc) ++ "Data tags unsupported"
+
+  N.Literal mty l    -> literal (fst <$> mty) l
+  N.Identifier i mm  -> modifier mm $ Right $ Identifier i
+  N.BlackBoxE str mm -> modifier mm $ Left str
+
+
+modifier :: Maybe N.Modifier
+         -> Either blackbox (BigRecur blackbox)
+         -> Expr blackbox
+modifier mm e = MTBBE $ MT Nothing $ case mm of
+  Nothing                             -> E <$> e
+  (Just (N.Indexed (ty, start, end))) -> Right $ Index (hwtype ty) start end $ MT (Just $ hwtype ty) e
+  (Just (N.DC _))                     -> error $ $(curLoc) ++ "DataCon context unsupported"
+  (Just (N.VecAppend))                -> error $ $(curLoc) ++ "Not sure how VecAppend works"
+
+literal :: Maybe N.HWType -> N.Literal -> Expr blackbox
+literal mty = \case
+  N.NumLit n      -> simp $ Number n
+  N.BoolLit False -> simp $ Number 0
+  N.BoolLit True  -> simp $ Number 1
+  N.BitLit N.H    -> literal mty $ N.BoolLit False
+  N.BitLit N.L    -> literal mty $ N.BoolLit True
+  N.BitLit N.U    -> simp $ Undefined
+  N.BitLit N.Z    -> simp $ HighImpedence
+  N.VecLit lits   -> MTBBE $ MT (hwtype <$> mty) $ Right $ E $ Concat $ case (lits, mty) of
+    ([] , _)                    -> []
+    (_  , Just (N.Vector _ ty)) -> literal (Just ty) <$> lits
+    (_  , Nothing)              -> literal Nothing <$> lits -- this is probably dead code
+    _                           -> error $ $(curLoc) ++ "Bad type for vector literal"
+
+  where simp = MTBBE . MT (hwtype <$> mty) . Right . E . Literal
+
+infixr 0 <$$>
+
+(<$$>) :: (Functor fx, Functor fy) => (a -> b) -> fx (fy a) -> fx (fy b)
+f <$$> x = (fmap . fmap) f x
---
+++ clash-verilog/src/CLaSH/Backend/Verilog/BoringTypes.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE PolyKinds, KindSignatures, TypeOperators #-}
+{-# LANGUAGE LambdaCase #-}
+module CLaSH.Backend.Verilog.BoringTypes where
+
+import Data.Foldable
+import Data.Traversable
+
+import Control.Applicative
+import Control.DeepSeq
+
+
+
+import qualified CLaSH.Netlist.Types as N
+
+-- | Component: base unit of a Netlist
+data Component blackbox
+  = Component
+  { componentName :: N.Identifier -- ^ Name of the component
+  , hiddenPorts   :: [(N.Identifier, HWType)] -- ^ Ports that have no correspondence the original function definition
+  , inputs        :: [(N.Identifier, HWType)] -- ^ Input ports
+  , output        :: (N.Identifier, HWType) -- ^ Output port
+  , declarations  :: [Declaration blackbox] -- ^ Internal declarations
+  }
+  deriving Show
+
+instance NFData blackbox => NFData (Component blackbox) where
+  rnf (Component nm hi inps outps decls) =
+    rnf nm `seq` rnf hi `seq` rnf inps `seq` rnf outps `seq` rnf decls
+
+data HWType
+  = Integer               -- ^ Just for meta ?
+  | Bits [(N.Size, Bool)] -- ^ Bits, [size of old field, isSigned]. List should never be empty
+  deriving (Eq, Show)
+
+instance NFData HWType where
+  rnf = \case
+    Integer  -> ()
+    Bits lst -> rnf $ rnf <$> lst
+
+data Declaration blackbox
+  = Assignment      -- ^ Signal assignment
+    [N.Identifier]  -- * Signal to assign (list because concats can be lvalues)
+    (Expr blackbox) -- * Assigned expression
+
+  | CondAssignment                           -- ^ Conditional signal assignment
+    N.Identifier                             -- * Signal to assign
+    (Expr blackbox)                          -- * Scrutinized expression
+    [(Maybe (Expr blackbox), Expr blackbox)] -- * List of: (Maybe expression scrutinized expression is compared with,RHS of alternative)
+
+
+  | InstDecl -- ^ Instantiation of another component
+    N.Identifier
+    N.Identifier
+    [(N.Identifier, Expr blackbox)]
+
+  | BlackBoxD blackbox -- ^ Instantiation of blackbox declaration
+
+  | NetDecl N.Identifier HWType (Maybe (Expr blackbox)) -- ^ Signal declaration
+  deriving Show
+
+instance NFData (Declaration blackbox) where
+  rnf a = a `seq` ()
+
+
+-- | CoreExpression used in RHS of a declaration
+data NonIndex recur
+  = Literal    Literal      -- ^ Literal expression
+  | Concat     [recur]      -- ^ New!
+  | Identifier N.Identifier -- ^ Signal reference
+  -- - | DataTag    HWType       -- ^ @Left e@: tagToEnum#, @Right e@: dataToTag#
+  -- -| DataCon  HWType  (Maybe Modifier) [CoreExpr] -- ^ DataCon application
+  deriving (Show, Functor, Foldable, Traversable)
+
+data Literal
+  = Number Integer
+  | HighImpedence
+  | Undefined
+  deriving Show
+
+-- | Core Expression type that doesn't permit indexing "twice in a row"
+data CoreExpr index noIndex
+  = E index
+  | Index HWType Int Int noIndex
+  deriving (Show, Functor, Foldable, Traversable)
+
+type CoreExprHelper (splice :: * -> *) recur = splice :$ CoreExpr (NonIndex :$ recur) (splice :$ NonIndex :$ recur)
+
+-- | Add optional typing to every node
+data MaybeTyped recur
+  = MT (Maybe HWType) recur
+  deriving (Show, Functor, Foldable, Traversable)
+
+infixr 0 :$
+type (:$) (f :: a -> *) (x :: a) = (f x)
+infixr 9 :.
+type (:.) (f :: b -> *) (g :: a -> b) (x :: a) = (f (g x))
+
+data Mu (t :: * -> *) = Mu (t :$ Mu t)
+
+type BigRecur blackbox = NonIndex :$ Expr blackbox
+type Splice blackbox x = MaybeTyped :$ Either blackbox x
+
+data Expr blackbox =
+  MTBBE (Splice blackbox
+                (CoreExpr (BigRecur blackbox)
+                      (Splice blackbox (BigRecur blackbox))))
+  deriving Show
