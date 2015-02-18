586c2995a7f3ef9c631230d3ca582987ffe6a7cb
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-02-18 14:50:12 +0100

    Adapt SigP.hs test to new prelude

--- tests/shouldwork/Signal/SigP.hs
+++ tests/shouldwork/Signal/SigP.hs
@@ -3,4 +3,4 @@ module SigP where
 import CLaSH.Prelude
 
 topEntity :: Signal (Bool, Bool) -> (Signal Bool, Signal Bool)
-topEntity = sWrap . register (False,False)
+topEntity = unbundle' . register (False,False)
