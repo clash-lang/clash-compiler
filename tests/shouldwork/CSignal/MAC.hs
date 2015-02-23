dda5b75825021b3e9535e828638feff0c020d72c
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-02-23 14:55:26 +0100

    Update CSignal/Mac test to new prelude

--- tests/shouldwork/CSignal/MAC.hs
+++ tests/shouldwork/CSignal/MAC.hs
@@ -3,16 +3,16 @@ module MAC where
 import CLaSH.Prelude
 import CLaSH.Prelude.Explicit
 
-type Clk100 = Clk "clk" 100
+type Clk100 = Clk "clk" 101
 
 clk100 :: SClock Clk100
 clk100 = sclock
 
 topEntity :: (CSignal Clk100 Integer, CSignal Clk100 Integer)
-          -> (CSignal Clk100 Bool, CSignal Clk100 Integer)
-topEntity = sync clk100 macT 0
+          -> CSignal Clk100 Integer
+topEntity = cmealyB clk100 macT 0
 
-macT s (x,y) = (s',Just o)
+macT s (x,y) = (s', o)
   where
     s' = s + (x * y)
     o  = s
