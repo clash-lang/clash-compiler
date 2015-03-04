c4d8ca619a9139c574a7a7b1dc03522648dc6978
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-03-04 12:03:18 +0100

    Add test for #44

---
+++ tests/shouldwork/Basic/RecordSumOfProducts.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module RecordSumOfProducts where
+
+import CLaSH.Prelude
+import Control.Applicative
+
+data DbState = DbInitDisp (Unsigned 4) | DbWriteRam (Signed 14) (Signed 14)
+             | DbDone
+    deriving (Show, Eq)
+
+data DbS = DbS { dbS :: DbState }
+
+
+topEntity = walkState <^> DbS (DbInitDisp 0)
+
+walkState :: DbS
+          -> Bit
+          -> (DbS, Bit)
+
+walkState (DbS (DbInitDisp n    )) i = (DbS (DbInitDisp (n+1)  ), 0)
+walkState s                        i = (s                       , i)
