dc098c6f9d29aeff48841d944aad16ff2f1aa603
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-05-05 17:09:18 +0200

    Update TopEntity specifications

--- src/CLaSH/Annotations/TopEntity.hs
+++ src/CLaSH/Annotations/TopEntity.hs
@@ -7,17 +7,21 @@ Copyright  :  (C) 2015, University of Twente
 License    :  BSD2 (see the file LICENSE)
 Maintainer :  Christiaan Baaij <christiaan.baaij@gmail.com>
 
+The 'TopEntity' annotations described in this module make it easier to put your
+design on an FPGA.
+
+We can exert some control how the top level function is created by the CλaSH
+compiler by annotating the @topEntity@ function with a 'TopEntity' annotation.
+You apply these annotation using the @ANN@ pragma like so:
+
 @
-{\-\# ANN topEntity
-  ('defTop'
-    { t_name     = "blinker"
-    , t_inputs   = [\"KEY1\"]
-    , t_outputs  = [\"LED\"]
-    , t_extraIn  = [ (\"CLOCK_50\", 1)
-                   , (\"KEY0\"    , 1)
-                   ]
-    , t_clocks   = [ 'defClkAltera' "altpll50" "CLOCK_50(0)" "not KEY0(0)" ]
-    }) \#-\}
+{\-\# ANN topEntity (TopEntity {t_name = ..., ...  }) \#-\}
+topEntity x = ...
+@
+
+For example, given the following specification:
+
+@
 topEntity :: Signal Bit -> Signal (BitVector 8)
 topEntity key1 = leds
   where
@@ -41,7 +45,53 @@ blinkerT (leds,mode,cntr) key1R = ((leds',mode',cntr'),leds)
           | otherwise = leds
 @
 
-Generates the following @blinker.vhdl@ file:
+The CλaSH compiler will normally generate the following @topEntity.vhdl@ file:
+
+@
+-- Automatically generated VHDL
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.MATH_REAL.ALL;
+use work.all;
+use work.types.all;
+
+entity topEntity is
+  port(input_0         : in std_logic_vector(0 downto 0);
+       -- clock
+       system1000      : in std_logic;
+       -- asynchronous reset: active low
+       system1000_rstn : in std_logic;
+       output_0        : out std_logic_vector(7 downto 0));
+end;
+
+architecture structural of topEntity is
+begin
+  topEntity_0_inst : entity topEntity_0
+    port map
+      (key1_i1         => input_0
+      ,system1000      => system1000
+      ,system1000_rstn => system1000_rstn
+      ,topLet_o        => output_0);
+end;
+@
+
+However, if we add the following 'TopEntity' annotation in the file:
+
+@
+{\-\# ANN topEntity
+  ('defTop'
+    { t_name     = "blinker"
+    , t_inputs   = [\"KEY1\"]
+    , t_outputs  = [\"LED\"]
+    , t_extraIn  = [ (\"CLOCK_50\", 1)
+                   , (\"KEY0\"    , 1)
+                   ]
+    , t_clocks   = [ 'defClkAltera' "altpll50" "CLOCK_50(0)" "not KEY0(0)" ]
+    }) \#-\}
+@
+
+The CλaSH compiler will generate the following @blinker.vhdl@ file instead:
 
 @
 -- Automatically generated VHDL
@@ -99,6 +149,21 @@ begin
 end;
 @
 
+Where we now have:
+
+* A top-level component that is called @blinker@.
+* Inputs and outputs that have a /user/-chosen name: @KEY1@, @LED@, etc.
+* An instantiated <https://www.altera.com/literature/ug/ug_altpll.pdf PLL>
+  component providing a stable clock signal from the free-running clock pin
+  @CLOCK_50@.
+* A reset that is /asynchronously/ asserted by the @lock@ signal originating from
+  the PLL, meaning that your design is kept in reset until the PLL is
+  providing a stable clock.
+  The reset is additionally /synchronously/ de-asserted to prevent
+  <http://en.wikipedia.org/wiki/Metastability_in_electronics metastability>
+  of your design due to unlucky timing of the de-assertion of the reset.
+
+See the documentation of 'TopEntity' for the meaning of all its fields.
 -}
 module CLaSH.Annotations.TopEntity
   ( -- * Data types
@@ -114,15 +179,38 @@ where
 import Data.Data
 import CLaSH.Signal.Explicit (systemClock)
 
--- | TopEntity specifications, fields are self-explanatory
+-- | TopEntity annotation
 data TopEntity
   = TopEntity
-  { t_name     :: String
-  , t_inputs   :: [String]       -- optional
-  , t_outputs  :: [String]       -- optional
-  , t_extraIn  :: [(String,Int)] -- optional
-  , t_extraOut :: [(String,Int)] -- optional
-  , t_clocks   :: [ClockSource]  -- optional
+  { t_name     :: String         -- ^ The name the top-level component should
+                                 -- have, put in a correspondingly named file.
+  , t_inputs   :: [String]       -- ^ List of names that are assigned in-order
+                                 -- to the inputs of the component.
+  , t_outputs  :: [String]       -- ^ List of names that are assigned in-order
+                                 -- to the outputs of the component.
+  , t_extraIn  :: [(String,Int)]
+  -- ^ Extra input ports, where every tuple holds the name of the input port and
+  -- the number of  bits are used for that input port.
+  --
+  -- So given a bit-width @n@, the port has type:
+  --
+  -- @std_logic_vector (n-1 downto 0)@ in VHDL
+  --
+  -- and
+  --
+  -- @logic [n-1:0]@ in (System)Verilog.
+  , t_extraOut :: [(String,Int)]
+  -- ^ Extra output ports, where every tuple holds the name of the input port
+  -- and the number of bits are used for that input port.
+  --
+  -- So given a bit-width @n@, the port has type:
+  --
+  -- @std_logic_vector (n-1 downto 0)@ in VHDL
+  --
+  -- and
+  --
+  -- @logic [n-1:0]@ in (System)Verilog.
+  , t_clocks   :: [ClockSource]  -- ^ List of clock sources
   }
   deriving (Data,Show)
 
@@ -130,12 +218,43 @@ data TopEntity
 data ClockSource
   = ClockSource
   { c_name  :: String                -- ^ Component name
-  , c_inp   :: Maybe (String,String) -- ^ optional: (Input port, clock pin)
-  , c_outp  :: [(String,String)]     -- ^ \[(output port,clock signal)\]
-  , c_reset :: Maybe (String,String) -- ^ optional: Asynchronous reset input
-  , c_lock  :: String                -- ^ Port name that indicates clock is stable
-  , c_sync  :: Bool                  -- ^ optional: devices connected this clock
-                                     -- source should be pulled out of reset in-sync
+  , c_inp   :: Maybe (String,String) -- ^ optional: @(Input port, clock pin/expression)@
+  , c_outp  :: [(String,String)]
+  -- ^ List of @(Output port, clock)@
+  --
+  -- The best way to create the 'String' representing the name of the clock is
+  -- to 'show' the corresponding singleton clock ('CLaSH.Signal.Explicit.sclock').
+  --
+  -- So given that you your design is synchronised to the 'CLaSH.Signal.Explicit.SystemClock'
+  -- and some another clock @ClkA@
+  --
+  -- > type ClkA = 'Clk "clkA" 2000
+  --
+  -- the best way to connect output clock ports is by doing:
+  --
+  -- > ClockSource { ..
+  -- >             , c_outp = [("c0", show (sclock :: SClock SystemClock))
+  -- >                        ,("c1", show (sclock :: SClock ClkA))
+  -- >                        ]
+  -- >             , ..
+  -- >             }
+  , c_reset :: Maybe (String,String) -- ^ optional: @(Reset port, reset pin/expression)@
+  , c_lock  :: String                -- ^ Output port name of the clock source
+                                     -- component indicating that the clock signal
+                                     -- is stable.
+  , c_sync  :: Bool
+  -- ^ Indicates whether the components synchronised by the clocks generated by
+  -- this clock source are pulled out of reset simultaneously.
+  --
+  -- The recommended setting if 'False'.
+  --
+  -- When 'c_sync' is set to 'False', the compiler generates reset synchronisers
+  -- which ensure that each component is synchronously pulled out of reset,
+  -- preventing <http://en.wikipedia.org/wiki/Metastability_in_electronics metastability>
+  -- introduced by unlucky timing of the reset de-assertion.
+  --
+  -- When 'c_sync' is set to 'True' those reset synchronisers are not generated
+  -- and there is change for reset-induced metastability.
   }
   deriving (Data,Show)
 
@@ -155,7 +274,7 @@ defTop = TopEntity
   }
 
 -- | A clock source that corresponds to the Altera PLL component with default
--- settings connected to the the system clock.
+-- settings to provide a stable 'systemClock'.
 --
 -- >>> defClkAltera "altpll50" "CLOCK(0)" "not KEY(0)"
 -- ClockSource {c_name = "altpll50", c_inp = Just ("inclk0","CLOCK(0)"), c_outp = [("c0","system1000")], c_reset = Just ("areset","not KEY(0)"), c_lock = "locked", c_sync = False}
@@ -168,9 +287,22 @@ defTop = TopEntity
 -- >     ,c0     => system1000
 -- >     ,areset => not KEY0(0)
 -- >     ,locked => altpll50_locked);
-defClkAltera :: String -- ^ Name of the component
-             -> String -- ^ Clock Pin/Expression
-             -> String -- ^ Reset Pin/Expression
+--
+-- If you are however generating (System)Verilog you should write:
+--
+-- >>> defClkAltera "altpll50" "CLOCK[0]" "~ KEY[0]"
+-- ClockSource {c_name = "altpll50", c_inp = Just ("inclk0","CLOCK[0]"), c_outp = [("c0","system1000")], c_reset = Just ("areset","~ KEY[0]"), c_lock = "locked", c_sync = False}
+--
+-- so that the following (System)Verilog is created:
+--
+-- > altpll50 altpll50_inst
+-- > (.inclk0 (CLOCK_50[0])
+-- > ,.c0 (system1000)
+-- > ,.areset (~ KEY0[0])
+-- > ,.locked (altpll50_locked));
+defClkAltera :: String -- ^ Name of the component.
+             -> String -- ^ Clock Pin/Expression of the free running clock.
+             -> String -- ^ Reset Pin/Expression controlling the reset of the PLL.
              -> ClockSource
 defClkAltera pllName clkExpr resExpr = ClockSource
   { c_name  = pllName
@@ -182,7 +314,7 @@ defClkAltera pllName clkExpr resExpr = ClockSource
   }
 
 -- | A clock source that corresponds to the Xilinx PLL/MMCM component with
--- default settings connected to the the system clock.
+-- settings to provide a stable 'systemClock'.
 --
 -- >>> defClkXilinx "clkwiz50" "CLOCK(0)" "not KEY(0)"
 -- ClockSource {c_name = "clkwiz50", c_inp = Just ("CLK_IN1","CLOCK(0)"), c_outp = [("CLK_OUT1","system1000")], c_reset = Just ("RESET","not KEY(0)"), c_lock = "LOCKED", c_sync = False}
@@ -195,9 +327,22 @@ defClkAltera pllName clkExpr resExpr = ClockSource
 -- >     ,CLK_OUT1 => system1000
 -- >     ,RESET    => not KEY0(0)
 -- >     ,LOCKED   => altpll50_locked);
-defClkXilinx :: String -- ^ Name of the component
-             -> String -- ^ Clock Pin/Expression
-             -> String -- ^ Reset Pin/Expression
+--
+-- If you are however generating (System)Verilog you should write:
+--
+-- >>> defClkXilinx "clkwiz50" "CLOCK[0]" "~ KEY[0]"
+-- ClockSource {c_name = "clkwiz50", c_inp = Just ("CLK_IN1","CLOCK[0]"), c_outp = [("CLK_OUT1","system1000")], c_reset = Just ("RESET","~ KEY[0]"), c_lock = "LOCKED", c_sync = False}
+--
+-- so that the following (System)Verilog is created:
+--
+-- > clkwiz50 clkwiz50_inst
+-- > (.CLK_IN1 (CLOCK_50[0])
+-- > ,.CLK_OUT1 (system1000)
+-- > ,.RESET (~ KEY0[0])
+-- > ,.LOCKED (altpll50_locked));
+defClkXilinx :: String -- ^ Name of the component.
+             -> String -- ^ Clock Pin/Expression of the free running clock.
+             -> String -- ^ Reset Pin/Expression controlling the reset of the PLL.
              -> ClockSource
 defClkXilinx pllName clkExpr resExpr = ClockSource
   { c_name  = pllName
