0f352f9aa4b6daf657cdf10e04352094bf5d9a8f
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-06-26 15:53:48 +0200

    Generate correct vector-indexing in SV backend

--- clash-systemverilog/src/CLaSH/Backend/SystemVerilog.hs
+++ clash-systemverilog/src/CLaSH/Backend/SystemVerilog.hs
@@ -309,7 +309,7 @@ expr_ _ (Identifier id_ (Just (Indexed (ty@(SP _ args),dcI,fI)))) = fromSLV argT
     end      = start - argSize + 1
 
 expr_ _ (Identifier id_ (Just (Indexed (ty@(Product _ _),_,fI)))) = text id_ <> dot <> verilogTypeMark ty <> "_sel" <> int fI
-expr_ _ (Identifier id_ (Just (Indexed ((Vector _ _),_,fI)))) = text id_ <> parens (int fI)
+expr_ _ (Identifier id_ (Just (Indexed ((Vector _ _),_,fI)))) = text id_ <> brackets (int fI)
 expr_ _ (Identifier id_ (Just (DC (ty@(SP _ _),_)))) = text id_ <> brackets (int start <> colon <> int end)
   where
     start = typeSize ty - 1
