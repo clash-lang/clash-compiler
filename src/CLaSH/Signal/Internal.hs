c7a404964f4c64772e917258d730f3d4f1eec10c
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-07-29 10:06:45 +0200

    Disable strictness analysis in CLaSH.Signal.Internal
    See https://github.com/clash-lang/clash-compiler/commit/721fcfa9198925661cd836668705f817bddaae3c
    for more details

--- src/CLaSH/Signal/Internal.hs
+++ src/CLaSH/Signal/Internal.hs
@@ -10,6 +10,12 @@
 
 {-# LANGUAGE Unsafe #-}
 
+-- Until we have a -fcpr-off DynFlag (it currently exists only as a StaticFlag),
+-- we just disable strictness analysis for this module completely. See
+-- https://github.com/clash-lang/clash-compiler/commit/721fcfa9198925661cd836668705f817bddaae3c
+-- for more details
+{-# OPTIONS_GHC -fno-strictness #-}
+
 {-# OPTIONS_HADDOCK show-extensions #-}
 
 {-|
