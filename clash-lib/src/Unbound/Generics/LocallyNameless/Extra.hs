8db1e571147f964c79db25e8187c31d54697daf0
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-08-02 10:22:56 +0200

    Compile againt unbound-generics-0.2

--- clash-lib/src/Unbound/Generics/LocallyNameless/Extra.hs
+++ clash-lib/src/Unbound/Generics/LocallyNameless/Extra.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
@@ -5,24 +6,36 @@
 
 module Unbound.Generics.LocallyNameless.Extra where
 
+#if MIN_VERSION_unbound_generics(0,2,0)
+#else
 import Control.DeepSeq
+#endif
 import Data.Hashable                           (Hashable(..),hash)
 import Data.Text                               (Text)
 import GHC.Real                                (Ratio)
 import Unbound.Generics.LocallyNameless.Alpha  (Alpha (..))
+#if MIN_VERSION_unbound_generics(0,2,0)
+#else
 import Unbound.Generics.LocallyNameless.Bind   (Bind (..))
 import Unbound.Generics.LocallyNameless.Embed  (Embed (..))
+#endif
 import Unbound.Generics.LocallyNameless.Name   (Name (..))
+#if MIN_VERSION_unbound_generics(0,2,0)
+#else
 import Unbound.Generics.LocallyNameless.Rebind (Rebind (..))
 import Unbound.Generics.LocallyNameless.Rec    (Rec,unrec)
+#endif
 import Unbound.Generics.LocallyNameless.Subst  (Subst (..))
 
+#if MIN_VERSION_unbound_generics(0,2,0)
+#else
 instance (NFData a, NFData b) => NFData (Bind a b)
 instance NFData a => NFData (Embed a)
 instance NFData (Name a)
 instance (NFData a, NFData b) => NFData (Rebind a b)
 instance (Alpha a, NFData a) => NFData (Rec a) where
   rnf r = seq (rnf (unrec r)) ()
+#endif
 
 instance Subst b (Ratio a) where
   subst  _ _ = id
@@ -32,8 +45,11 @@ instance Hashable (Name a) where
   hashWithSalt salt (Fn str int) = hashWithSalt salt (hashWithSalt (hash int) str)
   hashWithSalt salt (Bn i0  i1)  = hashWithSalt salt (hash i0 `hashWithSalt` i1)
 
+#if MIN_VERSION_unbound_generics(0,2,0)
+#else
 instance (Ord a) => Ord (Embed a) where
   compare (Embed a) (Embed b) = compare a b
+#endif
 
 instance Alpha Text where
   aeq' _ctx             = (==)
