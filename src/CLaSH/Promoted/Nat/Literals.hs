3fd7a3d309dffb3b0ad0beadeacd9a15b79cb3ac
Leon Schoorl <l.m.schoorl@student.utwente.nl>
2015-09-04 16:17:47 +0200

    Typo in docs

--- src/CLaSH/Promoted/Nat/Literals.hs
+++ src/CLaSH/Promoted/Nat/Literals.hs
@@ -19,7 +19,7 @@ d0 = snat :: SNat 0
 d1 = snat :: SNat 1
 d2 = snat :: SNat 2
 ...
-d1024 = snat :: SNat 102
+d1024 = snat :: SNat 1024
 @
 
 You can generate more 'SNat' literals using 'decLiteralsD' from "CLaSH.Promoted.Nat.TH"
