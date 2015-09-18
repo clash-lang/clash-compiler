0db87680feb02cb5123af455d9353443b1c3c0f2
Leon Schoorl <l.m.schoorl@student.utwente.nl>
2015-09-18 17:45:55 +0200

    Separate the vectors in the Bounds test
    This makes them easier to translate from clash

--- tests/shouldwork/Numbers/Bounds.hs
+++ tests/shouldwork/Numbers/Bounds.hs
@@ -1,18 +1,22 @@
 module Bounds where
 import CLaSH.Prelude
 
-(expected,actual) = unzip $
-        (   0, toInteger (minBound :: Index 43))
-     :> (  42, toInteger (maxBound :: Index 43))
-     :> (-256, toInteger (minBound :: Signed 9))
-     :> ( 255, toInteger (maxBound :: Signed 9))
-     :> (   0, toInteger (minBound :: Unsigned 9))
-     :> ( 511, toInteger (maxBound :: Unsigned 9))
-     :> ( -64, toInteger $ unFixed (minBound :: SFixed 4 3))
-     :> (  63, toInteger $ unFixed (maxBound :: SFixed 4 3))
-     :> (   0, toInteger $ unFixed (minBound :: UFixed 3 4))
-     :> ( 127, toInteger $ unFixed (maxBound :: UFixed 3 4))
-     :> Nil
+
+expected
+    =     0 :>  42
+    :> -256 :> 255
+    :>    0 :> 511
+    :>  -64 :>  63
+    :>    0 :> 127
+    :> (Nil::Vec 0 Integer)
+
+actual
+    =  toInteger (minBound :: Index 43)             :> toInteger (maxBound :: Index 43)
+    :> toInteger (minBound :: Signed 9)             :> toInteger (maxBound :: Signed 9)
+    :> toInteger (minBound :: Unsigned 9)           :> toInteger (maxBound :: Unsigned 9)
+    :> toInteger (unFixed (minBound :: SFixed 4 3)) :> toInteger (unFixed (maxBound :: SFixed 4 3))
+    :> toInteger (unFixed (minBound :: UFixed 3 4)) :> toInteger (unFixed (maxBound :: UFixed 3 4))
+    :> Nil
 
 topEntity :: Signal () -> Signal Integer
 topEntity = mealy loop actual
@@ -25,7 +29,6 @@ loop xs _ = (xs <<+ last xs, head xs)
 expectedOutput :: Signal Integer -> Signal Bool
 expectedOutput = outputVerifier expected
 
--- create the right number of inputs
-stimuli = map (const ()) actual
 
-testInput = stimuliGenerator stimuli
+testInput :: Signal ()
+testInput  = signal ()
