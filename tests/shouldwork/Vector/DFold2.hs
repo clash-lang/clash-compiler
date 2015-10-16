e421041617421588fa9c1d15cab6e57a14fdd497
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-10-16 12:12:54 +0200

    'smap' is now in the prelude

--- tests/shouldwork/Vector/DFold2.hs
+++ tests/shouldwork/Vector/DFold2.hs
@@ -4,13 +4,6 @@ module DFold2 where
 import CLaSH.Prelude
 import Data.Proxy
 
-smap :: KnownNat k => (forall l . SNat (k-1-l) -> a -> b) -> Vec k a -> Vec k b
-smap f xs = dfold (Proxy :: Proxy (VCons a))
-                  (\sn x xs -> f (xsL `subSNat` d1 `subSNat` sn) x :> xs)
-                  Nil xs
-  where
-    xsL = lengthS xs
-
 topEntity :: Vec 4 (Vec 4 (Unsigned 8)) -> Vec 4 (Vec 4 (Unsigned 8))
 topEntity = smap (flip rotateRightS)
 
