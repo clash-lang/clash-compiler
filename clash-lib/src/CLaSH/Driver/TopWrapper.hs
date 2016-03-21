clash-lang/clash-compiler

commit 7652df6c118b71307cebaa79f58d668129746a69

Generate VHDL tweaked for Xilinx Vivado
Vivado cannot infere blockRAMs when the elements are anything
else than std_logic_vector. Consequently, elements of arrays
are always std_logic_vector, and product types are no longer
representated as VHDL records.

Fixes #127

clash-lib/src/CLaSH/Driver/TopWrapper.hs
@@ -28,7 +28,7 @@ import CLaSH.Netlist.BlackBox.Types   (BlackBoxTemplate)
 import CLaSH.Netlist.Types            (BlackBoxContext (..), Component (..),
                                        Declaration (..), Expr (..), Identifier,
                                        HWType (..), Modifier (..), NetlistMonad,
-                                       emptyBBContext)
+                                       PortDirection(..), emptyBBContext)
 import CLaSH.Primitives.Types         (PrimMap, Primitive (..))
 import CLaSH.Util
 
@@ -80,14 +80,14 @@ mkTopWrapper primMap teM modName iw topComponent
 
     instDecl = InstDecl (componentName topComponent)
                         (append (componentName topComponent) (pack "_inst"))
-                        (zipWith (\(p,_) i -> (p,Identifier i Nothing))
+                        (zipWith (\(p,t) i -> (p,In,t,Identifier i Nothing))
                                  (inputs topComponent)
                                  idsI
                          ++
-                         map (\(p,_) -> (p,Identifier p Nothing))
+                         map (\(p,t) -> (p,In,t,Identifier p Nothing))
                              (hiddenPorts topComponent)
                          ++
-                         zipWith (\(p,_) i -> (p,Identifier i Nothing))
+                         zipWith (\(p,t) i -> (p,Out,t,Identifier i Nothing))
                                  (outputs topComponent)
                                  idsO)
 
@@ -231,9 +231,9 @@ mkClock (ClockSource {..}) = (clkDecls ++ [lockedDecl,instDecl],(lockedName,clks
     clkDecls     = map mkClockDecl clks
     instDecl     = InstDecl c_nameT (append c_nameT "_inst")
                  $ concat [ ports
-                          , maybe [] ((:[]) . (pack *** stringToVar))
+                          , maybe [] ((:[]) . (\(i,e) -> (pack i,In,Reset "" 0,stringToVar e)))
                                   c_reset
-                          , [(pack c_lock,Identifier lockedName Nothing)]
+                          , [(pack c_lock,Out,Reset "" 0,Identifier lockedName Nothing)]
                           ]
 
 mkClockDecl :: String -> Declaration
@@ -244,10 +244,11 @@ mkClockDecl s = NetDecl (pack s) (Clock (pack name) (read rate))
 
 -- | Create a single clock path
 clockPorts :: [(String,String)] -> [(String,String)]
-           -> ([(Identifier,Expr)],[String])
-clockPorts inp outp = (ports,clks)
+           -> ([(Identifier,PortDirection,HWType,Expr)],[String])
+clockPorts inp outp = (inPorts ++ outPorts,clks)
   where
-    ports = map (pack *** stringToVar) (inp ++ outp)
+    inPorts  = map (\(i,e) -> (pack i,In,Clock "" 0,stringToVar e)) inp
+    outPorts = map (\(i,e) -> (pack i,Out,Clock "" 0,stringToVar e)) inp
     clks  = map snd outp
 
 -- | Generate resets

CLaSH.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE CPP #-}
 
 #include "MachDeps.h"
+#define HDLSYN Vivado
 
 import CLaSH.Driver
 import CLaSH.Driver.Types
@@ -13,18 +14,19 @@ import CLaSH.Backend
 import CLaSH.Backend.SystemVerilog
 import CLaSH.Backend.VHDL
 import CLaSH.Backend.Verilog
+import CLaSH.Netlist.BlackBox.Types
 
 genSystemVerilog :: String
                  -> IO ()
-genSystemVerilog = doHDL (initBackend WORD_SIZE_IN_BITS :: SystemVerilogState)
+genSystemVerilog = doHDL (initBackend WORD_SIZE_IN_BITS HDLSYN :: SystemVerilogState)
 
 genVHDL :: String
         -> IO ()
-genVHDL = doHDL (initBackend WORD_SIZE_IN_BITS :: VHDLState)
+genVHDL = doHDL (initBackend WORD_SIZE_IN_BITS HDLSYN :: VHDLState)
 
 genVerilog :: String
            -> IO ()
-genVerilog = doHDL (initBackend WORD_SIZE_IN_BITS :: VerilogState)
+genVerilog = doHDL (initBackend WORD_SIZE_IN_BITS HDLSYN :: VerilogState)
 
 doHDL :: Backend s
        => s
@@ -34,7 +36,7 @@ doHDL b src = do
   pd      <- primDir b
   primMap <- generatePrimMap [pd,"."]
   (bindingsMap,tcm,tupTcm,topEnt,testInpM,expOutM) <- generateBindings primMap src Nothing
-  generateHDL bindingsMap (Just b) primMap tcm tupTcm (ghcTypeToHWType WORD_SIZE_IN_BITS) reduceConstant topEnt testInpM expOutM (CLaSHOpts 20 20 15 DebugFinal True WORD_SIZE_IN_BITS Nothing)
+  generateHDL bindingsMap (Just b) primMap tcm tupTcm (ghcTypeToHWType WORD_SIZE_IN_BITS) reduceConstant topEnt testInpM expOutM (CLaSHOpts 20 20 15 DebugFinal True WORD_SIZE_IN_BITS Nothing HDLSYN)
 
 main :: IO ()
 main = genVHDL "./examples/FIR.hs"

clash-ghc/clash-ghc.cabal
@@ -1,5 +1,5 @@
 Name:                 clash-ghc
-Version:              0.6.10
+Version:              0.6.11
 Synopsis:             CAES Language for Synchronous Hardware
 Description:
   CλaSH (pronounced ‘clash’) is a functional hardware description language that
@@ -94,10 +94,10 @@ Executable clash
                       unbound-generics          >= 0.1 && < 0.4,
                       unordered-containers      >= 0.2.1.0,
 
-                      clash-lib                 >= 0.6.10 && < 0.7,
-                      clash-systemverilog       >= 0.6.5,
-                      clash-vhdl                >= 0.6.7,
-                      clash-verilog             >= 0.6.5,
+                      clash-lib                 >= 0.6.11 && < 0.7,
+                      clash-systemverilog       >= 0.6.6,
+                      clash-vhdl                >= 0.6.8,
+                      clash-verilog             >= 0.6.6,
                       clash-prelude             >= 0.10.6 && < 0.11,
                       ghc-typelits-extra        >= 0.1,
                       ghc-typelits-natnormalise >= 0.3

clash-ghc/src-bin/InteractiveUI.hs
@@ -114,6 +114,7 @@ import           CLaSH.Driver.Types (CLaSHOpts(..))
 import           CLaSH.GHC.Evaluator
 import           CLaSH.GHC.GenerateBindings
 import           CLaSH.GHC.NetlistTypes
+import           CLaSH.Netlist.BlackBox.Types (HdlSyn)
 import qualified CLaSH.Primitives.Util
 import           CLaSH.Util (clashLibVersion)
 import qualified Data.Version as Data.Version
@@ -1555,7 +1556,7 @@ modulesLoadedMsg ok mods = do
      liftIO $ putStrLn $ showSDocForUser dflags unqual msg
 
 makeHDL' :: CLaSH.Backend.Backend backend
-         => (Int -> backend)
+         => (Int -> HdlSyn -> backend)
          -> IORef CLaSHOpts
          -> [FilePath]
          -> InputT GHCi ()
@@ -1570,7 +1571,7 @@ makeHDL' backend opts lst = makeHDL backend opts =<< case lst of
 
 makeHDL :: GHC.GhcMonad m
         => CLaSH.Backend.Backend backend
-        => (Int -> backend)
+        => (Int -> HdlSyn -> backend)
         -> IORef CLaSHOpts
         -> [FilePath]
         -> m ()
@@ -1578,6 +1579,7 @@ makeHDL backend optsRef srcs = do
   dflags <- GHC.getSessionDynFlags
   liftIO $ do opts  <- readIORef optsRef
               let iw = opt_intWidth opts
+                  syn = opt_hdlSyn opts
                   -- determine whether `-outputdir` was used
                   outputDir = do odir <- objectDir dflags
                                  hidir <- hiDir dflags
@@ -1587,21 +1589,21 @@ makeHDL backend optsRef srcs = do
                                     then Just odir
                                     else Nothing
                   opts' = opts {opt_hdlDir = maybe outputDir Just (opt_hdlDir opts)}
-              primDir <- CLaSH.Backend.primDir (backend iw)
+              primDir <- CLaSH.Backend.primDir (backend iw syn)
               primMap <- CLaSH.Primitives.Util.generatePrimMap [primDir,"."]
               forM_ srcs $ \src -> do
                 (bindingsMap,tcm,tupTcm,topEnt,testInpM,expOutM) <- generateBindings primMap src (Just dflags)
-                CLaSH.Driver.generateHDL bindingsMap (Just (backend iw)) primMap tcm
+                CLaSH.Driver.generateHDL bindingsMap (Just (backend iw syn)) primMap tcm
                   tupTcm (ghcTypeToHWType iw) reduceConstant topEnt testInpM expOutM opts'
 
 makeVHDL :: IORef CLaSHOpts -> [FilePath] -> InputT GHCi ()
-makeVHDL = makeHDL' (CLaSH.Backend.initBackend :: Int -> VHDLState)
+makeVHDL = makeHDL' (CLaSH.Backend.initBackend :: Int -> HdlSyn -> VHDLState)
 
 makeVerilog :: IORef CLaSHOpts -> [FilePath] -> InputT GHCi ()
-makeVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> VerilogState)
+makeVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> HdlSyn -> VerilogState)
 
 makeSystemVerilog :: IORef CLaSHOpts -> [FilePath] -> InputT GHCi ()
-makeSystemVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> SystemVerilogState)
+makeSystemVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> HdlSyn -> SystemVerilogState)
 
 -----------------------------------------------------------------------------
 -- :type

clash-ghc/src-bin/Main.hs
@@ -79,6 +79,7 @@ import           CLaSH.Backend.VHDL    (VHDLState)
 import           CLaSH.Backend.Verilog (VerilogState)
 import           CLaSH.Driver.Types (CLaSHOpts (..))
 import           CLaSH.GHC.CLaSHFlags
+import           CLaSH.Netlist.BlackBox.Types (HdlSyn (..))
 import           CLaSH.Rewrite.Types (DebugLevel (..))
 import           CLaSH.Util (clashLibVersion)
 import           CLaSH.GHC.LoadModules (ghcLibDir)
@@ -125,6 +126,7 @@ main = do
                              , opt_cleanhdl    = True
                              , opt_intWidth    = WORD_SIZE_IN_BITS
                              , opt_hdlDir      = Nothing
+                             , opt_hdlSyn      = Other
                              })
     (argv3, clashFlagWarnings) <- parseCLaSHFlags r argv2
 
@@ -945,18 +947,18 @@ abiHash strs = do
 -- -----------------------------------------------------------------------------
 -- VHDL Generation
 
-makeHDL' :: CLaSH.Backend.Backend backend => (Int -> backend) ->  IORef CLaSHOpts -> [(String,Maybe Phase)] -> Ghc ()
+makeHDL' :: CLaSH.Backend.Backend backend => (Int -> HdlSyn -> backend) ->  IORef CLaSHOpts -> [(String,Maybe Phase)] -> Ghc ()
 makeHDL' _       _ []   = throwGhcException (CmdLineError "No input files")
 makeHDL' backend r srcs = makeHDL backend r $ fmap fst srcs
 
 makeVHDL :: IORef CLaSHOpts -> [(String, Maybe Phase)] -> Ghc ()
-makeVHDL = makeHDL' (CLaSH.Backend.initBackend :: Int -> VHDLState)
+makeVHDL = makeHDL' (CLaSH.Backend.initBackend :: Int -> HdlSyn -> VHDLState)
 
 makeVerilog ::  IORef CLaSHOpts -> [(String, Maybe Phase)] -> Ghc ()
-makeVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> VerilogState)
+makeVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> HdlSyn -> VerilogState)
 
 makeSystemVerilog ::  IORef CLaSHOpts -> [(String, Maybe Phase)] -> Ghc ()
-makeSystemVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> SystemVerilogState)
+makeSystemVerilog = makeHDL' (CLaSH.Backend.initBackend :: Int -> HdlSyn -> SystemVerilogState)
 
 -- -----------------------------------------------------------------------------
 -- Util

clash-ghc/src-ghc/CLaSH/GHC/CLaSHFlags.hs
@@ -43,6 +43,7 @@ flagsClash r = [
   , defFlag "clash-noclean" (NoArg (liftEwM (setNoClean r)))
   , defFlag "clash-intwidth" (IntSuffix (setIntWidth r))
   , defFlag "clash-hdldir" (SepArg (setHdlDir r))
+  , defFlag "clash-hdlsyn" (SepArg (setHdlSyn r))
   ]
 
 setInlineLimit :: IORef CLaSHOpts
@@ -82,3 +83,10 @@ setHdlDir :: IORef CLaSHOpts
           -> String
           -> EwM IO ()
 setHdlDir r s = liftEwM $ modifyIORef r (\c -> c {opt_hdlDir = Just s})
+
+setHdlSyn :: IORef CLaSHOpts
+          -> String
+          -> EwM IO ()
+setHdlSyn r s = case readMaybe s of
+  Just hdlSyn -> liftEwM $ modifyIORef r (\c -> c {opt_hdlSyn = hdlSyn})
+  Nothing     -> addWarn (s ++ " is an invalid debug level")

clash-lib/clash-lib.cabal
@@ -1,5 +1,5 @@
 Name:                 clash-lib
-Version:              0.6.10
+Version:              0.6.11
 Synopsis:             CAES Language for Synchronous Hardware - As a Library
 Description:
   CλaSH (pronounced ‘clash’) is a functional hardware description language that

clash-lib/src/CLaSH/Backend.hs
@@ -12,10 +12,11 @@ import Control.Monad.State                  (State)
 import Text.PrettyPrint.Leijen.Text.Monadic (Doc)
 
 import CLaSH.Netlist.Types
+import CLaSH.Netlist.BlackBox.Types
 
 class Backend state where
   -- | Initial state for state monad
-  initBackend :: Int -> state
+  initBackend :: Int -> HdlSyn -> state
 
   -- | Location for the primitive definitions
   primDir :: state -> IO FilePath
@@ -54,3 +55,5 @@ class Backend state where
   toBV             :: HWType -> Text -> State state Doc
   -- | Convert from a bit-vector
   fromBV           :: HWType -> Text -> State state Doc
+  -- | Synthesis tool we're generating HDL for
+  hdlSyn           :: State state HdlSyn

commit 900396fe1a26917a9c9d1aae9e01bf7f0e62b704

Fix wrapper generation
Specifically, no output ports were generated for the clock
generators, and input ports were generated twice. This bug was
introduced by: 7652df6c118b71307cebaa79f58d668129746a69

Fixes [#49](https://github.com/clash-lang/clash-prelude/issues/49)

clash-lib/src/CLaSH/Driver/TopWrapper.hs
@@ -249,7 +249,7 @@ clockPorts :: [(String,String)] -> [(String,String)]
 clockPorts inp outp = (inPorts ++ outPorts,clks)
   where
     inPorts  = map (\(i,e) -> (pack i,In,Clock "" 0,stringToVar e)) inp
-    outPorts = map (\(i,e) -> (pack i,Out,Clock "" 0,stringToVar e)) inp
+    outPorts = map (\(i,e) -> (pack i,Out,Clock "" 0,stringToVar e)) outp
     clks  = map snd outp
 
 -- | Generate resets