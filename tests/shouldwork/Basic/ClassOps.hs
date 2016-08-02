f3484a975a5570e253638233cba7a6f1fccf63eb
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-08-02 16:48:20 +0200

    Properly fix Verilog mod primitives
    See #164

--- tests/shouldwork/Basic/ClassOps.hs
+++ tests/shouldwork/Basic/ClassOps.hs
@@ -6,7 +6,7 @@ topEntity :: (Integer,Integer) -> Integer
 topEntity = uncurry mod
 
 testInput :: Signal (Integer,Integer)
-testInput = stimuliGenerator $(v [(19,4)::(Integer,Integer),(7,3),(55,-10),(9,-2),(11,10)])
+testInput = stimuliGenerator $(v [(19,4)::(Integer,Integer),(7,3),(55,-10),(9,-2),(0,-10),(11,10)])
 
 expectedOutput :: Signal Integer -> Signal Bool
-expectedOutput = outputVerifier $(v ([3::Integer,1,-5,-1,1]))
+expectedOutput = outputVerifier $(v ([3::Integer,1,-5,-1,0,1]))
