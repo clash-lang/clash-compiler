0ef579a37332d9adebae33ce36d06c18c2479874
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-09-01 17:23:04 +0200

    doctest the blockRam examples

--- src/CLaSH/Prelude/BlockRam.hs
+++ src/CLaSH/Prelude/BlockRam.hs
@@ -20,7 +20,6 @@ codes:
 module CPU where
 
 import CLaSH.Prelude
-import qualified Data.List as L
 
 type InstrAddr = Unsigned 8
 type MemAddr   = Unsigned 5
@@ -178,8 +177,9 @@ prog = -- 0 := 4
 And test our system:
 
 @
-__>>> sampleN 31 $ system prog__
+>>> sampleN 31 $ system prog
 [0,0,0,0,0,4,4,4,4,4,4,4,4,6,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2]
+
 @
 
 to see that our system indeed calculates that the GCD of 6 and 4 is 2.
@@ -208,8 +208,9 @@ we need to drop the first few output samples, because the initial content of an
 output samples are also 'undefined'.
 
 @
-__>>> L.drop 5 $ sampleN 31 $ system2 prog__
-[4,4,4,4,4,4,4,4,6,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2]
+>>> printX $ sampleN 31 $ system2 prog
+[X,X,X,X,X,4,4,4,4,4,4,4,4,6,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2]
+
 @
 
 === Improvement 2: using @blockRam@
@@ -327,8 +328,9 @@ we need to drop the first sample, because the initial output of a
 also 'undefined'.
 
 @
-__>>> L.tail $ sampleN 33 $ system3 prog2__
-[0,0,0,0,0,4,4,4,4,4,4,4,4,6,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2]
+>>> printX $ sampleN 33 $ system3 prog2
+[X,0,0,0,0,0,4,4,4,4,4,4,4,4,6,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2]
+
 @
 
 This concludes the short introduction to using 'blockRam'.
@@ -374,6 +376,248 @@ import CLaSH.Signal.Bundle    (bundle)
 import CLaSH.Sized.Unsigned   (Unsigned)
 import CLaSH.Sized.Vector     (Vec, maxIndex, toList)
 import CLaSH.XException       (XException, errorX)
+
+{- $setup
+>>> import CLaSH.Prelude as C
+>>> import qualified Data.List as L
+>>> :set -XDataKinds -XRecordWildCards
+>>> type InstrAddr = Unsigned 8
+>>> type MemAddr = Unsigned 5
+>>> type Value = Signed 8
+>>> :{
+data Reg
+  = Zero
+  | PC
+  | RegA
+  | RegB
+  | RegC
+  | RegD
+  | RegE
+  deriving (Eq,Show,Enum)
+:}
+
+>>> :{
+data Operator = Add | Sub | Incr | Imm | CmpGt
+  deriving (Eq,Show)
+:}
+
+>>> :{
+data Instruction
+  = Compute Operator Reg Reg Reg
+  | Branch Reg Value
+  | Jump Value
+  | Load MemAddr Reg
+  | Store Reg MemAddr
+  | Nop
+  deriving (Eq,Show)
+:}
+
+>>> :{
+data MachCode
+  = MachCode
+  { inputX  :: Reg
+  , inputY  :: Reg
+  , result  :: Reg
+  , aluCode :: Operator
+  , ldReg   :: Reg
+  , rdAddr  :: MemAddr
+  , wrAddr  :: MemAddr
+  , wrEn    :: Bool
+  , jmpM    :: Maybe Value
+  }
+:}
+
+>>> :{
+nullCode = MachCode { inputX = Zero, inputY = Zero, result = Zero, aluCode = Imm
+                    , ldReg = Zero, wrAddr = 0, rdAddr = 0, wrEn = False
+                    , jmpM = Nothing
+                    }
+:}
+
+>>> :{
+alu Add   x y = x + y
+alu Sub   x y = x - y
+alu Incr  x _ = x + 1
+alu Imm   x _ = x
+alu CmpGt x y = if x > y then 1 else 0
+:}
+
+>>> :{
+cpu :: Vec 7 Value          -- ^ Register bank
+    -> (Value,Instruction)  -- ^ (Memory output, Current instruction)
+    -> ( Vec 7 Value
+       , (MemAddr,MemAddr,Bool,Value,InstrAddr)
+       )
+cpu regbank (memOut,instr) = (regbank',(rdAddr,wrAddr,wrEn,aluOut,fromIntegral ipntr))
+  where
+    -- Current instruction pointer
+    ipntr = regbank C.!! PC
+    -- Decoder
+    (MachCode {..}) = case instr of
+      Compute op rx ry res -> nullCode {inputX=rx,inputY=ry,result=res,aluCode=op}
+      Branch cr a          -> nullCode {inputX=cr,jmpM=Just a}
+      Jump a               -> nullCode {aluCode=Incr,jmpM=Just a}
+      Load a r             -> nullCode {ldReg=r,rdAddr=a}
+      Store r a            -> nullCode {inputX=r,wrAddr=a,wrEn=True}
+      Nop                  -> nullCode
+    -- ALU
+    regX   = regbank C.!! inputX
+    regY   = regbank C.!! inputY
+    aluOut = alu aluCode regX regY
+    -- next instruction
+    nextPC = case jmpM of
+               Just a | aluOut /= 0 -> ipntr + a
+               _                    -> ipntr + 1
+    -- update registers
+    regbank' = replace Zero   0
+             $ replace PC     nextPC
+             $ replace result aluOut
+             $ replace ldReg  memOut
+             $ regbank
+:}
+
+>>> :{
+dataMem :: Signal MemAddr
+        -> Signal MemAddr
+        -> Signal Bool
+        -> Signal Value
+        -> Signal Value
+dataMem wr rd en din = mealy dataMemT (C.replicate d32 0) (bundle (wr,rd,en,din))
+  where
+    dataMemT mem (wr,rd,en,din) = (mem',dout)
+      where
+        dout = mem C.!! rd
+        mem' | en        = replace wr din mem
+             | otherwise = mem
+:}
+
+>>> :{
+system :: KnownNat n => Vec n Instruction -> Signal Value
+system instrs = memOut
+  where
+    memOut = dataMem wrAddr rdAddr wrEn aluOut
+    (rdAddr,wrAddr,wrEn,aluOut,ipntr) = mealyB cpu (C.replicate d7 0) (memOut,instr)
+    instr  = asyncRom instrs <$> ipntr
+:}
+
+>>> :{
+-- Compute GCD of 4 and 6
+prog = -- 0 := 4
+       Compute Incr Zero RegA RegA :>
+       C.replicate d3 (Compute Incr RegA Zero RegA) C.++
+       Store RegA 0 :>
+       -- 1 := 6
+       Compute Incr Zero RegA RegA :>
+       C.replicate d5 (Compute Incr RegA Zero RegA) C.++
+       Store RegA 1 :>
+       -- A := 4
+       Load 0 RegA :>
+       -- B := 6
+       Load 1 RegB :>
+       -- start
+       Compute CmpGt RegA RegB RegC :>
+       Branch RegC 4 :>
+       Compute CmpGt RegB RegA RegC :>
+       Branch RegC 4 :>
+       Jump 5 :>
+       -- (a > b)
+       Compute Sub RegA RegB RegA :>
+       Jump (-6) :>
+       -- (b > a)
+       Compute Sub RegB RegA RegB :>
+       Jump (-8) :>
+       -- end
+       Store RegA 2 :>
+       Load 2 RegC :>
+       Nil
+:}
+
+>>> :{
+system2 :: KnownNat n => Vec n Instruction -> Signal Value
+system2 instrs = memOut
+  where
+    memOut = asyncRam d32 wrAddr rdAddr wrEn aluOut
+    (rdAddr,wrAddr,wrEn,aluOut,ipntr) = mealyB cpu (C.replicate d7 0) (memOut,instr)
+    instr  = asyncRom instrs <$> ipntr
+:}
+
+>>> :{
+cpu2 :: (Vec 7 Value,Reg)    -- ^ (Register bank, Load reg addr)
+     -> (Value,Instruction)  -- ^ (Memory output, Current instruction)
+     -> ( (Vec 7 Value,Reg)
+        , (MemAddr,MemAddr,Bool,Value,InstrAddr)
+        )
+cpu2 (regbank,ldRegD) (memOut,instr) = ((regbank',ldRegD'),(rdAddr,wrAddr,wrEn,aluOut,fromIntegral ipntr))
+  where
+    -- Current instruction pointer
+    ipntr = regbank C.!! PC
+    -- Decoder
+    (MachCode {..}) = case instr of
+      Compute op rx ry res -> nullCode {inputX=rx,inputY=ry,result=res,aluCode=op}
+      Branch cr a          -> nullCode {inputX=cr,jmpM=Just a}
+      Jump a               -> nullCode {aluCode=Incr,jmpM=Just a}
+      Load a r             -> nullCode {ldReg=r,rdAddr=a}
+      Store r a            -> nullCode {inputX=r,wrAddr=a,wrEn=True}
+      Nop                  -> nullCode
+    -- ALU
+    regX   = regbank C.!! inputX
+    regY   = regbank C.!! inputY
+    aluOut = alu aluCode regX regY
+    -- next instruction
+    nextPC = case jmpM of
+               Just a | aluOut /= 0 -> ipntr + a
+               _                    -> ipntr + 1
+    -- update registers
+    ldRegD'  = ldReg -- Delay the ldReg by 1 cycle
+    regbank' = replace Zero   0
+             $ replace PC     nextPC
+             $ replace result aluOut
+             $ replace ldRegD memOut
+             $ regbank
+:}
+
+>>> :{
+system3 :: KnownNat n => Vec n Instruction -> Signal Value
+system3 instrs = memOut
+  where
+    memOut = blockRam (C.replicate d32 0) wrAddr rdAddr wrEn aluOut
+    (rdAddr,wrAddr,wrEn,aluOut,ipntr) = mealyB cpu2 ((C.replicate d7 0),Zero) (memOut,instr)
+    instr  = asyncRom instrs <$> ipntr
+:}
+
+>>> :{
+prog2 = -- 0 := 4
+       Compute Incr Zero RegA RegA :>
+       C.replicate d3 (Compute Incr RegA Zero RegA) C.++
+       Store RegA 0 :>
+       -- 1 := 6
+       Compute Incr Zero RegA RegA :>
+       C.replicate d5 (Compute Incr RegA Zero RegA) C.++
+       Store RegA 1 :>
+       -- A := 4
+       Load 0 RegA :>
+       -- B := 6
+       Load 1 RegB :>
+       Nop :> -- Extra NOP
+       -- start
+       Compute CmpGt RegA RegB RegC :>
+       Branch RegC 4 :>
+       Compute CmpGt RegB RegA RegC :>
+       Branch RegC 4 :>
+       Jump 5 :>
+       -- (a > b)
+       Compute Sub RegA RegB RegA :>
+       Jump (-6) :>
+       -- (b > a)
+       Compute Sub RegB RegA RegB :>
+       Jump (-8) :>
+       -- end
+       Store RegA 2 :>
+       Load 2 RegC :>
+       Nil
+:}
+
+-}
 
 {-# INLINE blockRam #-}
 -- | Create a blockRAM with space for @n@ elements.
