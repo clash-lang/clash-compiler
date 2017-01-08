c0c45d305d5c7aeae36e19c493b98c083858b336
Christiaan Baaij <christiaan.baaij@gmail.com>
2017-01-08 15:39:57 +0000

    Add missing DivMod test

---
+++ tests/shouldwork/Basic/DivMod.hs
@@ -0,0 +1,25 @@
+module DivMod where
+
+import CLaSH.Prelude
+
+topEntity :: (Integer,Integer)
+topEntity = topEntity1 height depthInput filterHeight stride cycles
+  where
+    height = SNat @5
+    filterHeight = SNat @3
+    stride = SNat @2
+    depthInput = SNat @2
+    cycles = SNat @800
+
+topEntity1 height depthInput filterHeight stride cycles = snatToNum cycles `divMod` pools
+  where
+    pools :: Integer
+    pools =
+      rows *
+      (((snatToNum height - snatToNum filterHeight) `div` snatToNum stride) + 1) *
+      snatToNum depthInput
+
+    rows :: Integer
+    rows = ((snatToNum height - snatToNum filterHeight) `div` snatToNum stride) + 1
+{-# NOINLINE topEntity1 #-}
+
