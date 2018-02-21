6121c8898bd9b946e75aa4ee123c7dbfbf85d21f
Martijn Bastiaan <martijn@qbaylogic.com>
2018-02-21 15:25:03 +0100

    Added `{-# ANN ... #-}` notation for custom bit representations

--- src/Clash/Annotations/BitRepresentation.hs
+++ src/Clash/Annotations/BitRepresentation.hs
@@ -15,7 +15,9 @@ data types.
 
 module Clash.Annotations.BitRepresentation
  ( DataRepr(..)
+ , DataReprAnn(..)
  , ConstrRepr(..)
+ , TypeName(..)
  ) where
 
 import qualified Language.Haskell.TH.Syntax as TH
@@ -29,14 +31,61 @@ type Size     = Word
 
 type FieldAnn = BitMask
 
--- | Type annotation (Data)
+data TypeName = TN TH.Name [TypeName]
+              -- ^ Type name with a number of types as arguments
+              | TT TH.Name
+              -- ^ Type name terminal; equivalent to TN with an empty list
+                 deriving (Show, Data, Typeable)
+
+-- | Type annotation for inline annotations. Example usage:
+--
+-- @
+-- data Color = R | G | B
+-- {-# ANN module (DataReprAnn (TT ''Color) 2 [...]) #-}
+-- @
+--
+-- To annotate composed types, use TN. For example, if we want to annotate
+-- `Maybe Color`:
+--
+-- @
+-- {-# ANN module ( DataReprAnn
+--                    (TN ''Maybe [TT ''Color])
+--                    2
+--                    [...] ) #-}
+-- @
+data DataReprAnn =
+  DataReprAnn
+    TypeName
+    -- ^ Type this annotation is for
+    Size
+    -- ^ Size of type
+    [ConstrRepr]
+    -- ^ Constructors
+      deriving (Show, Data, Typeable)
+
+-- | Type annotation for annotations specified in a separate file, interpreted
+-- by Clash using '-fclash-custom-reprs <path>'.
+--
+-- @
+-- data Color = R | G | B
+-- colorAnn = DataRepr 2 [...] :: DataRepr Color
+-- @
+--
+-- To annotate composed types, simply extend /colorAnn/s type. For example, if
+-- we want to annotate `Maybe Color`:
+--
+-- @
+-- data Color = R | G | B
+-- colorAnn = DataRepr 2 [...] :: DataRepr (Maybe Color)
+-- @
+
 data DataRepr a =
   DataRepr
     Size
     -- ^ Size of type
     [ConstrRepr]
     -- ^ Constructors
-      deriving (Data, Typeable)
+      deriving (Show, Data, Typeable)
 
 -- | Constructor annotation.
 data ConstrRepr =
@@ -49,6 +98,6 @@ data ConstrRepr =
     -- ^ data & mask should be equal to..
     [FieldAnn]
     -- ^ Masks for fields. Indicates where fields are stored.
-      deriving (Data, Typeable)
+      deriving (Show, Data, Typeable)
 
 
