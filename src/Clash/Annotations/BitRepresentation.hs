5fcfbf5b2614b1917dde08695245f61b4839a055
Martijn Bastiaan <martijn@qbaylogic.com>
2018-03-05 09:28:04 +0100

    Move DataRepr' from clash-lib to clash-prelude, add bit representation derivation for parameterized types

--- src/Clash/Annotations/BitRepresentation.hs
+++ src/Clash/Annotations/BitRepresentation.hs
@@ -18,16 +18,33 @@ data types.
 
 module Clash.Annotations.BitRepresentation
  ( DataRepr(..)
+ , DataRepr'(..)
+ , ConstrRepr'(..)
  , DataReprAnn(..)
  , ConstrRepr(..)
+ , CustomReprs
+ , Type'(..)
+ , BitMask
+ , Value
+ , Size
  , reprType
+ , buildCustomReprs
+ , dataReprAnnToDataRepr'
+ , getConstrRepr
+ , getDataRepr
  ) where
 
+import GHC.Generics (Generic)
+import Control.DeepSeq (NFData)
+import Data.Typeable (Typeable)
+import Data.Hashable (Hashable)
+import Data.Data (Data)
+
+import qualified Data.Map as Map
+import qualified Data.Text.Lazy as Text
 import qualified Language.Haskell.TH.Lib as TH
 import qualified Language.Haskell.TH.Syntax as TH
-
-import Data.Data (Data)
-import Data.Typeable (Typeable)
+import qualified Language.Haskell.TH.Lift ()
 
 type BitMask  = Integer
 type Value    = Integer
@@ -39,25 +56,27 @@ reprType :: TH.TypeQ -> TH.ExpQ
 reprType qty = qty >>= TH.lift
 
 deriving instance TH.Lift TH.Type
-
--- NOTE: Don't import these from Language.Haskell.TH.Instances
---       Doing so will also import `instance Lift Exp`
---       And that changes certain TH mistakes from compile to run time errors.
 deriving instance TH.Lift TH.TyVarBndr
 deriving instance TH.Lift TH.TyLit
-deriving instance TH.Lift TH.Name
-deriving instance TH.Lift TH.OccName
-deriving instance TH.Lift TH.NameFlavour
-deriving instance TH.Lift TH.ModName
-deriving instance TH.Lift TH.NameSpace
-deriving instance TH.Lift TH.PkgName
+
+-- NOTE: The following instances are imported from Language.Haskell.TH.Instances.
+-- This module also implements 'instance Lift Exp', which might make debugging
+-- template haskell more difficult. Please uncomment these instnaces and the
+-- import of TH.Instances whenever it suits you.
+--
+--deriving instance TH.Lift TH.Name
+--deriving instance TH.Lift TH.OccName
+--deriving instance TH.Lift TH.NameFlavour
+--deriving instance TH.Lift TH.ModName
+--deriving instance TH.Lift TH.NameSpace
+--deriving instance TH.Lift TH.PkgName
 
 
 -- | Type annotation for inline annotations. Example usage:
 --
 -- @
 -- data Color = R | G | B
--- {-# ANN module (DataReprAnn $(typeOf [t|Color|]) 2 [...]) #-}
+-- {-# ANN module (DataReprAnn $(reprType [t|Color|]) 2 [...]) #-}
 -- @
 --
 -- Or if we want to annotate
@@ -65,7 +84,7 @@ deriving instance TH.Lift TH.PkgName
 --
 -- @
 -- {-# ANN module ( DataReprAnn
---                    $(typeOf [t|Maybe Color|])
+--                    $(reprType [t|Maybe Color|])
 --                    2
 --                    [...] ) #-}
 -- @
@@ -115,3 +134,76 @@ data ConstrRepr =
     [FieldAnn]
     -- ^ Masks for fields. Indicates where fields are stored.
       deriving (Show, Data, Typeable)
+
+
+-- |
+data Type' = AppTy' Type' Type'
+           | ConstTy' Text.Text
+           | LitTy' Integer
+    deriving (Generic, NFData, Eq, Typeable, Hashable, Ord, Show)
+
+-- |
+data DataRepr' =
+  DataRepr'
+    Type'
+    -- ^ Qualified name of type (recursive)
+    Size
+    -- ^ Size of data type
+    [ConstrRepr']
+    -- ^ Constructors
+      deriving (Show, Generic, NFData, Eq, Typeable, Hashable, Ord)
+
+-- |
+data ConstrRepr' =
+  ConstrRepr'
+    Text.Text
+    -- ^ Qualified name of constructor
+    Int
+    -- ^ Syntactical position in the custom representations definition.
+    BitMask
+    -- ^ Mask needed to determine constructor
+    Value
+    -- ^ Value after applying mask
+    [FieldAnn]
+    --
+      deriving (Show, Generic, NFData, Eq, Typeable, Ord, Hashable)
+
+dataReprAnnToDataRepr' :: DataReprAnn -> DataRepr'
+dataReprAnnToDataRepr' (DataReprAnn typ size constrs) =
+  DataRepr' (toType' typ) size (zipWith toConstrRepr' [0..] constrs)
+    where
+      toConstrRepr' :: Int -> ConstrRepr -> ConstrRepr'
+      toConstrRepr' n (ConstrRepr name mask value fieldanns) =
+        ConstrRepr' (thToText name) n (fromIntegral mask) value (map fromIntegral fieldanns)
+
+      thToText :: TH.Name -> Text.Text
+      thToText (TH.Name (TH.OccName name') (TH.NameG _namespace _pkgName (TH.ModName modName))) =
+        Text.pack $ modName ++ "." ++ name'
+      thToText name' = error $ {-$(curLoc) ++-} "Unexpected pattern: " ++ show name'
+
+      toType' :: TH.Type -> Type'
+      toType' ty = go ty
+        where
+          go (TH.ConT name')   = ConstTy' (thToText name')
+          go (TH.AppT ty1 ty2) = AppTy' (go ty1) (go ty2)
+          go (TH.LitT (TH.NumTyLit n)) = LitTy' n
+          go _ = error $ {-$(curLoc) ++-} "Unsupported type: " ++ show ty
+
+
+type CustomReprs = ( Map.Map Type' DataRepr'
+                   , Map.Map Text.Text ConstrRepr'
+                   )
+
+getDataRepr :: Type' -> CustomReprs -> Maybe DataRepr'
+getDataRepr name (reprs, _) = Map.lookup name reprs
+
+getConstrRepr :: Text.Text -> CustomReprs -> Maybe ConstrRepr'
+getConstrRepr name (_, reprs) = Map.lookup name reprs
+
+buildCustomRepr :: DataRepr' -> CustomReprs -> CustomReprs
+buildCustomRepr d@(DataRepr' name _size constrReprs) (dMap, cMap) =
+  let insertConstr c@(ConstrRepr' name' _ _ _ _) cMap' = Map.insert name' c cMap' in
+  (Map.insert name d dMap, foldr insertConstr cMap constrReprs)
+
+buildCustomReprs :: [DataRepr'] -> CustomReprs
+buildCustomReprs = foldr buildCustomRepr (Map.empty, Map.empty)
---
+++ src/Clash/Annotations/BitRepresentation/Util.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE TemplateHaskell   #-}
+module Clash.Annotations.BitRepresentation.Util
+  ( bitOrigins
+  , bitRanges
+  , isContinuousMask
+  , BitOrigin(..)
+  , Bit(..)
+  ) where
+
+
+import Data.Tuple (swap)
+import Data.List  (findIndex, group, mapAccumL)
+import Data.Bits  (Bits, testBit, testBit, shiftR)
+
+data Bit = H
+         | L
+         | U
+          deriving (Show,Eq)
+
+-- |
+data BitOrigin
+  = Lit [Bit]
+  | Field
+      Int
+      -- ^ Field number
+      Int
+      -- ^ Start bit (from..)
+      Int
+      -- ^ End bit (inclusive, ..downto)
+        deriving (Show)
+
+-- | Given a type size and one of its constructor this function will yield a
+-- specification of which bits the whole type is made up of. I.e., a
+-- construction plan on how to make the whole data structure, given its
+-- individual constructor fields.
+bitOrigins
+  :: Int
+  -> (Integer, Integer, [Integer])
+  -- ^ (mask, value, fields) from CustomRepr or CustomRepr'
+  -> [BitOrigin]
+bitOrigins size cRepr = mergeOrigins origins
+    where
+      origins = map (bitOrigin cRepr) (reverse [0..fromIntegral $ size - 1])
+
+      -- | Merge consequtive Constructor and Field fields (if applicable).
+      mergeOrigins :: [BitOrigin] -> [BitOrigin]
+      mergeOrigins (Lit n : Lit n' : fs) =
+        -- Literals can always be merged:
+        mergeOrigins $ Lit (n ++ n') : fs
+      mergeOrigins (Field n s e : Field n' s' e' : fs)
+        -- Consequtive fields with same field number merged:
+        | n == n'   = mergeOrigins $ Field n s e' : fs
+        -- No merge:
+        | otherwise = Field n s e : mergeOrigins (Field n' s' e' : fs)
+      -- Base cases:
+      mergeOrigins (x:fs) = x : mergeOrigins fs
+      mergeOrigins []     = []
+
+      -- | Determine origin of single bit
+      bitOrigin :: (Integer, Integer, [Integer]) -> Int -> BitOrigin
+      bitOrigin (mask, value, fields) n =
+        if testBit mask n then
+          Lit [if testBit value n then H else L]
+        else
+          case findIndex (\fmask -> testBit fmask n) fields of
+            Nothing ->
+              Lit [U]
+            Just fieldn ->
+              let fieldbitn = length $ filter id
+                                     $ take n
+                                     $ bitsToBools (fields !! fieldn) in
+              Field fieldn fieldbitn fieldbitn
+
+-- | Convert a number to a list of its bits
+-- Output is ordered from least to most significant bit.
+-- Only outputs bits until the highest set bit.
+--
+-- >>> map bitsToBools [0..2]
+-- [[],[True],[False,True]])
+--
+-- This also works for variable sized number like Integer.
+-- But not for negative numbers, because negative Integers have infinite bits set.
+bitsToBools :: (Num a, Bits a, Ord a) => a -> [Bool]
+bitsToBools 0 = []
+bitsToBools n | n < 0 = error "Can't deal with negative bitmasks/values"
+              | otherwise = testBit n 0 : bitsToBools (n `shiftR` 1)
+
+
+offsets
+  :: Int
+  -- ^ Offset
+  -> [Bool]
+  -- ^ Group
+  -> (Int, (Int, [Bool]))
+offsets offset group' =
+  (length group' + offset, (offset, group'))
+
+-- | Determine consecutively set bits in word. Will produce ranges from high
+-- to low. Examples:
+--
+--   bitRanges 0b10          == [(1,1)]
+--   bitRanges 0b101         == [(2,2),(0,0)]
+--   bitRanges 0b10011001111 == [(10,10),(7,6),(3,0)]
+--
+bitRanges :: Integer -> [(Int, Int)]
+bitRanges word = reverse $ map swap ranges
+  where
+    ranges  = map (\(ofs, grp) -> (ofs, ofs+length grp-1)) groups'
+    groups' = filter (head . snd) groups
+    groups  = snd $ mapAccumL offsets 0 (group bits)
+    bits    = bitsToBools word
+
+isContinuousMask :: Integer -> Bool
+isContinuousMask word =
+  -- Use case expression so we avoid calculating all groups
+  case bitRanges word of
+    -- At least two groups:
+    (_:_:_) -> False
+    -- Zero or one group:
+    _       -> True
