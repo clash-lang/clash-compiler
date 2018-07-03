1dc0dd6bda49a638de10dc96c110c5bdc2dbbd8b
Leon Schoorl <leon@qbaylogic.com>
2018-07-03 16:59:14 +0200

    Warn about problematic primitive definitions
    Warns when a primitive:
      * isn't marked NOINLINE
      * produces an error when evaluating its result to WHNF
      * isn't using all its arguments
    
    This should help with problems like #320.

--- clash-ghc/src-ghc/Clash/GHC/GenerateBindings.hs
+++ clash-ghc/src-ghc/Clash/GHC/GenerateBindings.hs
@@ -15,19 +15,26 @@ module Clash.GHC.GenerateBindings
 where
 
 import           Control.Lens            ((%~),(&),view,_1)
+import           Control.Monad           (unless, when)
 import           Control.Monad.State     (State)
 import qualified Control.Monad.State     as State
 import           Data.Coerce             (coerce)
 import           Data.Either             (lefts, rights)
 import           Data.IntMap.Strict      (IntMap)
 import qualified Data.IntMap.Strict      as IMS
+import qualified Data.HashMap.Strict     as HashMap
+import           Data.List               (isPrefixOf)
+import qualified Data.Text               as Text
 
 import qualified BasicTypes              as GHC
 import qualified CoreSyn                 as GHC
+import qualified Demand                  as GHC
 import qualified DynFlags                as GHC
 import qualified IdInfo                  as GHC
+import qualified Outputable              as GHC
 import qualified Name                    as GHC hiding (varName)
 import qualified TyCon                   as GHC
+import qualified Type                    as GHC
 import qualified TysWiredIn              as GHC
 import qualified Var                     as GHC
 import qualified SrcLoc                  as GHC
@@ -53,7 +60,7 @@ import           Clash.Primitives.Util   (generatePrimMap)
 import           Clash.Rewrite.Util      (mkInternalVar, mkSelectorCase)
 import           Clash.Unique
   (listToUniqMap, lookupUniqMap, mapUniqMap, unionUniqMap, uniqMapToUniqSet)
-import           Clash.Util              ((***),first)
+import           Clash.Util              ((***),first,traceIf)
 
 generateBindings
   :: [FilePath]
@@ -117,12 +124,14 @@ mkBindings primMap bindings clsOps unlocatable = do
                                 inl = GHC.inlinePragmaSpec . GHC.inlinePragInfo $ GHC.idInfo v
                             tm <- coreToTerm primMap unlocatable sp e
                             v' <- coreToId v
+                            checkPrimitive primMap v
                             return [(v', (v', sp, inl, tm))]
                           GHC.Rec bs -> do
                             tms <- mapM (\(v,e) -> do
                                           let sp = GHC.getSrcSpan v
                                           tm <- coreToTerm primMap unlocatable sp e
                                           v' <- coreToId v
+                                          checkPrimitive primMap v
                                           return (v',sp,tm)
                                         ) bs
                             case tms of
@@ -135,6 +144,47 @@ mkBindings primMap bindings clsOps unlocatable = do
                        ) clsOps
 
   return (mkVarEnv (concat bindingsList), mkVarEnv clsOpList)
+
+-- | If this CoreBndr is a primitive, check it's Haskell definition
+--   for potential problems.
+--
+-- Warns when a primitive:
+--   * isn't marked NOINLINE
+--   * produces an error when evaluating its result to WHNF
+--   * isn't using all its arguments
+checkPrimitive :: PrimMap a -> GHC.CoreBndr -> State GHC2CoreState ()
+checkPrimitive primMap v = do
+  name' <- qualfiedNameString (GHC.varName v)
+  when (name' `HashMap.member` primMap) $ do
+    let
+      info = GHC.idInfo v
+      inline = GHC.inlinePragmaSpec $ GHC.inlinePragInfo info
+      strictness = GHC.strictnessInfo info
+      ty = GHC.varType v
+      (argTys,_resTy) = GHC.splitFunTys . snd . GHC.splitForAllTys $ ty
+      (dmdArgs,_dmdRes) = GHC.splitStrictSig strictness
+      nrOfArgs = length argTys
+      loc = case GHC.getSrcLoc v of
+              GHC.UnhelpfulLoc _ -> ""
+              GHC.RealSrcLoc l   -> showPpr l ++ ": "
+      warnIf cond msg = traceIf cond ("\n"++loc++"Warning: "++msg) return ()
+    qName <- Text.unpack <$> qualfiedNameString (GHC.varName v)
+    let primStr = "primitive " ++ qName ++ " "
+    unless (qName == "Clash.XException.errorX" || "GHC." `isPrefixOf` qName) $ do
+      warnIf (inline /= GHC.NoInline)
+        (primStr ++ "isn't marked NOINLINE."
+        ++ "\nThis might make Clash ignore this primitive.")
+      warnIf (GHC.appIsBottom strictness nrOfArgs)
+        (primStr
+        ++ "produces a result that can't be evaluated to WHNF, "
+        ++ "because it results in an error."
+        ++ "\nThis might make Clash ignore this primitive.")
+      warnIf (any GHC.isAbsDmd dmdArgs)
+        (primStr
+        ++ "isn't using all its arguments, some might be optimized away.")
+  where
+    showPpr :: GHC.Outputable a => a -> String
+    showPpr = GHC.showSDocUnsafe . GHC.ppr
 
 mkClassSelector
   :: InScopeSet
