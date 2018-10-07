4a838c45eda664c7e7b3add9f8a0a76d41b95dbf
Christiaan Baaij <christiaan.baaij@gmail.com>
2018-10-07 17:42:37 +0200

    Travis: precache dependencies

---
+++ .travis/script.sh
@@ -0,0 +1,26 @@
+set -x
+# test that source-distributions can be generated
+(cd "./clash-lib" && cabal sdist && cd "..")
+mv "."/clash-lib/dist/clash-lib-*.tar.gz ${DISTDIR}/
+(cd "./clash-ghc" && cabal sdist && cd "..")
+mv "."/clash-ghc/dist/clash-ghc-*.tar.gz ${DISTDIR}/
+find ${DISTDIR} -maxdepth 1 -name '*.tar.gz' -exec tar -xvf '{}' -C ${DISTDIR} \;
+printf 'packages: %b/clash-lib-*/*.cabal %b/clash-ghc-*/*.cabal clash-prelude/*.cabal clash-cosim/*.cabal testsuite/*.cabal benchmark/*.cabal benchmark/profiling/*/*.cabal\npackage clash-testsuite\n  flags: travisci cosim\n\npackage clash-ghc\n  executable-dynamic: True\n' ${DISTDIR} ${DISTDIR} > cabal.project
+cat cabal.project
+# this builds all libraries and executables (without tests/benchmarks)
+cabal new-build -w ${HC} --disable-tests --disable-benchmarks all
+
+# Build with installed constraints for packages in global-db
+if $INSTALLED; then echo cabal new-build -w ${HC} --disable-tests --disable-benchmarks $(${HCPKG} list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all | sh; else echo "Not building with installed constraints"; fi
+
+# build & run tests, build benchmarks
+cabal new-build -w ${HC} ${TEST} ${BENCH} all
+if [ "x$TEST" = "x--enable-tests" ]; then cabal new-run -w ${HC} -- clash-testsuite -j16; fi
+
+# cabal check
+(cd ${DISTDIR}/clash-lib-* && cabal check)
+(cd ${DISTDIR}/clash-ghc-* && cabal check)
+
+# haddock
+cd ${SRCDIR}
+if $HADDOCK; then cabal new-haddock -w ${HC} ${TEST} ${BENCH} all; else echo "Skipping haddock generation";fi
