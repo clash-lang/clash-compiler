9e7c2748689da3bfcc45328258b5419040e25156
Martijn Bastiaan <martijn@hmbastiaan.nl>
2018-10-23 18:43:30 +0200

    Remove `-w` flag from Travis scripts

--- .travis/script.sh
+++ .travis/script.sh
@@ -9,10 +9,10 @@ find ${DISTDIR} -maxdepth 1 -name '*.tar.gz' -exec tar -xvf '{}' -C ${DISTDIR} \
 sed -i 's/flags: cosim/flags: travisci cosim/g' cabal.project
 cat cabal.project
 # this builds all libraries and executables 
-cabal new-build -w ${HC} all
+cabal new-build all
 
 # Build with installed constraints for packages in global-db
-if $INSTALLED; then echo cabal new-build -w ${HC} $(${HCPKG} list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all | sh; else echo "Not building with installed constraints"; fi
+if $INSTALLED; then echo cabal new-build $(${HCPKG} list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all | sh; else echo "Not building with installed constraints"; fi
 
 # cabal check
 (cd ${DISTDIR}/clash-lib-* && cabal check)
@@ -20,6 +20,6 @@ if $INSTALLED; then echo cabal new-build -w ${HC} $(${HCPKG} list --global --sim
 
 # haddock
 cd ${SRCDIR}
-if $HADDOCK; then cabal new-haddock -w ${HC} ${TEST} ${BENCH} all; else echo "Skipping haddock generation";fi
+if $HADDOCK; then cabal new-haddock ${TEST} ${BENCH} all; else echo "Skipping haddock generation";fi
 set +e
 set +x
