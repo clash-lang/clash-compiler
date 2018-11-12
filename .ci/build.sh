cec098354a6ab46915d7672547cee8a5725dc678
Leon Schoorl <leon@qbaylogic.com>
2018-11-12 16:07:50 +0100

    Move CI testing to GitLab

---
+++ .ci/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -xeo pipefail
+
+# run new-update first to generate the cabal config file that we can then modify
+cabal new-update
+
+echo "store-dir: ${PWD}/cabal-store" >> ${HOME}/.cabal/config
+
+cabal new-build all
+
+# Build with installed constraints for packages in global-db
+echo cabal new-build $(ghc-pkg list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all | sh
