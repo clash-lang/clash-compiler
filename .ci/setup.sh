37ddfda804799e6d1e6228d8881bbaa27c96ad89
Leon Schoorl <leon@qbaylogic.com>
2018-11-13 20:47:02 +0100

    Disable Haddock and doctests on ghc-8.6.2

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -5,3 +5,9 @@ apt-get install -yq cabal-install-head $GHC
 cabal --version
 ghc --version
 cp .ci/cabal.project.local .
+
+# see https://github.com/haskell/haddock/issues/900
+if [ "$GHC" == ghc-8.6.2 ]; then
+echo "Haddock and doctests are broken on GHC 8.6, disabling"
+cp .ci/cabal.project.local-8.6 cabal.project.local
+fi
