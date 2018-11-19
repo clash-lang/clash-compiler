8119aee033816db0b7f920aa673c0fa114cd06f7
Leon Schoorl <leon@qbaylogic.com>
2018-11-19 17:00:50 +0100

    Fail fast on HEAD because of singletons

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -6,8 +6,15 @@ cabal --version
 ghc --version
 cp .ci/cabal.project.local .
 
-# see https://github.com/haskell/haddock/issues/900
-if [ "$GHC" == ghc-8.6.2 ]; then
-echo "Haddock and doctests are broken on GHC 8.6, disabling"
-cp .ci/cabal.project.local-8.6 cabal.project.local
-fi
+case "$GHC" in
+  ghc-8.6* )
+    # see https://github.com/haskell/haddock/issues/900
+    echo "Haddock and doctests are broken on GHC 8.6, disabling"
+    cp .ci/cabal.project.local-8.6 cabal.project.local
+    ;;
+  ghc-head )
+    # singletons is broken on head: https://github.com/goldfirere/singletons/issues/357
+    # This constraint on a future version makes the ghc-head build fail fast
+    echo 'constraints: singletons > 2.5.1' >> cabal.project.local
+  ;;
+esac
