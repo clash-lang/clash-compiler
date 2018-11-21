2d38fcd7b763a4e17c6b4584d6741f4323220f3b
Leon Schoorl <leon@qbaylogic.com>
2018-11-21 16:08:49 +0100

    Docker image: hardcode trusted gpg keys of PPAs
    Also add cache-buster variable to automatically re-fetch the ghc packages

--- .ci/docker/build-and-publish-docker-image.sh
+++ .ci/docker/build-and-publish-docker-image.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xeo pipefail
 DIR=`dirname "$0"`
-docker build -t clash-ci-image "$DIR"
+now=$(date)
+docker build -t clash-ci-image --build-arg "GHC_FETCH_DATE=$now" "$DIR"
+echo Press enter to upload image to dockerhub...
+read
 docker tag clash-ci-image leonschoorl/clash-ci-image:trusty
 docker push leonschoorl/clash-ci-image:trusty
