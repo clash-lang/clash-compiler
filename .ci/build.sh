2cf2d2af46b8e289aa76ee57d508f279599225b9
Leon Schoorl <leon@qbaylogic.com>
2018-11-23 15:23:22 +0100

    CI: Detect when haddock generation fails

--- .ci/build.sh
+++ .ci/build.sh
@@ -14,3 +14,17 @@ cabal new-build all -j$THREADS
 
 # Build with installed constraints for packages in global-db
 echo cabal new-build $(ghc-pkg list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all -j$THREADS | sh
+
+# Check that documentation was generated succesfully
+if [ "$GHC" = "ghc-8.6.2" ]; then
+  haddock_pkgs="clash-lib clash-cosim"
+else
+  haddock_pkgs="clash-lib clash-cosim clash-prelude"
+fi
+
+for pkg in ${haddock_pkgs}; do
+  if [ ! -e dist-newstyle/build/*/ghc-*/${pkg}-*/doc/html/${pkg}/index.html ]; then
+    echo "Haddock generation failed for package ${pkg}"
+    exit 1
+  fi
+done
