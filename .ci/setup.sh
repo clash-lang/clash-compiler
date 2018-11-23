ab260a6a0a1ff1247a1719f9c2cd2b7fa7eb9f31
Leon Schoorl <leon@qbaylogic.com>
2018-11-23 17:43:13 +0100

    CI: Disable GHC HEAD testing everywhere

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -19,9 +19,4 @@ case "$GHC" in
     echo "Haddock and doctests are broken on GHC 8.6, disabling"
     cp .ci/cabal.project.local-8.6 cabal.project.local
     ;;
-  ghc-head )
-    # singletons is broken on head: https://github.com/goldfirere/singletons/issues/357
-    # This constraint on a future version makes the ghc-head build fail fast
-    echo 'constraints: singletons > 2.5.1' >> cabal.project.local
-  ;;
 esac
