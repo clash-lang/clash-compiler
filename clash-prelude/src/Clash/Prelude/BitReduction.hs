76553cce8bccaf741d79bc0ce73cd828455a6b82
Martijn Bastiaan <martijn@hmbastiaan.nl>
2018-11-27 11:49:29 +0000

    Extend documentation of `reduceAnd`, `reduceOr`, and `reduceXor`

--- clash-prelude/src/Clash/Prelude/BitReduction.hs
+++ clash-prelude/src/Clash/Prelude/BitReduction.hs
@@ -34,6 +34,11 @@ import Clash.Sized.Internal.BitVector (Bit, reduceAnd#, reduceOr#, reduceXor#)
 -- 11_1111
 -- >>> reduceAnd (-1 :: Signed 6)
 -- 1
+--
+-- Zero width types will evaluate to '1':
+--
+-- >>> reduceAnd (0 :: Unsigned 0)
+-- 1
 reduceAnd :: (BitPack a, KnownNat (BitSize a)) => a -> Bit
 reduceAnd v = reduceAnd# (pack v)
 
@@ -47,6 +52,11 @@ reduceAnd v = reduceAnd# (pack v)
 -- >>> pack (0 :: Signed 6)
 -- 00_0000
 -- >>> reduceOr (0 :: Signed 6)
+-- 0
+--
+-- Zero width types will evaluate to '0':
+--
+-- >>> reduceOr (0 :: Unsigned 0)
 -- 0
 reduceOr :: (BitPack a, KnownNat (BitSize a)) => a -> Bit
 reduceOr v = reduceOr# (pack v)
@@ -66,5 +76,10 @@ reduceOr v = reduceOr# (pack v)
 -- 11_1011
 -- >>> reduceXor (-5 :: Signed 6)
 -- 1
+--
+-- Zero width types will evaluate to '0':
+--
+-- >>> reduceXor (0 :: Unsigned 0)
+-- 0
 reduceXor :: (BitPack a, KnownNat (BitSize a)) => a -> Bit
 reduceXor v = reduceXor# (pack v)
