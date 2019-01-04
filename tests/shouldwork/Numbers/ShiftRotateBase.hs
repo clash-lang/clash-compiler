befa0071619909cf3850c82cf9cfb8d9c4026f70
Leon Schoorl <leon@qbaylogic.com>
2019-01-04 16:36:07 +0100

    Add test for shift and rotate functions

---
+++ tests/shouldwork/Numbers/ShiftRotateBase.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+module ShiftRotateBase where
+import Clash.Prelude
+import Clash.Explicit.Testbench
+import Data.Word
+import Data.Int
+
+testpattern = 0b1010011000
+
+-- test with shift/rotate amounts: 0..16,200
+amounts :: Vec _ Int
+amounts = $(listToVecTH [0::Int ..16]) :< 200
+
+testall v i
+  = ( testAs @(Unsigned 8)  v i
+    , testAs @(Signed 8)    v i
+    , testAs @(BitVector 8) v i
+    , testAs @(Word8)       v i
+    , testAs @(Int8)        v i
+    , testAs @(Unsigned 70) v i
+    )
+{-# NOINLINE testall #-}
+
+testAs
+  :: (Num b, Bits b) => Integer -> Int -> Vec Ops b
+testAs v i = map (\f -> f (fromInteger v) i) shiftsAndRots
+
+type Ops = 4
+
+shiftsAndRots :: Bits a => Vec Ops (a -> Int -> a)
+shiftsAndRots = shiftL :> shiftR :> rotateL :> rotateR :> Nil
--- testsuite/Main.hs
+++ testsuite/Main.hs
@@ -162,6 +162,7 @@ runClashTest =
         , runTest ("tests" </> "shouldwork" </> "Numbers") defBuild [] "Resize"       (["","Resize_testBench"],"Resize_testBench",True)
         , runTest ("tests" </> "shouldwork" </> "Numbers") defBuild [] "Resize2"      (["","Resize2_testBench"],"Resize2_testBench",True)
         , runTest ("tests" </> "shouldwork" </> "Numbers") defBuild [] "SatMult"      ([""],"SatMult_topEntity",False)
+        , runTest ("tests" </> "shouldwork" </> "Numbers") defBuild ["-itests/shouldwork/Numbers"] "ShiftRotate"         (["","ShiftRotate_testBench"],"ShiftRotate_testBench",True)
         , runTest ("tests" </> "shouldwork" </> "Numbers") defBuild [] "Strict"       (["","Strict_testBench"],"Strict_testBench",True)
         , runTest ("tests" </> "shouldwork" </> "Numbers") defBuild [] "UnsignedZero" (["","UnsignedZero_testBench"],"UnsignedZero_testBench",True)
         , runTest ("tests" </> "shouldwork" </> "Numbers") defBuild [] "SignedZero"   (["","SignedZero_testBench"],"SignedZero_testBench",True)
