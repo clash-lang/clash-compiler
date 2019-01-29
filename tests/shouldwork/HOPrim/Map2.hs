da97c9787eb2c554a275c411786db5c65a3f7994
Martijn Bastiaan <martijn@hmbastiaan.nl>
2019-01-29 10:50:37 +0000

    Fix `mkFunInput` for cases where func in HO returns `Data`/`Literal`/..

---
+++ tests/shouldwork/HOPrim/Map2.hs
@@ -0,0 +1,26 @@
+module Map2 where
+
+import Clash.Prelude
+import Clash.Explicit.Testbench
+
+go
+  :: Signal System ()
+  -> Signal System Bool
+go _ = pure True
+
+topEntity
+  :: Signal System (Vec 4 ())
+  -> Signal System (Vec 4 Bool)
+topEntity =
+  bundle . fmap go . unbundle
+{-# NOINLINE topEntity #-}
+
+
+testBench :: SystemClockReset => Signal System Bool
+testBench = done
+  where
+    testInput      = stimuliGenerator clk rst ((repeat ()) :> Nil)
+    expectedOutput = outputVerifier clk rst ((repeat True):>Nil)
+    done           = expectedOutput (topEntity testInput)
+    clk            = tbSystemClockGen (not <$> done)
+    rst            = systemResetGen
