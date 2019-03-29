26a9c612b0cbd7057f00bb61b0cb5556c26bb9be
Christiaan Baaij <christiaan.baaij@gmail.com>
2019-03-29 23:36:48 +0000

    Add a top-down AppProp transformation
    The previous version was applied in a innerMost traversal,
    propagating a single (innermost) argument at a time.
    
    The new version tries to propagate as many arguments as possible,
    and does it in a top-down traversal.
    
    The goal is to ultimately reduces the number of transformation,
    and traversals, ultimately reducing compile times.

--- clash-lib/src/Clash/Normalize/Strategy.hs
+++ clash-lib/src/Clash/Normalize/Strategy.hs
@@ -31,7 +31,7 @@ normalization :: NormRewrite
 normalization = rmDeadcode >-> constantPropgation >-> etaTL >-> rmUnusedExpr >-!-> anf >-!-> rmDeadcode >->
                 bindConst >-> letTL >-> evalConst >-!-> cse >-!-> cleanup >-> recLetRec
   where
-    etaTL      = apply "etaTL" etaExpansionTL !-> innerMost (apply "applicationPropagation" appProp)
+    etaTL      = apply "etaTL" etaExpansionTL !-> topdownR (apply "applicationPropagation" appPropFast)
     anf        = topdownR (apply "nonRepANF" nonRepANF) >-> apply "ANF" makeANF >-> topdownR (apply "caseCon" caseCon)
     letTL      = topdownSucR (apply "topLet" topLet)
     recLetRec  = apply "recToLetRec" recToLetRec
@@ -54,7 +54,7 @@ constantPropgation = propagate >-> repeatR inlineAndPropagate >->
                      caseFlattening >-> dec >-> spec >-> dec >->
                      conSpec
   where
-    propagate          = innerMost (applyMany transPropagate)
+    propagate          = topdownRR (applyMany transPropagate)
     inlineAndPropagate = (topdownR (applyMany transInlineSafe) >-> inlineNR)
                          !-> propagate
     spec               = bottomupR (applyMany specTransformations)
@@ -64,7 +64,7 @@ constantPropgation = propagate >-> repeatR inlineAndPropagate >->
 
     transPropagate :: [(String,NormRewrite)]
     transPropagate =
-      [ ("applicationPropagation", appProp              )
+      [ ("applicationPropagation", appPropFast          )
       , ("bindConstantVar"       , bindConstantVar      )
       , ("caseLet"               , caseLet              )
       , ("caseCase"              , caseCase             )
@@ -240,6 +240,9 @@ liftNonRep.
 -- | Topdown traversal, stops upon first success
 topdownSucR :: Rewrite extra -> Rewrite extra
 topdownSucR r = r >-! (allR (topdownSucR r))
+
+topdownRR :: Rewrite extra -> Rewrite extra
+topdownRR r = repeatR (topdownR r)
 
 innerMost :: Rewrite extra -> Rewrite extra
 innerMost r = bottomupR (r !-> innerMost r)
