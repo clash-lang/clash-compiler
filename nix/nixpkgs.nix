90fa58ba13f3b6897c00e2b02501eba90a4f5d7b
Martijn Bastiaan <martijn@hmbastiaan.nl>
2019-07-18 17:10:08 +0200

    Fix nix build and add nix to CI (#675)

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -14,6 +14,12 @@ let
         ghc-typelits-extra =
           self.callCabal2nix "ghc-typelits-extra" sources.ghc-typelits-extra {};
 
+        type-errors =
+          self.callCabal2nix "type-errors" sources.type-errors {};
+
+        first-class-families =
+          self.callCabal2nix "first-class-families" sources.first-class-families {};
+
         # Internal overrides
         clash-lib = import ../clash-lib {};
         clash-ghc = import ../clash-ghc {};
