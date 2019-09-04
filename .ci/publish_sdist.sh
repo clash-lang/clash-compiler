a49eb2c808d555d9b7651c403ae49d14184ecd56
Martijn Bastiaan <martijn@hmbastiaan.nl>
2019-09-04 09:20:53 +0200

    Auto release full releases on Hackage

--- .ci/publish_sdist.sh
+++ .ci/publish_sdist.sh
@@ -10,12 +10,8 @@ set +u
 
 if [[ "$HACKAGE_RELEASE" == "yes" ]]; then
     # Release tag set, upload as release.
-    # TODO: Right now, this branch will do the same as the other: make a
-    # release candidate. We can change it to actually have it release after
-    # we've gained enough trust in the release logic.
-    echo "XXX: Release logic activated!"
-    cabal upload --username=${HACKAGE_USERNAME} --password=${PASSWORD} ${SDIST} 
-    cabal upload --documentation --username=${HACKAGE_USERNAME} --password=${PASSWORD} ${DDIST} 
+    cabal upload --publish --username=${HACKAGE_USERNAME} --password=${PASSWORD} ${SDIST}
+    cabal upload --publish --documentation --username=${HACKAGE_USERNAME} --password=${PASSWORD} ${DDIST}
 elif [[ "$HACKAGE_RELEASE" == "no" ]]; then
     # Upload as release candidate
     cabal upload --username=${HACKAGE_USERNAME} --password=${PASSWORD} ${SDIST} 
