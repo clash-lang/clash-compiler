dd3be7996456737cab91d3a8398c507ec7e0fb59
Christiaan Baaij <christiaan.baaij@gmail.com>
2019-09-16 07:46:48 +0200

    Add tests for all modes, and slave spi latch options

--- test/Test/Cores/SPI.hs
+++ test/Test/Cores/SPI.hs
@@ -1,5 +1,6 @@
 module Test.Cores.SPI where
 
+import qualified Data.List as L
 import Data.Maybe
 import qualified Prelude as P
 
@@ -11,49 +12,87 @@ import qualified Clash.Explicit.Prelude as E
 
 import Clash.Cores.SPI
 
-testMode :: SPIMode
-testMode = SPIMode2
-
 masterInBP
   :: KnownDomain dom
-  => BitVector 8
+  => BitVector n
   -> Clock dom
   -> Reset dom
   -> Signal dom Bool
-  -> Signal dom (Maybe (BitVector 8))
+  -> Signal dom (Maybe (BitVector n))
 masterInBP val clk rst =
   E.moore clk rst enableGen
           (\_ i -> i)
           (\b -> if b then Nothing else Just val)
           True
 
-testMasterSlave :: Signal System (Maybe (BitVector 8), Maybe (BitVector 8))
-testMasterSlave = bundle (slaveOut,masterOut)
+testMasterSlave
+  :: 1 <= m
+  => SPIMode
+  -- ^ SPI Mode
+  -> Bool
+  -- ^ Whether the SPI slave should latch SPI signals
+  -> SNat m
+  -- ^ Half-period of the clock divider for the SPI master
+  -> BitVector 10
+  -- ^ Value master sends to slave
+  -> BitVector 10
+  -- ^ Value slave sends to master
+  -> Int
+  -- ^ Sample duration
+  -> (([BitVector 10],Int),([BitVector 10],Int))
+  -- ^
+  --
+  -- 1.1 Different slave outputs captured, should be equal to master value
+  -- 1.2 Number of slave outputs captured
+  -- 2.1 Different master outputs captured, should be equal to slave value
+  -- 2.2 Number of master outputs captured
+testMasterSlave spiMode latchSPI divHalf mVal sVal duration =
+   let s = sampleN duration (bundle (slaveOut,masterOut))
+       (slaveOutS,masterOutS) = P.unzip s
+       ss0 = catMaybes slaveOutS
+       ms0 = catMaybes masterOutS
+   in  ((L.nub ss0,P.length ss0),(L.nub ms0,P.length ms0))
  where
-  slaveIn = pure (0b01100111 :: BitVector 8)
+  slaveIn = pure sVal
   (misoZ,slaveOut) =
     exposeClockResetEnable spiSlaveLatticeSBIO
-      clk rst enableGen testMode True sclk mosi miso ss slaveIn
+      clk rst enableGen spiMode latchSPI sclk mosi miso ss slaveIn
   miso = veryUnsafeToBiSignalIn misoZ
 
-  masterIn = masterInBP (0b01100111 :: BitVector 8) clk rst bp
+  masterIn = masterInBP mVal clk rst bp
 
   (sclk,mosi,ss,bp,masterOut) =
     exposeClockResetEnable spiMaster
-      clk rst enableGen testMode d4 masterIn (readFromBiSignal miso)
+      clk rst enableGen spiMode divHalf masterIn (readFromBiSignal miso)
 
   clk = systemClockGen
   rst = systemResetGen
 
-masterX =
-  let s = sampleN 100 testMasterSlave
-      (ss,ms) = P.unzip s
-      ss0 = catMaybes ss
-      ms0 = catMaybes ms
-  in  ((ss0,P.length ss0),(ms0,P.length ms0))
-
 tests :: TestTree
 tests =
   testGroup "SPI"
-  [ testCase "Mode2" (masterX @?= (([0b01100111],1),([0b01100111],1)))
+  [ testCase "Mode0, Divider:8, Slave latch: True"
+      (testMasterSlave SPIMode0 True d4 0b0110011101 0b0110010101 (3*84) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
+  , testCase "Mode1, Divider:8, Slave latch: True"
+      (testMasterSlave SPIMode1 True d4 0b0110011101 0b0110010101 (3*84) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
+  , testCase "Mode2, Divider:8, Slave latch: True"
+      (testMasterSlave SPIMode2 True d4 0b0110011101 0b0110010101 (3*84) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
+  , testCase "Mode3, Divider:8, Slave latch: True"
+      (testMasterSlave SPIMode3 True d4 0b0110011101 0b0110010101 (3*84) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
+  , testCase "Mode0, Divider:2, Slave latch: False"
+      (testMasterSlave SPIMode0 False d1 0b0110011101 0b0110010101 (3*25) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
+  , testCase "Mode1, Divider:2, Slave latch: False"
+      (testMasterSlave SPIMode1 False d1 0b0110011101 0b0110010101 (3*25) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
+  , testCase "Mode2, Divider:2, Slave latch: False"
+      (testMasterSlave SPIMode2 False d1 0b0110011101 0b0110010101 (3*25) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
+  , testCase "Mode3, Divider:2, Slave latch: False"
+      (testMasterSlave SPIMode3 False d1 0b0110011101 0b0110010101 (3*25) @?=
+       (([0b0110011101],3),([0b0110010101],3)))
   ]
