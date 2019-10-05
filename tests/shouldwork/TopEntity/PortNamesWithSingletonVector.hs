9618f640a18ed9530c8856a726d5f7fb7ae88b6c
Jim McStanton <jim@jhmcstanton.com>
2019-10-05 15:08:00 -0500

    Added verilog output test for Vec 1 bit.
    Confirms behavior seen in #810.

---
+++ tests/shouldwork/TopEntity/PortNamesWithSingletonVector.hs
@@ -0,0 +1,56 @@
+module PortNamesWithSingletonVector where
+
+import qualified Prelude as P
+import Data.List (isInfixOf)
+import System.Environment (getArgs)
+import System.FilePath ((</>), takeDirectory)
+
+import Clash.Prelude
+import Clash.Explicit.Testbench
+
+{-# ANN topEntity
+  (Synthesize
+    { t_name     = "PortNamesWithSingletonVector_topEntity"
+    , t_inputs   =
+        [ PortName "inp0"
+        , PortName "inp1"
+        , PortName "inp2"
+        , PortName "inp3"
+        , PortName "inp4"
+        , PortName "inp5"
+        , PortName "inp6"
+        ]
+    , t_output = PortName "outp0"
+    }) #-}
+topEntity
+  :: Bit
+  -> BitVector 1
+  -> Unsigned 1
+  -> Vec 1 Bit
+  -> Vec 1 (BitVector 1)
+  -> Vec 1 (Unsigned 1)
+  -> Vec 1 (Signed 1)
+  -> Bool
+topEntity _ _ _ _ _ _ _ = True
+
+-- File content test
+assertIn :: String -> String -> IO ()
+assertIn needle haystack
+  | needle `isInfixOf` haystack = return ()
+  | otherwise                   = P.error $ P.concat [ "Expected:\n\n  ", needle
+                                                     , "\n\nIn:\n\n", haystack ]
+
+mainVerilog :: IO ()
+mainVerilog = do
+  [topDir] <- getArgs
+  content  <- readFile (takeDirectory topDir </> "PortNamesWithSingletonVector_topEntity" </> "PortNamesWithSingletonVector_topEntity.v")
+
+  mapM_ (`assertIn` content)
+    [ "input  inp0"
+    , "input [0:0] inp1"
+    , "input [0:0] inp2"
+    , "input [0:0] inp3"
+    , "input [0:0] inp4"
+    , "input [0:0] inp5"
+    , "input [0:0] inp6"
+    ]
