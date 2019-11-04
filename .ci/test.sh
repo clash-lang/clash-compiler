7dfd58b5ea9b99c2d888bb62cc6d56490aa49e11
Alex McKenna <alex@qbaylogic.com>
2019-11-04 11:50:56 +0100

    Bump clash-cores to version 1.1.0
    The clash-cores package has been bumped to version 1.1.0 (the same
    as clash-{ghc, lib, prelude} is currently). The misc. project
    configuration files now include clash-cores, which should be the
    minimum it needs to be merged.

--- .ci/test.sh
+++ .ci/test.sh
@@ -10,7 +10,7 @@ sed "s/^ghci/ghc -fno-code/" clash-dev > clash-dev-test
 sh clash-dev-test
 
 # Check whether version numbers in snap / clash-{prelude,lib,ghc} are the same
-cabal_files="clash-prelude/clash-prelude.cabal clash-lib/clash-lib.cabal clash-ghc/clash-ghc.cabal"
+cabal_files="clash-prelude/clash-prelude.cabal clash-lib/clash-lib.cabal clash-ghc/clash-ghc.cabal clash-cores/clash-cores.cabal"
 snapcraft_file="bindist/linux/snap/snap/snapcraft.yaml"
 versions=$(grep "^[vV]ersion" $cabal_files $snapcraft_file | grep -Eo '[0-9]+(\.[0-9]+)+')
 
