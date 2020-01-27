8475c249c6d32843eae102e55d22b56cf9f4c953
Dr. ERDI Gergo <gergo@erdi.hu>
2020-01-27 20:28:51 +0800

    Change `Show` instance of `SNat @n` where `n` doesn't have a corresponding `dN` helper function (Fixes #1038)

--- clash-prelude/src/Clash/Promoted/Nat.hs
+++ clash-prelude/src/Clash/Promoted/Nat.hs
@@ -109,7 +109,10 @@ snatProxy :: KnownNat n => proxy n -> SNat n
 snatProxy _ = SNat
 
 instance Show (SNat n) where
-  show p@SNat = 'd' : show (snatToInteger p)
+  show p@SNat | n <= 1024 = 'd' : show n
+              | otherwise = "SNat @" <> show n
+    where
+      n = snatToInteger p
 
 instance ShowX (SNat n) where
   showsPrecX = showsPrecXWith showsPrec
