7fcaff0b03945806a7037c8ac7084a3d479efc43
Martijn Bastiaan <martijn@hmbastiaan.nl>
2020-03-06 09:09:58 +0100

    CI: replace 'devel' with 'stable' if releasing to snap beta/stable

--- .ci/snap_publish.sh
+++ .ci/snap_publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 SNAPCRAFT_BUILD_ENVIRONMENT_CPU=$(nproc)
 export SNAPCRAFT_BUILD_ENVIRONMENT_CPU
 SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=16G
@@ -12,10 +14,19 @@ if [ "$(cat snap-last-run-hash)" == "$(git rev-parse HEAD)-${RELEASE_CHANNEL}" ]
   echo "Already built and published $(git rev-parse HEAD) on ${RELEASE_CHANNEL}. Nothing to do!";
   exit 0;
 fi
-
 git rev-parse HEAD > snap-last-run-hash
 echo "-${RELEASE_CHANNEL}" >> snap-last-run-hash
+
 cd bindist/linux/snap || exit
+
+# Make sure devel is in snap/snapcraft.yaml before replacing it with 'stable'
+# (if applicable). sed doesn't fail if it doesn't replace anything.
+grep devel snap/snapcraft.yaml
+if [[ ${RELEASE_CHANNEL} == "stable" || ${RELEASE_CHANNEL} == "beta" ]]; then
+  # The Snap Store only allows grade=stable for stable snaps
+  sed -i s/devel/stable/ snap/snapcraft.yaml
+fi
+
 echo $SNAPCRAFT_LOGIN_FILE | base64 --decode --ignore-garbage > snapcraft.login
 snapcraft login --with snapcraft.login
 snapcraft
