cb0abd42bacd6bac45b0112bc828d90467f32ca9
Martijn Bastiaan <martijn@hmbastiaan.nl>
2020-04-28 13:24:09 +0200

    [snap] Install and expose cabal binary (#1293)

--- bindist/linux/snap/bindist.nix
+++ bindist/linux/snap/bindist.nix
@@ -6,6 +6,8 @@ buildEnv {
   name = "clash-compiler-bindist";
   paths = [
     bash
+    pkgs.binutils
+    pkgs.haskellPackages.cabal-install
     (pkgs.haskellPackages.ghcWithPackages (p: with p; [clash-prelude clash-lib]))
     pkgs.haskellPackages.clash-ghc
   ];
