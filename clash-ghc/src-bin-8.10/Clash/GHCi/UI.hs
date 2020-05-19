a0515c543998521ccab9cfef6018863915904dd3
Alex McKenna <alexmckenna@outlook.com>
2020-05-19 15:26:12 +0100

    Fix partial-evaluator on GHC 8.10

--- clash-ghc/src-bin-8.10/Clash/GHCi/UI.hs
+++ clash-ghc/src-bin-8.10/Clash/GHCi/UI.hs
@@ -150,7 +150,13 @@ import           Clash.Backend.VHDL (VHDLState)
 import           Clash.Backend.Verilog (VerilogState)
 import qualified Clash.Driver
 import           Clash.Driver.Types (ClashOpts(..))
+
+#if EXPERIMENTAL_EVALUATOR
+import           Clash.GHC.PartialEval
+#else
 import           Clash.GHC.Evaluator
+#endif
+
 import           Clash.GHC.GenerateBindings
 import           Clash.GHC.NetlistTypes
 import           Clash.GHCi.Common
@@ -2235,7 +2241,11 @@ makeHDL backend optsRef srcs = do
                   tcm
                   tupTcm
                   (ghcTypeToHWType iw fp)
+#if EXPERIMENTAL_EVALUATOR
+                  ghcEvaluator
+#else
                   evaluator
+#endif
                   topEntities
                   mainTopEntity
                   opts2
