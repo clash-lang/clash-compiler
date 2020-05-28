3e7884549e5bf7362c8ca1b1f72009b6f4f94443
Leon Schoorl <leon@qbaylogic.com>
2020-05-28 19:49:21 +0200

    Make sure head and tail Verilog blackboxes return correctly typed values
    The part-selects (var[x:y]) always return unsigned values in Verilog.
    This can create problems when we call head or tail on a vector of signed
    values.
    
    This is fixed by using (and implementing) ~FROMBV to add a cast when the result is Signed.
    
    Fixes #1351
    
    (cherry picked from commit c81b5015381b46ae67fe74ae20b786f97bc2a690)

--- testsuite/Main.hs
+++ testsuite/Main.hs
@@ -402,6 +402,7 @@ runClashTest = defaultMain $ clashTestRoot
         , runTest "Signum" def
         , runTest "Strict" def
         , runTest "T1019" def{hdlSim=False}
+        , runTest "T1351" def
         , outputTest ("tests" </> "shouldwork" </> "Numbers") allTargets [] ["-itests/shouldwork/Numbers"] "UndefinedConstantFolding"  "main"
         , runTest "UnsignedZero" def
         ]
