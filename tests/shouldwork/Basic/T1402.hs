43779fdd653b72480093a85e65956d98278e0f14
Christiaan Baaij <christiaan.baaij@gmail.com>
2020-06-24 18:23:24 +0200

    Warn about globally enabled Worker/Wrapper
    It breaks T1402, but according to commit
    b7632cd3782e6867c72c2cb174a0081e36cb8529#diff-9a99b6ae874a89baf2e411a7740f501b
    it can also be beneficial, so we should forcefully disable it
    either. Experts can enable it on a file-by-file basis using
    a `{-# OPTIONS_GHC -fworker-wrapper #-}` at their own risk.
    
    (cherry picked from commit 957a302c9aa3afce17c623e7daa79dfd14dc10fb)

--- tests/shouldwork/Basic/T1402.hs
+++ tests/shouldwork/Basic/T1402.hs
@@ -1,7 +1,8 @@
-{-# OPTIONS_GHC -O #-}
+{-# OPTIONS_GHC -fno-worker-wrapper #-}
 module T1402 where
 
 import Clash.Prelude
+import Clash.Explicit.Testbench
 
 topEntity :: Maybe (BitVector 128) -> BitVector 128 -> (BitVector 128,Bool)
 topEntity q y = case q of
@@ -12,3 +13,16 @@ f :: Bool -> BitVector 128 ->  (BitVector 128, Bool)
 f t x = case x of
   1 -> (x,t)
   _ -> (x,t)
+
+testBench :: Signal System Bool
+testBench = done
+  where
+    testInput1 = stimuliGenerator clk rst $(listToVecTH
+        [Just (maxBound :: (BitVector 128))])
+    testInput2 = stimuliGenerator clk rst $(listToVecTH
+        [maxBound :: (BitVector 128)])
+    expectedOutput = outputVerifier' clk rst $(listToVecTH
+        [(maxBound :: (BitVector 128), True)])
+    done           = expectedOutput (topEntity <$> testInput1 <*> testInput2)
+    clk            = tbSystemClockGen (not <$> done)
+    rst            = systemResetGen
