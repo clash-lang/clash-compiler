31304f4adb27d241ac2fc1553a8fdbcaf7f8a655
Christiaan Baaij <christiaan.baaij@gmail.com>
2020-07-11 19:21:18 +0200

    Move tag check to ci/setup.hs

--- .ci/test.sh
+++ .ci/test.sh
@@ -12,24 +12,6 @@ if [[ "$RUN_CLASHDEV" = "yes" ]]; then
   fi
 fi
 
-# You'd think comparing v${version} with ${CI_COMMIT_TAG} would do the
-# trick, but no..
-CI_COMMIT_TAG=${CI_COMMIT_TAG:-}
-version=$(echo $versions | tr ' ' '\n' | head -n 1)
-tag_version=${CI_COMMIT_TAG:1:${#CI_COMMIT_TAG}-1}  # Strip first character (v0.99 -> 0.99)
-
-if [[ ${tag_version} != "" && ${version} != ${tag_version} ]]; then
-    if [[ "${CI_COMMIT_TAG:0:1}" == "v" ]]; then
-        echo "Tag name and distribution's release number should match:"
-        echo "  Tag version:          ${CI_COMMIT_TAG}"
-        echo "  Distribution version: v${version}"
-        exit 1;
-    else
-        echo "\$CI_COMMIT_TAG should start with a 'v'. Found: ${CI_COMMIT_TAG}"
-        exit 1;
-    fi
-fi
-
 # Run unittests, doctests
 if [[ "$RUN_LIBTESTS" = "yes" ]]; then
   # Create a ghc environment file needed for the doctests
