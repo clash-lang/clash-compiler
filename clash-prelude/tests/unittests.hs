528fe9fce37b750e34d65c563d2ea2a5fc6b10dd
Peter Lebbing <peter@digitalbrains.com>
2020-08-31 16:31:25 +0200

    Add unittests for SaturatingNum Fixed

--- clash-prelude/tests/unittests.hs
+++ clash-prelude/tests/unittests.hs
@@ -6,9 +6,10 @@ import qualified Clash.Tests.AutoReg
 import qualified Clash.Tests.BitPack
 import qualified Clash.Tests.BitVector
 import qualified Clash.Tests.DerivingDataRepr
+import qualified Clash.Tests.Fixed
+import qualified Clash.Tests.NFDataX
 import qualified Clash.Tests.Signal
 import qualified Clash.Tests.Signed
-import qualified Clash.Tests.NFDataX
 import qualified Clash.Tests.TopEntityGeneration
 import qualified Clash.Tests.Unsigned
 
@@ -18,9 +19,10 @@ tests = testGroup "Unittests"
   , Clash.Tests.BitPack.tests
   , Clash.Tests.BitVector.tests
   , Clash.Tests.DerivingDataRepr.tests
+  , Clash.Tests.Fixed.tests
+  , Clash.Tests.NFDataX.tests
   , Clash.Tests.Signal.tests
   , Clash.Tests.Signed.tests
-  , Clash.Tests.NFDataX.tests
   , Clash.Tests.TopEntityGeneration.tests
   , Clash.Tests.Unsigned.tests
   ]
