4eda72039c220563bccd4f17b21ed011dd6b3057
Leon Schoorl <leon@qbaylogic.com>
2020-10-20 13:34:05 +0200

    Provide mkBlackBoxContext with the correct primName (#1553)
    Fixes #1524
    
        map (fold f) ...
    
    Would fail to generate the netlist because:
     1. `mkBlackBoxContext` for `fold` would be called  with "__INTERNAL__" as name
     2. this  would cause  `funcPlurality` to default to 1
     3. and thus not enough copies of `f` would be pre-rendered
     4. causing the rendering of `fold` to fail: "not enough functions rendered"

---
+++ tests/shouldwork/BlackBox/T1524.hs
@@ -0,0 +1,29 @@
+module T1524 where
+
+import Clash.Prelude
+import Clash.Explicit.Testbench
+
+topEntity :: Vec 2 (Vec 3 Int) -> Vec 2 Int
+topEntity = f @2 @(3-1)
+{-# NOINLINE topEntity #-}
+
+f :: Vec k (Vec (l+1) Int) -> Vec k Int
+f input = map g input
+
+g :: Vec (l+1) Int -> Int
+g = fold (+)
+
+case1 :: Vec 2 (Vec 3 Int)
+case1 = (1 :> 2 :> 3 :> Nil) :> (10 :> 20 :> 30 :> Nil) :> Nil
+case2 = (-1 :> -2 :> -3 :> Nil) :> (10 :> -20 :> 30 :> Nil) :> Nil
+
+input = case1 :> case2 :> Nil
+expected = (6 :> 60 :> Nil) :> (-6 :> 20 :> Nil) :> Nil
+
+testBench :: Signal System Bool
+testBench = done
+ where
+  done = outputVerifier' clk aclr expected (topEntity <$> inp)
+  clk  = tbSystemClockGen (not <$> done)
+  inp  = stimuliGenerator clk aclr input
+  aclr = systemResetGen
