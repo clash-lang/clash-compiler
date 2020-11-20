67ad4944940431449049afe8f08a1823a39b647e
Martijn Bastiaan <martijn@hmbastiaan.nl>
2020-11-20 16:00:21 +0100

    Switch to GitHub docker registry
    Workaround pull limits of dockerhub.com

--- .ci/docker/build-and-publish-docker-image.sh
+++ .ci/docker/build-and-publish-docker-image.sh
@@ -2,17 +2,19 @@
 
 set -xeo pipefail
 
+REPO="docker.pkg.github.com/clash-lang/clash-compiler"
+NAME="clash-ci"
 DIR=$(dirname "$0")
 now=$(date +%F)
 
-docker build -t "clashlang/clash-ci:$now" "$DIR"
-docker tag "clashlang/clash-ci:$now" "clashlang/clash-ci:latest"
+docker build -t "${REPO}/${NAME}:$now" "$DIR"
+docker tag "${REPO}/${NAME}:$now" "${REPO}/${NAME}:latest"
 
-read -p "Push to DockerHub? (y/N) " push
+read -p "Push to GitHub? (y/N) " push
 
 if [[ $push =~ ^[Yy]$ ]]; then
-        docker push "clashlang/clash-ci:$now"
-        docker push "clashlang/clash-ci:latest"
+        docker push "${REPO}/${NAME}:$now"
+        docker push "${REPO}/${NAME}:latest"
 else
         echo "Skipping push to container registry"
 fi
