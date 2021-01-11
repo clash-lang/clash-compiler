e624ed3abd0c33ffaf5f14f031ee99de71a0dea3
Martijn Bastiaan <martijn@hmbastiaan.nl>
2021-01-11 19:46:31 +0100

    Publish Haddock as CI artifact

--- .ci/build_docs.sh
+++ .ci/build_docs.sh
@@ -8,6 +8,29 @@ else
   haddock_pkgs="clash-lib clash-cosim clash-prelude"
 fi
 
+# Cache dependencies
+cabal v2-build all --enable-documentation
+
+mkdir -p hadocs
+
 for pkg in ${haddock_pkgs}; do
-  cabal v2-haddock ${pkg} --enable-documentation
+  cabal v2-haddock ${pkg} --enable-documentation |& tee haddock_log
+
+  set +e
+
+  # TODO: We're not yet ready for the check below :-)
+  # if grep -q "Missing documentation" haddock_log; then
+  #   echo -e "\e[1m\e[31mMissing documentation! Scroll up for full log.\e[0m"
+  #   grep --color=always -n -C 5 "Missing documentation" haddock_log
+  #   exit 1
+  # fi
+
+  # if grep -q "If you qualify the identifier, haddock can try to link it anyway" haddock_log; then
+  #   echo -e "\e[1m\e[31mIdentifier out of scope! Scroll up for full log.\e[0m"
+  #   grep --color=always -n -C 5 "If you qualify the identifier, haddock can try to link it anyway" haddock_log
+  #   exit 1
+  # fi
+
+  # Copy documention to hadocs/
+  ln -s "$(dirname "$(tail -n1 haddock_log)")" hadocs/${pkg}
 done
