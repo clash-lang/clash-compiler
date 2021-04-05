b0cb707a5c26f3c311069fbb97e3d6f4fc6d02ab
Christiaan Baaij <christiaan.baaij@gmail.com>
2021-04-05 18:14:00 +0200

    Normalize `Annotate` attributes
    Fixes #1742

---
+++ tests/shouldwork/Issues/T1742.hs
@@ -0,0 +1,27 @@
+module T1742 where
+
+import           Clash.Prelude
+import           Clash.Signal
+
+import           Clash.Annotations.SynthesisAttributes
+import           Clash.Annotations.TopEntity
+import           Clash.Annotations.TH
+
+createDomain vSystem{vName="Bank2C", vPeriod=20000, vResetKind=Asynchronous, vResetPolarity=ActiveLow}
+
+type Pin t x y = t `Annotate` 'StringAttr "chip_pin" x
+                   `Annotate` 'StringAttr "altera_attribute" ("-name IO_STANDARD \"" `AppendSymbol` y `AppendSymbol` "\"")
+
+top :: HiddenClockResetEnable dom => Signal dom (BitVector 4)
+top = s where s = register 0 (s + 1)
+
+topEntity
+  :: "CLK_50_B2C"  ::: Pin (Clock Bank2C) "PIN_AW38" "1.2 V"
+  -> "CPU_RESET_n" ::: Signal Bank2C Bool
+  -> "LED"         ::: Signal Bank2C (BitVector 4)
+topEntity clk rstnButton = exposeClockResetEnable top clk rst en where
+  en = enableGen
+  rst = unsafeFromLowPolarity rstnButton
+  rstSync = resetSynchronizer clk rst en
+
+makeTopEntityWithName 'topEntity "shell"
