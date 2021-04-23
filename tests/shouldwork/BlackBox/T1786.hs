8b51781e3817532c3ca50aaf247f94dafbd980f1
Peter Lebbing <peter@digitalbrains.com>
2021-04-23 11:51:45 +0200

    Fix #1786.
    Some functionality is added: when `~ISACTIVEENABLE` is used on a signal
    of type `Signal dom Bool` and it is always true, `~ISACTIVEENABLE` now
    evaluates to true as well. Before this commit, it was not an error to
    use `~ISACTIVEENABLE` on a `Signal dom Bool`, but it would not be
    checked whether it was always true or not.

---
+++ tests/shouldwork/BlackBox/T1786.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE BangPatterns #-}
+module T1786 where
+
+import           Clash.Prelude
+
+import           Clash.Explicit.Testbench
+import           Clash.Annotations.Primitive
+
+import           Data.String.Interpolate.IsString (i)
+import           Data.String.Interpolate.Util     (unindent)
+
+testEnable :: Signal System Bool
+testEnable = testAlwaysEnabled enableGen
+{-# NOINLINE testEnable #-}
+
+-- Only call with always-enabled Enable if you want the model to match the HDL.
+testAlwaysEnabled
+  :: Enable System
+  -> Signal System Bool
+testAlwaysEnabled !_ = pure True
+{-# NOINLINE testAlwaysEnabled #-}
+{-# ANN testAlwaysEnabled (InlinePrimitive [VHDL] $ unindent [i|
+  [ { "BlackBox" :
+      { "name"      : "T1786.testAlwaysEnabled"
+      , "kind"      : "Expression"
+      , "template"  : "~IF ~ISACTIVEENABLE[0] ~THEN false ~ELSE true ~FI"
+      }
+    }
+  ]
+  |]) #-}
+
+testEnableTB :: Signal System Bool
+testEnableTB = done
+ where
+  done = outputVerifier' clk rst (True :> Nil) testEnable
+  clk  = tbSystemClockGen (not <$> done)
+  rst  = systemResetGen
+{-# NOINLINE testEnableTB #-}
+{-# ANN testEnableTB (TestBench 'testEnable) #-}
+
+testBool :: Signal System Bool
+testBool = testAlwaysEnabledBool (pure True)
+{-# NOINLINE testBool #-}
+
+-- Only call with always-true input if you want the model to match the HDL.
+testAlwaysEnabledBool
+  :: Signal System Bool
+  -> Signal System Bool
+testAlwaysEnabledBool !_ = pure True
+{-# NOINLINE testAlwaysEnabledBool #-}
+{-# ANN testAlwaysEnabledBool (InlinePrimitive [VHDL] $ unindent [i|
+  [ { "BlackBox" :
+      { "name"      : "T1786.testAlwaysEnabledBool"
+      , "kind"      : "Expression"
+      , "template"  : "~IF ~ISACTIVEENABLE[0] ~THEN false ~ELSE true ~FI"
+      }
+    }
+  ]
+  |]) #-}
+
+testBoolTB :: Signal System Bool
+
+testBoolTB = done
+ where
+  done = outputVerifier' clk rst (True :> Nil) testBool
+  clk  = tbSystemClockGen (not <$> done)
+  rst  = systemResetGen
+{-# NOINLINE testBoolTB #-}
+{-# ANN testBoolTB (TestBench 'testBool) #-}
