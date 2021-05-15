87f0d158960f9fcccea4a07c2c378efaa7531807
Martijn Bastiaan <martijn@hmbastiaan.nl>
2021-05-15 13:43:26 +0200

    Always run Debian bindist, add caching (#1808)

--- .ci/docker/build-and-publish-docker-image.sh
+++ .ci/docker/build-and-publish-docker-image.sh
@@ -7,19 +7,30 @@ NAME="clash-ci-"
 DIR=$(dirname "$0")
 now=$(date +%F)
 
-GHC_VERSIONS="9.0.1 8.10.4 8.8.4 8.6.5"
-for GHC_VERSION in $GHC_VERSIONS
-do
-  docker build --build-arg ghc_version=${GHC_VERSION} -t "${REPO}/${NAME}${GHC_VERSION}:$now" "$DIR"
-  docker tag "${REPO}/${NAME}${GHC_VERSION}:$now" "${REPO}/${NAME}${GHC_VERSION}:latest"
-done
+GHC_VERSIONS=(  "9.0.1"   "8.10.4"  "8.8.4"   "8.6.5")
+CABAL_VERSIONS=("3.4.0.0" "3.2.0.0" "3.2.0.0" "3.0.0.0")
 
+for i in "${!GHC_VERSIONS[@]}"
+do
+  GHC_VERSION="${GHC_VERSIONS[i]}"
+  CABAL_VERSION="${CABAL_VERSIONS[i]}"
+
+  docker build \
+    --build-arg cabal_version=${CABAL_VERSION} \
+    --build-arg ghc_version=${GHC_VERSION} \
+    -t "${REPO}/${NAME}${GHC_VERSION}:$now" "$DIR"
+
+  docker tag \
+    "${REPO}/${NAME}${GHC_VERSION}:$now" \
+    "${REPO}/${NAME}${GHC_VERSION}:latest"
+done
 
 read -p "Push to GitHub? (y/N) " push
 
 if [[ $push =~ ^[Yy]$ ]]; then
-  for GHC_VERSION in $GHC_VERSIONS
+  for i in "${!GHC_VERSIONS[@]}"
   do
+    GHC_VERSION="${GHC_VERSIONS[i]}"
     docker push "${REPO}/${NAME}${GHC_VERSION}:$now"
     docker push "${REPO}/${NAME}${GHC_VERSION}:latest"
   done
