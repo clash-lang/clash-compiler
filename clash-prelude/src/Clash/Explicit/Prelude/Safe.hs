91f2d818bb9bea677d735b77f553c89f05286c50
Basile Henry <bjm.henry@gmail.com>
2021-05-27 11:40:11 +0200

    clash-prelude: Fix link to Safe Haskell docs
    (cherry picked from commit 08474a9490fba01e4eed2a4c76a0fd5f176df2f6)

--- clash-prelude/src/Clash/Explicit/Prelude/Safe.hs
+++ clash-prelude/src/Clash/Explicit/Prelude/Safe.hs
@@ -5,7 +5,7 @@ Copyright  :  (C) 2013-2016, University of Twente,
 License    :  BSD2 (see the file LICENSE)
 Maintainer :  Christiaan Baaij <christiaan.baaij@gmail.com>
 
-__This is the <https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/safe_haskell.html Safe> API only of "Clash.Explicit.Prelude"__
+__This is the <https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/exts/safe_haskell.html Safe> API only of "Clash.Explicit.Prelude"__
 
 This module defines the explicitly clocked counterparts of the functions
 defined in "Clash.Prelude".
