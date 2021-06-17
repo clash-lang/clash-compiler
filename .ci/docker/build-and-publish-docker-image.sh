92a8df94b08da92aef677dc8419f763e6691c7d3
Alex McKenna <alex@qbaylogic.com>
2021-06-17 15:17:59 +0200

    Upgrade CI docker image
    The CI docker image is now multi-stage, and uses buildkit on compatible
    machines to build stages in parallel. This means that changes to the
    dockerfile (like updating a dependency build from source) will now
    no longer force any other dependencies to be rebuilt.
    
    As a bonus, the final stage now only has dependencies needed to build
    and run clash-testsuite, meaning the image is ~0.7GB smaller.

--- .ci/docker/build-and-publish-docker-image.sh
+++ .ci/docker/build-and-publish-docker-image.sh
@@ -7,8 +7,12 @@ NAME="clash-ci-"
 DIR=$(dirname "$0")
 now=$(date +%F)
 
-GHC_VERSIONS=(  "9.0.1"   "8.10.4"  "8.8.4"   "8.6.5")
+GHC_VERSIONS=(  "9.0.1"   "8.10.2"  "8.8.4"   "8.6.5")
 CABAL_VERSIONS=("3.4.0.0" "3.2.0.0" "3.2.0.0" "3.0.0.0")
+
+# We want to use docker buildkit so that our layers are built in parallel. This
+# is ignored completely on versions of docker which don't support buildkit.
+export DOCKER_BUILDKIT=1
 
 for i in "${!GHC_VERSIONS[@]}"
 do
@@ -18,11 +22,9 @@ do
   docker build \
     --build-arg cabal_version=${CABAL_VERSION} \
     --build-arg ghc_version=${GHC_VERSION} \
-    -t "${REPO}/${NAME}${GHC_VERSION}:$now" "$DIR"
-
-  docker tag \
-    "${REPO}/${NAME}${GHC_VERSION}:$now" \
-    "${REPO}/${NAME}${GHC_VERSION}:latest"
+    -t "${REPO}/${NAME}${GHC_VERSION}:$now" \
+    -t "${REPO}/${NAME}${GHC_VERSION}:latest" \
+    "$DIR"
 done
 
 read -p "Push to GitHub? (y/N) " push
