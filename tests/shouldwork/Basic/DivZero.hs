9bb5a37ee1d6a8a16c124986ac25b0ccd21c5243
Christiaan Baaij <christiaan.baaij@gmail.com>
2021-07-05 22:18:57 +0200

    VHDL: no div/rem/mod by zero in gen'd code
    Because Clash translates to concurrent assignments, things like
    
    ```
    if y /= 0 then quot x y else 0
    ```
    
    would result in a VHDL sim error (at least on GHDL) because all
    subterms of the if-then-else expression would be evaluated
    concurrently, and `x / y` would throw a sim error whenever `y=0`
    even though that result could never be observed.
    
    So now, for the division operators we add the following:
    ```
    -- pragma translate_off
    when (y /= 0) else (others => 'X')
    -- pragma translate_on
    ```
    To ensure the division by zero exception isn't thrown during
    VHDL simulation.

---
+++ tests/shouldwork/Basic/DivZero.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -fconstraint-solver-iterations=5 #-}
+module DivZero where
+
+import Clash.Prelude
+import Clash.Explicit.Testbench
+import GHC.Natural
+import qualified Data.List as L
+
+type T8 = (BitVector 8, Unsigned 8, Signed 8, Index 8, Int, Word, Integer)
+
+topEntity :: T8 -> T8
+topEntity (b,u,s,x,i,w,nX) =
+  ( 4 `quot` b
+  , 4 `quot` u
+  , 4 `quot` s
+  , 4 `quot` x
+  , 4 `quot` i
+  , 4 `quot` w
+  , 4 `quot` nX)
+{-# NOINLINE topEntity #-}
+
+t8ToEnum :: T8 -> (BitVector 8, Unsigned 8, Signed 8, Index 8, Int, Word, Int)
+t8ToEnum (b,u,s,x,i,w,nX) = (b,u,s,x,i,w, fromEnum nX)
+
+testBench :: Signal System Bool
+testBench = done
+  where
+    testInput      = pure (0, 0, 0, 0, 0, 0, 0)
+    expectedOutput = outputVerifierBitVector' clk rst
+                       ($(bLit (L.replicate (3 * 8 + 3 + 3 * 64) '.')) :> Nil)
+    done           = expectedOutput (pack . t8ToEnum . topEntity <$> testInput)
+    clk            = tbSystemClockGen (not <$> done)
+    rst            = systemResetGen
