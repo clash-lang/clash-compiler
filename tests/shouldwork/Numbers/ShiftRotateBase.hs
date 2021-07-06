c5913cad3ef2100520adc9642d8d28625f57ad2a
Christiaan Baaij <christiaan.baaij@gmail.com>
2021-07-06 09:38:17 +0200

    VHDL: don't overflow natural range in shift/rotate
    Haskell `shift{R;L}` and `rotate{R;L}` take an `Int` as the
    shift amount. In VHDL, `shift_{left;right}` and
    `rotate_{left;right}` take a `natural` as a shift amount.
    On 64 bit systems Haskell's `maxBound :: Int` is larger
    than VHDL's `natural'high` (in (nearly) all VHDL simulators).
    
    So for `shift_{left;right}` we saturate the shift amount to
    `natural'high`, and for `rotate_{left;right}` we do the rotate amount
    modulo the word width, all in order for the VHDL simulator not to error
    out with an out-of-bounds exception when the shift/rotate amount is
    larger than `natural'high`.

--- tests/shouldwork/Numbers/ShiftRotateBase.hs
+++ tests/shouldwork/Numbers/ShiftRotateBase.hs
@@ -8,9 +8,10 @@ import Data.Int
 
 testpattern = 0b1010011000
 
--- test with shift/rotate amounts: 0..16,200
+-- test with shift/rotate amounts: 0..16,200, and a really large
+-- non-power-of-two.
 amounts :: Vec _ Int
-amounts = $(listToVecTH [0::Int ..16]) :< 200
+amounts = $(listToVecTH [0::Int ..16]) :< 200 :< (maxBound - 5)
 
 testall v i
   = ( testAs @(Unsigned 8)  v i
