ac97f0d044ba44cc783f9fddfc656b8b511886b6
Peter Lebbing <peter@digitalbrains.com>
2022-01-27 17:01:57 +0100

    blockRam(File) primitive: fix undefined enable (#2054)
    The Haskell models of `blockRam#` and `blockRamFile#` treated an _undefined_
    write enable as an asserted enable. But an _undefined_ value in Haskell can
    correspond to any value whatsoever in HDL, so HDL simulation might or might not
    write. With this commit, the `XException` of the write enable is written as the
    value in the RAM, since it could have either been written to or not been
    written to. On the next read of that address, it will return the `XException`.
    
    This issue did not propagate to any other `blockRam` variants, the bug solely
    manifested when using the `blockRam#` and `blockRamFile#` primitives directly.
    All the other variants built upon those primitives always have their write
    address undefined whenever the write enable is undefined, and that case was
    properly handled by the primitive.
    
    The issue is identical to one of the issues in PR #2006 and PR #2031,
    for different memory primitives.

--- clash-prelude/tests/Clash/Tests/BlockRam.hs
+++ clash-prelude/tests/Clash/Tests/BlockRam.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Clash.Tests.BlockRam (tests) where
 
 import qualified Data.List as List
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import qualified Clash.Explicit.Prelude as E
+import Clash.Explicit.BlockRam (blockRam#)
 import Clash.Prelude
 
 readRam
@@ -23,7 +27,44 @@ addrNotTooStrict =
    in List.tail (sampleN @System 15 (readRam addr)) @?=
         [255,0,0,0,0,0,0,0,255,255,255,255,255,255]
 
+primRam
+  :: Signal System Int
+  -> Signal System Bool
+  -> Signal System Int
+  -> Signal System Int
+  -> Signal System (Maybe Int)
+primRam rd we wr din =
+  maybeIsX <$> blockRam# clockGen enableGen (0 :> 1 :> Nil) rd we wr din
+
+type PrimSamples = [(Int, Bool, Int, Int, Maybe Int)]
+
+undefEn :: PrimSamples
+
+-- Undefined enable:
+--    The written-to address should read 'undefined', but other addresses
+--    should still have their data.
+
+--                               rd  enable     waddr      wdata      dout
+
+undefEn =                     [ ( 0, undefined, 0        , 2        , Nothing)
+                              , ( 0, False    , 0        , 3        , Just 0 )
+                              , ( 1, False    , 0        , 3        , Nothing)
+                              , ( 1, False    , 0        , 3        , Just 1 )
+                              ]
+
+primRamAssertion
+  :: PrimSamples
+  -> Assertion
+primRamAssertion samples = actual @?= expectedOutput
+ where
+  (rd, we, wr, din, expectedOutput) = List.unzip5 samples
+  actual = E.sampleN (List.length samples) $ primRam (fromList rd)
+                                                     (fromList we)
+                                                     (fromList wr)
+                                                     (fromList din)
+
 tests :: TestTree
 tests = testGroup "BlockRam"
   [ testCase "Address strictness" addrNotTooStrict
+  , testCase "Undefined enable" $ primRamAssertion undefEn
   ]
