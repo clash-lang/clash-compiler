cd8e4d4b7e385be11d63c7451f1fb69c0bcde7f6
Martijn Bastiaan <martijn@qbaylogic.com>
2022-01-29 15:37:54 +0100

    Add test loading primitives from an external file

---
+++ tests/shouldwork/BlackBox/ExternalPrimitive.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+
+module ExternalPrimitive where
+
+import qualified Prelude as P
+import Data.List (isInfixOf)
+import System.Environment (getArgs)
+import System.FilePath ((</>))
+
+import Clash.Prelude
+import Clash.Annotations.Primitive (hasBlackBox)
+
+jsonPrim :: Int
+jsonPrim = 0
+{-# NOINLINE jsonPrim #-}
+{-# ANN jsonPrim hasBlackBox #-}
+
+yamlPrim :: Int
+yamlPrim = 1
+{-# NOINLINE yamlPrim #-}
+{-# ANN yamlPrim hasBlackBox #-}
+
+topEntity :: Vec _ Int
+topEntity =
+     jsonPrim
+  :> yamlPrim
+  :> Nil
+
+assertIn :: String -> String -> IO ()
+assertIn needle haystack
+  | needle `isInfixOf` haystack = return ()
+  | otherwise                   = P.error $ P.concat [ "Expected:\n\n  ", needle
+                                                     , "\n\nIn:\n\n", haystack ]
+
+mainVHDL :: IO ()
+mainVHDL = do
+  [topDir] <- getArgs
+  content <- readFile (topDir </> show 'topEntity </> "topEntity.vhdl")
+  assertIn "fekkjruzth-json" content
+  assertIn "kod0x1ta1k-yaml" content
