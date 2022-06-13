clash-lang/clash-compiler

commit 753f58301612f3198c514d607c467f123926c3f3

Disallow implicit nets in (System)Verilog (#2174)
Verilog allows nets to be implicitly defined in a module, assuming
they are of type `wire` by default. However, for Clash-generated
Verilog it should never be the case that a net is implicitly defined
in a design. We can disallow this by setting the default net type
to none.

According to IEEE 1364-2005:

    When the `default_nettype is set to none, all nets shall be
    explicitly declared. If a net is not explicitly declared,
    an error is generated.

This seems a much more sensible default for Clash.

clash-lib/src/Clash/Backend/SystemVerilog.hs
@@ -217,12 +217,14 @@ genSystemVerilog _ sp seen c = do
   where
     cName   = componentName c
     verilog = commentHeader <> line <>
+              nettype <> line <>
               timescale <> line <>
               module_ c
     commentHeader
          = "/* AUTOMATICALLY GENERATED SYSTEMVERILOG-2005 SOURCE CODE."
       <> line <> "** GENERATED BY CLASH " <> string (Text.pack clashVer) <> ". DO NOT MODIFY."
       <> line <> "*/"
+    nettype = "`default_nettype none"
     timescale = "`timescale 100fs/100fs"
 
 -- | Generate a SystemVerilog package containing type definitions for the given HWTypes
@@ -524,11 +526,19 @@ module_ c =
   inPorts  = sequence [ sigPort (Nothing,isBiSignalIn ty) (i,ty) Nothing | (i,ty)  <- inputs c  ]
   outPorts = sequence [ sigPort (Just wr,False) p iEM | (wr, p, iEM) <- outputs c ]
 
+  -- NOTE [net types and data types]
+  --
+  -- SystemVerilog makes a distinction between the type of a net and the data
+  -- type of a signal. For output ports / inout ports this is fine, as there
+  -- is only one possible type. For input ports when using `default_nettype none
+  -- we have to specify the net type as wire explicitly (or vendor tools will
+  -- claim the net from the port declaration is implicitly defined).
+
   wr2ty (Nothing,isBidirectional)
     | isBidirectional
     = "inout"
     | otherwise
-    = "input"
+    = "input wire" -- See NOTE [net types and data types]
   wr2ty (Just _,_)
     = "output"
 

clash-lib/src/Clash/Backend/Verilog.hs
@@ -210,7 +210,7 @@ genVerilog sp seen c = do
     Ap $ idSeen %= Id.union seen
 
     Ap (setSrcSpan sp)
-    v    <- commentHeader <> line <> timescale <> line <> module_ c
+    v    <- commentHeader <> line <> nettype <> line <> timescale <> line <> module_ c
     incs <- Ap $ use includes
     return ((TextS.unpack (Id.toText cName), v), incs)
   where
@@ -219,6 +219,7 @@ genVerilog sp seen c = do
          = "/* AUTOMATICALLY GENERATED VERILOG-2001 SOURCE CODE."
       <> line <> "** GENERATED BY CLASH " <> string (Text.pack clashVer) <> ". DO NOT MODIFY."
       <> line <> "*/"
+    nettype = "`default_nettype none"
     timescale = "`timescale 100fs/100fs"
 
 sigPort

tests/shouldwork/Issues/T1171.hs
@@ -55,4 +55,4 @@ mainSystemVerilog = do
   [topDir] <- getArgs
   content  <- readFile (topDir </> show 'f </> "f.sv")
 
-  assertIn "input logic en  // enable" content
+  assertIn "input wire logic en  // enable" content

tests/shouldwork/SynthesisAttributes/T1771.hs
@@ -63,6 +63,6 @@ mainSystemVerilog = do
   [topDir] <- getArgs
   content <- readFile (topDir </> show 'topEntity </> "topEntity.sv")
 
-  assertIn "(* foo = \"AA\" *) input logic [0:0] pin_a" content
-  assertIn "(* foo = \"BB\", bar = 1 *) input logic [0:0] pin_b" content
-  assertIn "(* foo = \"CC\" *) input logic [0:0] pin_c" content
+  assertIn "(* foo = \"AA\" *) input wire logic [0:0] pin_a" content
+  assertIn "(* foo = \"BB\", bar = 1 *) input wire logic [0:0] pin_b" content
+  assertIn "(* foo = \"CC\" *) input wire logic [0:0] pin_c" content

commit daf71518aa6b215c7ed81c5fb7eb10789a09e589

Always include the net type wire in (System)Verilog
When declaring input ports / inout ports, the net type still has
to be specified as there are multiple valid net types (it just so
happens that Clash only cares about producing wires). This fixes
an unintended mistake introduced in #2174.

clash-lib/src/Clash/Backend/SystemVerilog.hs
@@ -536,7 +536,7 @@ module_ c =
 
   wr2ty (Nothing,isBidirectional)
     | isBidirectional
-    = "inout"
+    = "inout" -- no net type here, it gets added by verilogType.
     | otherwise
     = "input wire" -- See NOTE [net types and data types]
   wr2ty (Just _,_)

clash-lib/src/Clash/Backend/Verilog.hs
@@ -233,11 +233,53 @@ sigPort wor (Id.toText -> pName) hwType iEM =
       (portType <+> verilogType hwType <+> stringS pName <> iE <> encodingNote hwType)
   where
     portType = case wor of
-                 Nothing   -> if isBiSignalIn hwType then "inout" else "input"
+                 -- See NOTE [net and variable ports]
+                 Nothing   -> if isBiSignalIn hwType then "inout wire" else "input wire"
                  Just Wire -> "output" <+> "wire"
                  Just Reg  -> "output" <+> "reg"
 
     iE = maybe emptyDoc (noEmptyInit . expr_ False) iEM
+
+{-
+NOTE [net and variable ports]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+In Verilog, ports are typically seen written with the default implicit net type
+of wire, i.e.
+
+  input foo,
+  output bar,
+
+which is really a shorthand for
+
+  input wire foo,
+  output wire bar,
+
+When we use `default_nettype none however, this is no longer allowed as all
+nets must be explicitly given their type. When generating code we must include
+the additional word wire (the net type).
+
+What is the benefit of this? When we have a default net type, any variable
+generated which is not declared is given an implicit declaration of this
+default type. This can obscure clash errors, i.e. if Clash generated
+
+  wire foo;
+  fo <= ...;
+
+Then simulators would act as though this was written:
+
+  wire foo;
+  wire fo;
+  fo <= ...;
+
+Which should never be the desired behaviour for code generated by Clash.
+
+One final point, Verilog allows output to also be a variable type, i.e.
+
+  output reg foo;
+
+If a port is an input or an inout then it can only be a net and not a variable
+according to the standard, so input reg and inout reg are impossible.
+-}
 
 module_ :: Component -> VerilogM Doc
 module_ c =

tests/shouldwork/Issues/T1171.hs
@@ -48,7 +48,7 @@ mainVerilog = do
   [topDir] <- getArgs
   content  <- readFile (topDir </> show 'f </> "f.v")
 
-  assertIn "input  en // enable" content
+  assertIn "input wire  en // enable" content
 
 mainSystemVerilog :: IO ()
 mainSystemVerilog = do

tests/shouldwork/SynthesisAttributes/T1771.hs
@@ -54,9 +54,9 @@ mainVerilog = do
   [topDir] <- getArgs
   content <- readFile (topDir </> show 'topEntity </> "topEntity.v")
 
-  assertIn "(* foo = \"AA\" *) input [0:0] pin_a" content
-  assertIn "(* foo = \"BB\", bar = 1 *) input [0:0] pin_b" content
-  assertIn "(* foo = \"CC\" *) input [0:0] pin_c" content
+  assertIn "(* foo = \"AA\" *) input wire [0:0] pin_a" content
+  assertIn "(* foo = \"BB\", bar = 1 *) input wire [0:0] pin_b" content
+  assertIn "(* foo = \"CC\" *) input wire [0:0] pin_c" content
 
 mainSystemVerilog :: IO ()
 mainSystemVerilog = do

tests/shouldwork/TopEntity/PortNamesWithSingletonVector.hs
@@ -46,11 +46,11 @@ mainVerilog = do
   content  <- readFile (topDir </> show 'topEntity </> "PortNamesWithSingletonVector_topEntity.v")
 
   mapM_ (`assertIn` content)
-    [ "input  inp0"
-    , "input [0:0] inp1"
-    , "input [0:0] inp2"
-    , "input [0:0] inp3"
-    , "input [0:0] inp4"
-    , "input [0:0] inp5"
-    , "input [0:0] inp6"
+    [ "input wire  inp0"
+    , "input wire [0:0] inp1"
+    , "input wire [0:0] inp2"
+    , "input wire [0:0] inp3"
+    , "input wire [0:0] inp4"
+    , "input wire [0:0] inp5"
+    , "input wire [0:0] inp6"
     ]