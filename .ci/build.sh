b58991a147466e267989a114d4f33c345c192c17
Martijn Bastiaan <martijn@qbaylogic.com>
2022-08-16 08:41:48 +0200

    CI: Don't transfer executables in artifacts
    This has been causing weird errors on CI where executables fail with:
    
        cannot open shared object file: No such file or directory
    
    while `ldd` prints some very weird output:
    
        libHSCabal-3.6.3.0-[..]-ghc8.10.7.so => not found
        libHSCabal-3.6.3.0-[..]-ghc8.10.7.so => /root/.cabal/store/[..]
    
    Building the executables when needed mitigates these problems, while
    reducing artifact sizes too.
    
    (cherry picked from commit 486d213d45c564cce246f777f698484ebe04aeec)

--- .ci/build.sh
+++ .ci/build.sh
@@ -22,5 +22,20 @@ if [[ "$GHC_HEAD" != "yes" ]]; then
 fi
 set -u
 
-# Build with default constraints
-cabal v2-build all --write-ghc-environment-files=always
+# Build compiler _libraries_, but not the executable. This makes sure the executable
+# is being built by the job that is going to use it too. In turn, this prevents
+# weird errors where executables cannot find some shared libraries.
+cabal v2-build \
+  lib:clash-prelude \
+  lib:clash-prelude-hedgehog \
+  lib:clash-lib \
+  lib:clash-lib-hedgehog \
+  lib:clash-ghc \
+  lib:clash-cores \
+  lib:clash-testsuite
+
+# Make sure all dependencies of all packages in the clash-compiler project are
+# compiled. This makes sure no future CI job is going to build it. Furthermore,
+# this command makes sure we transfer all the right dependencies as artifacts -
+# as they will end up in Cabal's 'plan.json'.
+cabal v2-build all --only-dependencies
