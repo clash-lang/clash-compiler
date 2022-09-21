d8965e604a7e3537da3047632a0ffa522ab806e2
Peter Lebbing <peter@digitalbrains.com>
2022-09-21 15:15:03 +0200

    Tweak some Stack / Stack CI settings
    - Pass `-Wall -Wcompat` to GHC when building with Stack (not just CI,
      also the `stack.yaml` in the root). These options only apply to the
      local packages, it will not change the GHC options for dependencies.
    - Pass `--pedantic` to Stack in CI runs
    
        This switch adds `-Wall -Werror` to the options passed to GHC.
        Options specified in `stack.yaml` (like `-Wcompat`) are still
        passed; it is cumulative. Future versions of Stack might add
        behavior to `--pedantic`.
    - Caching on GitHub Windows CI is broken in cache@v2:
      https://github.com/actions/cache/issues/666#issuecomment-1002945087
      cache@v3 exhibits the same issue, going back to cache@v1
    - Drop unneeded `extra-deps`
    - Use an LTS resolver for GitHub Stack CI with GHC 9.0.2, instead of a
      nightly
    - While editing these files anyway, bumping all resolvers to the latest
      LTS
    
    GitLab jobs `nix-build` and `debian-bindist` still do not have
    -Wall -Werror. This could be addressed in a later PR.

--- .ci/stack_build.sh
+++ .ci/stack_build.sh
@@ -5,4 +5,7 @@ apt update
 apt install wget -y
 wget -q https://get.haskellstack.org/ -O stack_install.sh
 sh stack_install.sh
-stack build -j${THREADS}
+# Note: the --pedantic switch adds -Wall -Werror to the options passed to GHC.
+# Options specified in stack.yaml (like -Wcompat) are still passed; it is
+# cumulative. Future versions of Stack might add behavior to --pedantic.
+stack build -j${THREADS} --pedantic
