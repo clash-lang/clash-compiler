b86464ef6f81fc1d7a63693c4a6101a0fd11668e
Leon Schoorl <leon@qbaylogic.com>
2023-01-20 14:40:14 +0100

    CI: fix the build-clash-dev job
    It was sometimes failing during CPP because `MIN_VERSION_prettyprinter` 
    wasn't defined.
    
    Such macros are automatically generated by GHC for every exposed 
    package.
    Which we do through cabal generating a .ghc.environment file.
    Because prettyprinter is only a dependency of clash-lib and not 
    clash-prelude, just building clash-prelude wasn't enough to expose 
    prettyprinter.
    By now building all clash-lib's dependencies this should now be fixed.
    But I don't fully understand why this even worked (sometimes) before.

--- .ci/build_clash_dev.sh
+++ .ci/build_clash_dev.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -xeou pipefail
 
+# Make sure all our deps are build, and listed in the .ghc.environment file
+cabal --write-ghc-environment-files=always v2-build --only-dependencies clash-lib
+
 # Check that clash-dev works
-cabal --write-ghc-environment-files=always v2-build clash-prelude
 echo "" > clash-dev.result
 echo 'main >> writeFile "clash-dev.result" "success"' | ./clash-dev
 if [[ "$(cat clash-dev.result)" != "success" ]]; then
