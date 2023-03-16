070a6d05d8a3698adfe55c6564bd6ef04d6d529b
Peter Lebbing <peter@digitalbrains.com>
2023-03-16 14:02:05 +0100

    Drop snap package (#2439)
    The Clash snap package has not been a recommended way to use Clash for
    quite some time, and it is a hassle to support.
    
    In order to build a snap package, we build .deb packages for Clash with
    Ubuntu 20.04 LTS. But the interaction between the Debian build system
    and GHC is problematic, requiring significant effort to support and to
    upgrade to a more recent Ubuntu release.
    
    Additionally, snap packages have their own issues on distributions other
    than Ubuntu. Given that we no longer recommend people use our snap
    package and given the effort required to keep supporting it, we have
    decided to drop the snap package.

--- .ci/bindist/linux/debian/go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x -euf -o pipefail
-
-cd $(dirname $0)
-
-docker run \
-  -v $(realpath ../../../..):/clash-compiler-src \
-  $(cat $1/buildinfo.json | jq -r '.docker') \
-  /clash-compiler-src/.ci/bindist/linux/debian/scripts/build.sh ${1}
