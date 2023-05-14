392ecf5a79dea79f61b26acbc33cd3a283ba4b60
Martijn Bastiaan <martijn@qbaylogic.com>
2023-05-14 09:10:16 +0200

    Add module documentation to `Clash.Cores.Xilinx.Xpm{.Cdc}`

--- clash-cores/src/Clash/Cores/Xilinx/Xpm.hs
+++ clash-cores/src/Clash/Cores/Xilinx/Xpm.hs
@@ -2,6 +2,10 @@
   Copyright   :  (C) 2023, Google LLC
   License     :  BSD2 (see the file LICENSE)
   Maintainer  :  QBayLogic B.V. <devops@qbaylogic.com>
+
+  A collection of Xilinx Parameterized Macros (XPMs) supported by all modern
+  Xilinx FPGA platforms, including  UltraScale and UltraScale+. More information
+  is available in [Xilinx's documentation](https://docs.xilinx.com/r/en-US/ug974-vivado-ultrascale-libraries/Xilinx-Parameterized-Macros).
 -}
 
 module Clash.Cores.Xilinx.Xpm
