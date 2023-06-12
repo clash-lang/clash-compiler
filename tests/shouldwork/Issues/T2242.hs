2ca2a5f29ba5cf30855fc1dd18a705f0d14e8fe5
Nadia Yvette Chambers <nadia.chambers@iohk.io>
2023-06-12 15:05:25 +0200

    Add back mainVHDL and mainSystemVerilog.

--- tests/shouldwork/Issues/T2242.hs
+++ tests/shouldwork/Issues/T2242.hs
@@ -2,6 +2,7 @@ module T2242 where
 import System.Environment
 import System.FilePath
 import Clash.Prelude
+
 topEntity ::
   HiddenClock System =>
   Signal System (Unsigned 8) ->
@@ -9,8 +10,25 @@ topEntity ::
 topEntity = dflipflop
 {-# NOINLINE topEntity #-}
 
+checkContent :: String -> IO ()
+checkContent content
+  | '$' `elem` content = error "Error: output contains $ symbol"
+  | otherwise = pure ()
+
+mainVHDL :: IO ()
+mainVHDL = do
+  [topDir] <- getArgs
+  content  <- readFile (topDir </> show 'topEntity </> "topEntity.vhdl")
+  checkContent content
+
 mainVerilog :: IO ()
 mainVerilog = do
   [topDir] <- getArgs
-  content <- readFile $ topDir </> show 'topEntity </> "topEntity.v"
-  if '$' `elem` content then error "Error: output contains $ symbol" else pure ()
+  content  <- readFile (topDir </> show 'topEntity </> "topEntity.v")
+  checkContent content
+
+mainSystemVerilog :: IO ()
+mainSystemVerilog = do
+  [topDir] <- getArgs
+  content  <- readFile (topDir </> show 'topEntity </> "topEntity.sv")
+  checkContent content
