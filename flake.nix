149885cef097ecfdb910797f73f0c1f54fc5b191
Vekhir <134215107+Vekhir@users.noreply.github.com>
2023-09-21 17:34:12 +0200

    Support Aeson 2.2 (#2578)
    Update nixpkgs to the latest unstable
    
    Co-authored-by: rowanG077 <goemansrowan@gmail.com>
    Co-authored-by: Peter Lebbing <peter@digitalbrains.com>

--- flake.nix
+++ flake.nix
@@ -36,16 +36,16 @@
       # The versions of GHC that we want to be able to build / develop against
       # within the nix environment. Since nix is lazy, only derivations for
       # versions of GHC which are used are actually evaluated.
-      ghcVersions = [ "ghc902" "ghc961" ];
+      ghcVersions = [ "ghc902" "ghc962" ];
 
       # We pick a single version of GHC to use by default within nix. This is
       # probably cleaner than always having N copies of each package / app and
       # being forced to refer to them by their GHC version.
-      defaultGhcVersion = "ghc961";
+      defaultGhcVersion = "ghc962";
 
       # Overlays are not per-system, so let's only compute them once.
       # For each version of GHC we produce a `pkgs.clashPackages-ghcVER`, e.g.
-      # `pkgs.clashPackages-ghc961`.
+      # `pkgs.clashPackages-ghc962`.
       overlays =
         let
           makeOverlay =
