d6a23859de781edcb2df0e90d40c7363df41f0d4
Leon Schoorl <leon@qbaylogic.com>
2023-11-30 17:18:00 +0100

    Fix test XpmCdcSingle
    Previously its test signal was:
      fromList [0..] :: Signal dom (Unsigned 1)
    which evaluates to 0 :> 1 :> errorX "finite list".
    When that flows though sample . fmap pack . unsafeSynchronizer it turns
    into
      [0b0, 0b1, 0b., 0b., 0b., ...

--- tests/shouldwork/Cores/Xilinx/XpmTestCommon.hs
+++ tests/shouldwork/Cores/Xilinx/XpmTestCommon.hs
@@ -1,8 +1,28 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -fforce-recomp #-}
 module XpmTestCommon where
 
 import Clash.Explicit.Prelude
+import Language.Haskell.TH (runIO)
+import System.Random
+
 
 createDomain vXilinxSystem{vName="D3",  vPeriod=hzToPeriod 30e6}
 createDomain vXilinxSystem{vName="D5",  vPeriod=hzToPeriod 50e6}
 createDomain vXilinxSystem{vName="D10", vPeriod=hzToPeriod 100e6}
 createDomain vXilinxSystem{vName="D11", vPeriod=hzToPeriod 110e6}
+
+randomSeed :: Int
+randomSeed = $(runIO (randomIO @Int) >>= lift)
+
+genTestData :: forall dom a z. (KnownDomain dom, BitPack a, (BitSize a + z) ~ 64) => Int -> Clock dom -> Signal dom a
+genTestData seed clk = (unpack . truncateB @BitVector @(BitSize a) . pack) <$> out
+ where
+  (out,gen) = unbundle $ genWord64 <$> delay clk enableGen (mkStdGen seed) gen
+
+-- dummy implementation
+instance NFDataX StdGen where
+  deepErrorX = errorX
+  hasUndefined = const False
+  ensureSpine = id
+  rnfX = const ()
