eafdbd9325a4bcbd598f52ccfd87421b53b91779
Leon Schoorl <leon@qbaylogic.com>
2023-12-20 16:08:44 +0100

    fixup XpmCdcPulse test

--- tests/shouldwork/Cores/Xilinx/XpmCdcPulse.hs
+++ tests/shouldwork/Cores/Xilinx/XpmCdcPulse.hs
@@ -15,56 +15,56 @@ topEntity = 0
 
 tb0 = done
  where
-  --                          src dst stages  samples              init  reg
-  done =             Types.tb @D3 @D5 @4      @100     Proxy Proxy False False SNat expected
-  expected = $(Types.expected @D3 @D5 @4      @100     Proxy Proxy False False SNat SNat)
+  --                          src dst stages  samples              init  reg   rstUsed
+  done =             Types.tb @D3 @D5 @4      @100     Proxy Proxy False False True SNat expected
+  expected = $(Types.expected @D3 @D5 @4      @100     Proxy Proxy False False True SNat SNat)
 {-# ANN tb0 (TestBench 'topEntity) #-}
 
 tb1 = done
  where
-  --                          src dst stages  samples              init  reg
-  done =             Types.tb @D5 @D3 @4      @100     Proxy Proxy False True SNat expected
-  expected = $(Types.expected @D5 @D3 @4      @100     Proxy Proxy False True SNat SNat)
+  --                          src dst stages  samples              init  reg  rstUsed
+  done =             Types.tb @D5 @D3 @4      @100     Proxy Proxy False True True SNat expected
+  expected = $(Types.expected @D5 @D3 @4      @100     Proxy Proxy False True True SNat SNat)
 {-# ANN tb1 (TestBench 'topEntity) #-}
 
 tb2 = done
  where
-  --                          src dst stages  samples              init reg
-  done =             Types.tb @D3 @D5 @10     @100     Proxy Proxy True False SNat expected
-  expected = $(Types.expected @D3 @D5 @10     @100     Proxy Proxy True False SNat SNat)
+  --                          src dst stages  samples              init reg   rstUsed
+  done =             Types.tb @D3 @D5 @10     @100     Proxy Proxy True False False SNat expected
+  expected = $(Types.expected @D3 @D5 @10     @100     Proxy Proxy True False False SNat SNat)
 {-# ANN tb2 (TestBench 'topEntity) #-}
 
 tb3 = done
  where
-  --                          src dst stages  samples              init reg
-  done =             Types.tb @D3 @D5 @2      @100     Proxy Proxy True True SNat expected
-  expected = $(Types.expected @D3 @D5 @2      @100     Proxy Proxy True True SNat SNat)
+  --                          src dst stages  samples              init reg  rstUsed
+  done =             Types.tb @D3 @D5 @2      @100     Proxy Proxy True True True SNat expected
+  expected = $(Types.expected @D3 @D5 @2      @100     Proxy Proxy True True True SNat SNat)
 {-# ANN tb3 (TestBench 'topEntity) #-}
 
 tb4 = done
  where
-  --                          src dst  stages  samples              init  reg
-  done =             Types.tb @D5 @D10 @2      @100     Proxy Proxy False False SNat expected
-  expected = $(Types.expected @D5 @D10 @2      @100     Proxy Proxy False False SNat SNat)
+  --                          src dst  stages  samples              init  reg   rstUsed
+  done =             Types.tb @D5 @D10 @2      @100     Proxy Proxy False False True SNat expected
+  expected = $(Types.expected @D5 @D10 @2      @100     Proxy Proxy False False True SNat SNat)
 {-# ANN tb4 (TestBench 'topEntity) #-}
 
 tb5 = done
  where
-  --                          src dst  stages  samples              init  reg
-  done =             Types.tb @D10 @D5 @2      @100     Proxy Proxy False True SNat expected
-  expected = $(Types.expected @D10 @D5 @2      @100     Proxy Proxy False True SNat SNat)
+  --                          src dst  stages  samples              init  reg  rstUsed
+  done =             Types.tb @D10 @D5 @2      @100     Proxy Proxy False True True SNat expected
+  expected = $(Types.expected @D10 @D5 @2      @100     Proxy Proxy False True True SNat SNat)
 {-# ANN tb5 (TestBench 'topEntity) #-}
 
 tb6 = done
  where
-  --                          src dst  stages  samples              init reg
-  done =             Types.tb @D5 @D11 @2      @100     Proxy Proxy True False SNat expected
-  expected = $(Types.expected @D5 @D11 @2      @100     Proxy Proxy True False SNat SNat)
+  --                          src dst  stages  samples              init reg   rstUsed
+  done =             Types.tb @D5 @D11 @2      @100     Proxy Proxy True False True SNat expected
+  expected = $(Types.expected @D5 @D11 @2      @100     Proxy Proxy True False True SNat SNat)
 {-# ANN tb6 (TestBench 'topEntity) #-}
 
 tb7 = done
  where
   --                          src dst  stages  samples              init reg
-  done =             Types.tb @D11 @D5 @2      @100     Proxy Proxy True True SNat expected
-  expected = $(Types.expected @D11 @D5 @2      @100     Proxy Proxy True True SNat SNat)
+  done =             Types.tb @D11 @D5 @2      @100     Proxy Proxy True True True SNat expected
+  expected = $(Types.expected @D11 @D5 @2      @100     Proxy Proxy True True True SNat SNat)
 {-# ANN tb7 (TestBench 'topEntity) #-}
