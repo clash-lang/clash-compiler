2062e02a7ab850e7aa4502559ef3431e5976889c
Diego Diverio <diego@monads.fr>
2024-09-24 11:33:28 +0200

    Enable GHC 9.8.2 and remove GHC 9.0.2.

---
+++ nix/overlay-ghc98.nix
@@ -0,0 +1,21 @@
+{ pkgs }:
+final: prev:
+let
+  inherit (pkgs.haskell.lib) doJailbreak markUnbroken;
+in
+{
+  # Use an older version than the default in nixpkgs. Since rewrite-inspector
+  # is basically abandonware it catches fire with brick 1.0+.
+  brick = doJailbreak prev.brick_0_70_1;
+
+  vty = doJailbreak (prev.callHackage "vty" "5.39" { });
+
+  # Marked as broken in nixpkgs, since it specifies much older dependencies
+  # than the defaults in nixpkgs.
+  rewrite-inspector = doJailbreak (markUnbroken prev.rewrite-inspector);
+
+  derive-storable-plugin = doJailbreak prev.derive-storable-plugin;
+
+  # Marken as broken, but compiles anyway.
+  hedgehog-fakedata = doJailbreak (markUnbroken prev.hedgehog-fakedata);
+}
