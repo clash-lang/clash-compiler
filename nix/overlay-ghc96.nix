e35490aface43fc828c5b79ab1627795796fdf05
Diego Diverio <diego@monads.fr>
2024-09-24 15:40:58 +0200

    Remove Hackage calls for packages already in nixpkgs.

--- nix/overlay-ghc96.nix
+++ nix/overlay-ghc96.nix
@@ -30,24 +30,11 @@ in
   # than the defaults in nixpkgs.
   rewrite-inspector = doJailbreak (markUnbroken prev.rewrite-inspector);
 
-  # We want a version that matches with singletons-th, but the tests in here
-  # are also a bit flaky since GHC 9.6 isn't officially supported.
-  singletons-base = dontCheck (prev.callHackage "singletons-base" "3.2" { }); # dontCheck prev.singletons-base_3_2;
-
-  # Use a newer version than the default in nixpkgs.
-  singletons-th = prev.callHackage "singletons-th" "3.2" { }; # prev.singletons-th_3_2;
-
   # Needs a newer text than the .cabal file currently uploaded to hackage.
   string-qq = doJailbreak prev.string-qq;
 
-  # Needs a newer version than the default in nixpkgs.
-  th-desugar = prev.callHackage "th-desugar" "1.15" { };
-
   # Needs a newer base than the .cabal file currently uploaded to hackage.
   vector-binary-instances = doJailbreak prev.vector-binary-instances;
-
-  # Use an older version than the default in nixpkgs.
-  th-abstraction = prev.callHackage "th-abstraction" "0.5.0.0" {};
 
   # type-errors 0.2.0.2 is bounded on doctest >=0.16.0.1 && <0.22
   doctest = prev.callHackage "doctest" "0.21.1" { };
