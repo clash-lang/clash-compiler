c6bda10c9617345c621e015551b9d90a307799f5
Peter Lebbing <peter@digitalbrains.com>
2024-12-01 15:32:55 +0100

    DDR: Improve API, document
    The existing constraint can be redefined in terms of `DomainPeriod`.
    Since the virtual DDR domain only exists in simulation, it really
    doesn't matter if it doesn't match with the real domain in all its
    parameters, so we can drop those and make the constraints more intuitive
    to read.
    
    The primitives for Xilinx and Intel only support the rising edge as the
    active edge of the domain.
    
    The real domain of the functions was the `slow` domain, and the `fast`
    domain was the virtual domain. The new names `dom` and `domDDR`
    reinforce the fact that `dom` is the real domain.

--- clash-prelude/src/Clash/Explicit/DDR.hs
+++ clash-prelude/src/Clash/Explicit/DDR.hs
@@ -50,8 +50,9 @@ import Clash.Signal.Internal
 >>> import Clash.Explicit.Prelude
 >>> import Clash.Explicit.DDR
 >>> :{
-instance KnownDomain "Fast" where
-  type KnownConf "Fast" = 'DomainConfiguration "Fast" 5000 'Rising 'Asynchronous 'Defined 'ActiveHigh
+type DDR = "DDR" :: Domain
+instance KnownDomain "DDR" where
+  type KnownConf "DDR" = 'DomainConfiguration "DDR" 5000 'Rising 'Asynchronous 'Defined 'ActiveHigh
   knownDomain = SDomainConfiguration SSymbol SNat SRising SAsynchronous SDefined SActiveHigh
 :}
 
@@ -62,24 +63,37 @@ instance KnownDomain "Fast" where
 -- Consumes a DDR input signal and produces a regular signal containing a pair
 -- of values.
 --
--- >>> printX $ sampleN 5 $ ddrIn systemClockGen systemResetGen enableGen (-1,-2,-3) (fromList [0..10] :: Signal "Fast" Int)
+-- Data is clocked in on both edges of the clock signal. We can discern the
+-- /active edge/ of the clock and the /other edge/. When the domain has the
+-- rising edge as the active edge (which is the most common), this means that
+-- the /rising/ edge is the /active/ edge and the /falling/ edge is the /other/
+-- edge.
+--
+-- Of the output pair @(o0, o1)@, @o0@ is the data clocked in on the /other/
+-- edge and @o1@ is the data clocked in on the /active/ edge, and @o0@ comes
+-- before @o1@ in time. With a domain where the rising edge is the active edge,
+-- this means @o0@ is clocked in on the falling clock edge and @o1@ is clocked
+-- in on the rising clock edge. For a domain with the falling edge as the active
+-- edge, this is the other way around, but @o0@ still comes before @o1@ in time.
+--
+-- >>> sampleN 5 $ ddrIn @Int @System @DDR clockGen resetGen enableGen (-1,-2,-3) (fromList [0..10])
 -- [(-1,-2),(-1,-2),(-3,2),(3,4),(5,6)]
 ddrIn
-  :: ( HasCallStack
-     , NFDataX a
-     , KnownConfiguration fast ('DomainConfiguration fast fPeriod edge reset init polarity)
-     , KnownConfiguration slow ('DomainConfiguration slow (2*fPeriod) edge reset init polarity) )
-  => Clock slow
-  -- ^ clock
-  -> Reset slow
-  -- ^ reset
-  -> Enable slow
+  :: forall a dom domDDR
+   . HasCallStack
+  => NFDataX a
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
   -> (a, a, a)
-  -- ^ reset values
-  -> Signal fast a
+  -- ^ Reset values
+  -> Signal domDDR a
   -- ^ DDR input signal
-  -> Signal slow (a, a)
-  -- ^ normal speed output pairs
+  -> Signal dom (a, a)
+  -- ^ Normal speed output pair @(o0, o1)@
 ddrIn clk rst en (i0,i1,i2) =
   withFrozenCallStack $ ddrIn# clk rst en i0 i1 i2
 
@@ -87,21 +101,22 @@ ddrIn clk rst en (i0,i1,i2) =
 -- For details about all the seq's en seqX's
 -- see the [Note: register strictness annotations] in Clash.Signal.Internal
 ddrIn#
-  :: forall a slow fast fPeriod polarity edge reset init
-   . ( HasCallStack
-     , NFDataX a
-     , KnownConfiguration fast ('DomainConfiguration fast fPeriod edge reset init polarity)
-     , KnownConfiguration slow ('DomainConfiguration slow (2*fPeriod) edge reset init polarity) )
-  => Clock slow
-  -> Reset slow
-  -> Enable slow
+  :: forall a dom domDDR
+   . HasCallStack
+  => NFDataX a
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
   -> a
   -> a
   -> a
-  -> Signal fast a
-  -> Signal slow (a,a)
+  -> Signal domDDR a
+  -> Signal dom (a,a)
 ddrIn# (Clock _ Nothing) (unsafeToActiveHigh -> hRst) (fromEnable -> ena) i0 i1 i2 =
-  case resetKind @fast of
+  case resetKind @domDDR of
     SAsynchronous ->
       goAsync
         ( deepErrorX "ddrIn: initial value 0 undefined"
@@ -119,10 +134,10 @@ ddrIn# (Clock _ Nothing) (unsafeToActiveHigh -> hRst) (fromEnable -> ena) i0 i1 
   where
     goSync
       :: (a, a, a)
-      -> Signal slow Bool
-      -> Signal slow Bool
-      -> Signal fast a
-      -> Signal slow (a,a)
+      -> Signal dom Bool
+      -> Signal dom Bool
+      -> Signal domDDR a
+      -> Signal dom (a,a)
     goSync (o0,o1,o2) rt@(~(r :- rs)) ~(e :- es) as@(~(x0 :- x1 :- xs)) =
       let (o0',o1',o2') = if r then (i0,i1,i2) else (o2,x0,x1)
       in o0 `seqX` o1 `seqX` (o0,o1)
@@ -131,10 +146,10 @@ ddrIn# (Clock _ Nothing) (unsafeToActiveHigh -> hRst) (fromEnable -> ena) i0 i1 
 
     goAsync
       :: (a, a, a)
-      -> Signal slow Bool
-      -> Signal slow Bool
-      -> Signal fast a
-      -> Signal slow (a, a)
+      -> Signal dom Bool
+      -> Signal dom Bool
+      -> Signal domDDR a
+      -> Signal dom (a, a)
     goAsync (o0,o1,o2) ~(r :- rs) ~(e :- es) as@(~(x0 :- x1 :- xs)) =
       let (o0',o1',o2',o3',o4') = if r then (i0,i1,i0,i1,i2) else (o0,o1,o2,x0,x1)
       in o0' `seqX` o1' `seqX` (o0',o1')
@@ -151,38 +166,56 @@ ddrIn# _ _ _ _ _ _ =
 --
 -- Produces a DDR output signal from a normal signal of pairs of input.
 --
--- >>> sampleN 7 (ddrOut systemClockGen systemResetGen enableGen (-1) (fromList [(0,1),(2,3),(4,5)]) :: Signal "Fast" Int)
+-- Data is clocked out on both edges of the clock signal. We can discern the
+-- /active edge/ of the clock and the /other edge/. When the domain has the
+-- rising edge as the active edge (which is the most common), this means that
+-- the /rising/ edge is the /active/ edge and the /falling/ edge is the /other/
+-- edge.
+--
+-- Of the input pair @(i0, i1)@, @i0@ is the data clocked out on the /active/
+-- edge and @i1@ is the data clocked out on the /other/ edge, and @i0@ comes
+-- before @i1@ in time. With a domain where the rising edge is the active edge,
+-- this means @i0@ is clocked out on the rising clock edge and @i1@ is clocked
+-- out on the falling clock edge. For a domain with the falling edge as the
+-- active edge, this is the other way around, but @i0@ still comes before @i1@
+-- in time.
+--
+-- >>> sampleN 7 (ddrOut @Int @System @DDR clockGen resetGen enableGen (-1) (fromList [(0,1),(2,3),(4,5)]))
 -- [-1,-1,-1,2,3,4,5]
 ddrOut
-  :: ( HasCallStack
-     , NFDataX a
-     , KnownConfiguration fast ('DomainConfiguration fast fPeriod edge reset init polarity)
-     , KnownConfiguration slow ('DomainConfiguration slow (2*fPeriod) edge reset init polarity) )
-  => Clock slow
-  -> Reset slow
-  -> Enable slow
+  :: forall a dom domDDR
+   . HasCallStack
+  => NFDataX a
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
   -> a
-  -- ^ reset value
-  -> Signal slow (a, a)
-  -- ^ Normal speed input pairs
-  -> Signal fast a
+  -- ^ Reset value
+  -> Signal dom (a, a)
+  -- ^ Normal speed input pair @(i0, i1)@
+  -> Signal domDDR a
   -- ^ DDR output signal
 ddrOut clk rst en i0 =
   uncurry (withFrozenCallStack $ ddrOut# clk rst en i0) . unbundle
 
 
 ddrOut#
-  :: ( HasCallStack
-     , NFDataX a
-     , KnownConfiguration fast ('DomainConfiguration fast fPeriod edge reset init polarity)
-     , KnownConfiguration slow ('DomainConfiguration slow (2*fPeriod) edge reset init polarity) )
-  => Clock slow
-  -> Reset slow
-  -> Enable slow
+  :: forall a dom domDDR
+   . HasCallStack
+  => NFDataX a
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
   -> a
-  -> Signal slow a
-  -> Signal slow a
-  -> Signal fast a
+  -> Signal dom a
+  -> Signal dom a
+  -> Signal domDDR a
 ddrOut# clk rst en i0 xs ys =
     -- We only observe one reset value, because when the mux switches on the
     -- next clock level, the second register will already be outputting its
--- clash-prelude/src/Clash/Xilinx/DDR.hs
+++ clash-prelude/src/Clash/Xilinx/DDR.hs
@@ -10,8 +10,8 @@ For general information about DDR primitives see "Clash.Explicit.DDR".
 For more information about the Xilinx DDR primitives see:
 
 * Vivado Design Suite 7 Series FPGA and Zynq-7000 All Programmable SoC
-  Libraries Guide, UG953 (v2018.3) December 5, 2018, p371-373,p481-483,
-  <https://www.xilinx.com/support/documentation/sw_manuals/xilinx2018_3/ug953-vivado-7series-libraries.pdf>
+  Libraries Guide, UG953 (v2022.2) October 19, 2022, p369-371, p477-479,
+  <https://www.xilinx.com/content/dam/xilinx/support/documents/sw_manuals/xilinx2022_2/ug953-vivado-7series-libraries.pdf>
 -}
 
 {-# LANGUAGE CPP #-}
@@ -21,9 +21,13 @@ For more information about the Xilinx DDR primitives see:
 module Clash.Xilinx.DDR
   ( iddr
   , oddr
+    -- * Internal
+  , iddr#
+  , oddr#
   )
 where
 
+import Data.Bifunctor
 import GHC.Stack (HasCallStack, withFrozenCallStack)
 
 import Clash.Annotations.Primitive (hasBlackBox)
@@ -34,56 +38,92 @@ import Clash.Explicit.DDR
 -- primitive in @SAME_EDGE@ mode.
 --
 -- Reset values are @0@
+--
+-- Of the output pair @(o0, o1)@, @o0@ is the data clocked in on the /falling/
+-- edge and @o1@ is the data clocked in on the /rising/ edge, and @o0@ comes
+-- before @o1@ in time.
+--
+-- __NB__: This primitive only supports rising edges as the active edge.
 iddr
-  :: ( HasCallStack
-     , KnownConfiguration fast ('DomainConfiguration fast fPeriod edge reset init polarity)
-     , KnownConfiguration slow ('DomainConfiguration slow (2*fPeriod) edge reset init polarity)
-     , KnownNat m )
-  => Clock slow
-  -- ^ clock
-  -> Reset slow
-  -- ^ reset
-  -> Enable slow
-  -- ^ global enable
-  -> Signal fast (BitVector m)
+  :: forall a dom domDDR
+   . HasCallStack
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => DomainActiveEdge dom ~ 'Rising
+  => BitPack a
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
+  -> Signal domDDR a
   -- ^ DDR input signal
-  -> Signal slow ((BitVector m),(BitVector m))
-  -- ^ normal speed output pairs
-iddr clk rst en = withFrozenCallStack ddrIn# clk rst en 0 0 0
+  -> Signal dom (a, a)
+  -- ^ Normal speed output pair @(o0, o1)@
+iddr clk rst en =
+  fmap (bimap unpack unpack) . withFrozenCallStack (iddr# clk rst en) .
+    fmap pack
+
+iddr#
+  :: forall n dom domDDR
+   . HasCallStack
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => DomainActiveEdge dom ~ 'Rising
+  => KnownNat n
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
+  -> Signal domDDR (BitVector n)
+  -> Signal dom (BitVector n, BitVector n)
+iddr# clk rst en = withFrozenCallStack ddrIn# clk rst en 0 0 0
 -- See: https://github.com/clash-lang/clash-compiler/pull/2511
-{-# CLASH_OPAQUE iddr #-}
-{-# ANN iddr hasBlackBox #-}
+{-# CLASH_OPAQUE iddr# #-}
+{-# ANN iddr# hasBlackBox #-}
 
 -- | Xilinx specific variant of 'ddrOut' implemented using the Xilinx ODDR
 -- primitive in @SAME_EDGE@ mode.
 --
 -- Reset value is @0@
+--
+-- Of the input pair @(i0, i1)@, @i0@ is the data clocked out on the /rising/
+-- edge and @i1@ is the data clocked out on the /falling/ edge, and @i0@ comes
+-- before @i1@ in time.
+--
+-- __NB__: This primitive only supports rising edges as the active edge.
 oddr
-  :: ( KnownConfiguration fast ('DomainConfiguration fast fPeriod edge reset init polarity)
-     , KnownConfiguration slow ('DomainConfiguration slow (2*fPeriod) edge reset init polarity)
-     , KnownNat m )
-  => Clock slow
-  -- ^ clock
-  -> Reset slow
-  -- ^ reset
-  -> Enable slow
-  -- ^ global enable
-  -> Signal slow (BitVector m, BitVector m)
-  -- ^ normal speed input pairs
-  -> Signal fast (BitVector m)
+  :: forall a dom domDDR
+   . HasCallStack
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => DomainActiveEdge dom ~ 'Rising
+  => BitPack a
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
+  -> Signal dom (a, a)
+  -- ^ Normal speed input pair @(i0, i1)@
+  -> Signal domDDR a
   -- ^ DDR output signal
-oddr clk rst en = uncurry (withFrozenCallStack oddr# clk rst en) . unbundle
+oddr clk rst en =
+  fmap unpack . uncurry (withFrozenCallStack oddr# clk rst en) . unbundle .
+    fmap (bimap pack pack)
 
 oddr#
-  :: ( KnownConfiguration fast ('DomainConfiguration fast fPeriod edge reset init polarity)
-     , KnownConfiguration slow ('DomainConfiguration slow (2*fPeriod) edge reset init polarity)
-     , KnownNat m )
-  => Clock slow
-  -> Reset slow
-  -> Enable slow
-  -> Signal slow (BitVector m)
-  -> Signal slow (BitVector m)
-  -> Signal fast (BitVector m)
+  :: forall n dom domDDR
+   . HasCallStack
+  => KnownDomain dom
+  => KnownDomain domDDR
+  => DomainPeriod dom ~ (2 * DomainPeriod domDDR)
+  => DomainActiveEdge dom ~ 'Rising
+  => KnownNat n
+  => Clock dom
+  -> Reset dom
+  -> Enable dom
+  -> Signal dom (BitVector n)
+  -> Signal dom (BitVector n)
+  -> Signal domDDR (BitVector n)
 oddr# clk rst en = ddrOut# clk rst en 0
 -- See: https://github.com/clash-lang/clash-compiler/pull/2511
 {-# CLASH_OPAQUE oddr# #-}
