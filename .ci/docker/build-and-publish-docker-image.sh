9d94353c3a8a2f5b15144e550803a7287cb1f828
Peter Lebbing <peter@digitalbrains.com>
2025-03-06 12:22:49 +0100

    CI: Downgrade Cabal for GHC 8.10.7 (#2899)
    Cabal 3.14 silently ignores `haddock-options` in `cabal.project`. The
    change is deliberate (though not warning about it probably is not). The
    change is documented in some places:
    
    https://github.com/haskell/cabal/blob/master/release-notes/cabal-install-3.14.0.0.md
    https://github.com/haskell/cabal/blob/master/release-notes/WIP-Cabal-3.12.x.0.md
    https://github.com/haskell/cabal/pull/9177
    
    (Note that the deprecation message apparently never made it to the
    release notes)
    
    We should probably think about how to fix this in the future, but for
    now rolling back to Cabal 3.12 will allow us to build the documentation
    as we want to again. The mentioned `haddock-version-cpp` flag in the
    linked cabal PR 9177 above  to get a CPP define for
    `__HADDOCK_VERSION__` did not work at all for me.
    
    PR #2201 was supposed to update our Haddock generation to 9.0.2 but
    accidentally picked 8.10.7 for the uploads to Hackage, which also fixed
    issue #2200. I'm careful about upgrading Haddock now as I've experienced
    several times now that there were undocumented changes going from one
    version to the next that broke some rendering, and carefully rereading
    our complete Haddock to scan for such breakage is an impossible task.
    One day we'll have to upgrade, when we drop GHC 8.10 from Clash.

--- .ci/docker/build-and-publish-docker-image.sh
+++ .ci/docker/build-and-publish-docker-image.sh
@@ -13,10 +13,9 @@ elif [[ "$1" != "" ]]; then
   echo "Unrecognized argument: $1" >&2
   exit 1
 fi
-
-UBUNTU_VERSION=jammy-20240808
+UBUNTU_VERSION=jammy-20250126
 GHC_VERSIONS=("9.10.1" "9.8.4"  "9.6.6"  "9.4.8"  "9.2.8"   "9.0.2"   "8.10.7")
-CABAL_VERSION="3.14.1.0"
+CABAL_VERSIONS=("3.14.1.1" "3.14.1.1" "3.14.1.1" "3.14.1.1" "3.14.1.1" "3.14.1.1" "3.12.1.0")
 
 # We want to use docker buildkit so that our layers are built in parallel. This
 # is ignored completely on versions of docker which don't support buildkit.
@@ -25,6 +24,7 @@ export DOCKER_BUILDKIT=1
 for i in "${!GHC_VERSIONS[@]}"
 do
   GHC_VERSION="${GHC_VERSIONS[i]}"
+  CABAL_VERSION="${CABAL_VERSIONS[i]}"
 
   docker build \
     --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
