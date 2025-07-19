2c8beef8e18f51679a6fe4f0f571b39d88356c25
Martijn Bastiaan <martijn@qbaylogic.com>
2025-07-19 12:22:11 +0200

    Bump bounds for `ghc-prim` in `clash-{prelude,ghc}`
    (cherry picked from commit b043b6ad5f59df706cabd5dc8e3b721f7eb047f3)

--- .ci/docker/build-and-publish-docker-image.sh
+++ .ci/docker/build-and-publish-docker-image.sh
@@ -14,8 +14,8 @@ elif [[ "$1" != "" ]]; then
   exit 1
 fi
 
-UBUNTU_VERSION=jammy-20250126
-GHC_VERSIONS=("9.10.1" "9.8.4" "9.6.6" "9.4.8" "9.2.8" "9.0.2" "8.10.7" "8.8.4" "8.6.5")
+UBUNTU_VERSION=jammy-20250819
+GHC_VERSIONS=("9.10.2" "9.8.4" "9.6.6" "9.4.8" "9.2.8" "9.0.2" "8.10.7" "8.8.4" "8.6.5")
 CABAL_VERSIONS=("3.14.1.1" "3.14.1.1" "3.14.1.1" "3.14.1.1" "3.14.1.1" "3.14.1.1" "3.12.1.0" "3.14.1.1" "3.14.1.1")
 
 # We want to use docker buildkit so that our layers are built in parallel. This
