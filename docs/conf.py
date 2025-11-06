8bb5020de793355f5efa3adabf58699a0d1beba0
Peter Lebbing <peter@digitalbrains.com>
2025-11-06 11:04:41 +0100

    Up version number

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ master_doc = 'index'
 project = 'Clash'
 copyright = '2017-2019, The Clash Developers'
 author = 'The Clash Developers'
-version = '1.8.3'
+version = '1.8.4'
 release = version
 
 # Syntax highlighting
