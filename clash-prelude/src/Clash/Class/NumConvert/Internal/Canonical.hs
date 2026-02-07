af86afbe1deb3ee333df6b02c7123b3a8711a9f0
Martijn Bastiaan <martijn@qbaylogic.com>
2026-02-07 12:35:21 +0100

    Fix overlapping instances in `NumConvert` with `Canonical`
    Previously, conversions such as:
    
        f :: Word32 -> Word64
        f = numConvert
    
    would fail with an "overlapping instances" error, because it could match
    on both `NumConvert a Word64` and `NumConvert Word32 a`. This can't be
    solved through `OVERLAPPING` pragmas (and friends), because neither is
    more specific than the other.
    
    To fix this we make the intuition that some types are "equal" to others
    explicit using a `Canonical` type family. This can then be used in
    `numConvertVia` and `maybeNumConvertVia`.
    
    Fixes #2986

---
+++ clash-prelude/src/Clash/Class/NumConvert/Internal/Canonical.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_HADDOCK hide #-}
+
+#include "MachDeps.h"
+
+{- |
+Copyright  :  (C) 2026     , Martijn Bastiaan
+License    :  BSD2 (see the file LICENSE)
+Maintainer :  QBayLogic B.V. <devops@qbaylogic.com>
+-}
+module Clash.Class.NumConvert.Internal.Canonical where
+
+import Clash.Sized.BitVector
+import Clash.Sized.Index
+import Clash.Sized.Signed
+import Clash.Sized.Unsigned
+
+import Data.Int (Int16, Int32, Int64, Int8)
+import Data.Word (Word16, Word32, Word64, Word8)
+
+-- | Type family mapping types to their canonical \"unwrapped\" Clash form.
+-- This is used by 'Clash.Class.NumConvert.numConvertVia' and
+-- 'Clash.Class.NumConvert.maybeNumConvertVia' to determine the intermediate type.
+type family Canonical a
+
+-- Instances for Data.Word types
+type instance Canonical Word = Unsigned WORD_SIZE_IN_BITS
+type instance Canonical Word64 = Unsigned 64
+type instance Canonical Word32 = Unsigned 32
+type instance Canonical Word16 = Unsigned 16
+type instance Canonical Word8 = Unsigned 8
+
+-- Instances for Data.Int types
+type instance Canonical Int = Signed WORD_SIZE_IN_BITS
+type instance Canonical Int64 = Signed 64
+type instance Canonical Int32 = Signed 32
+type instance Canonical Int16 = Signed 16
+type instance Canonical Int8 = Signed 8
+
+-- Bit
+type instance Canonical Bit = BitVector 1
+
+-- Clash types are already in canonical form
+type instance Canonical (Unsigned n) = Unsigned n
+type instance Canonical (Signed n) = Signed n
+type instance Canonical (BitVector n) = BitVector n
+type instance Canonical (Index n) = Index n
