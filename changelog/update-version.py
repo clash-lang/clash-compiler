070655d37885ae643155681621a583d7e52b3867
The-Redstar <eismquantum2@gmail.com>
2026-03-10 17:45:57 +0100

    update-version now also replaces dependencies

--- changelog/update-version.py
+++ changelog/update-version.py
@@ -7,11 +7,13 @@ os.chdir("..")
 while True:
   version = input("New version: ")
   conf = input(f"Changing to version {version}. Confirm? [Y/n]: ")
-  if conf=="Y":
-    break
+  if conf in ["n","N","no"]:
+    continue
+  break
 
 cabal = re.compile(r"(^[Vv]ersion:\s*)(.*)()",re.MULTILINE)
 py = re.compile(r"(^version = \')(.*)(\')",re.MULTILINE)
+dep = re.compile(r"(clash-prelude|clash-lib)(\s*== )([\d.]+)(,)")
 
 for fname,pat in [
     ("clash-prelude/clash-prelude.cabal",cabal),
@@ -23,6 +25,7 @@ for fname,pat in [
 ]:
     with open(fname,"r+") as fp:
         contents = fp.read()
+        contents = dep.sub(r"\g<1>\g<2>"+version+r"\g<4>",contents)
         if pat.search(contents):
             contents = pat.sub(r"\g<1>"+version+r"\g<3>",contents,count=1)
             fp.seek(0)
