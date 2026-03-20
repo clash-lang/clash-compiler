83e906547b38a3d8bdeaf5ec839507f3478812fb
Peter Lebbing <peter@digitalbrains.com>
2026-03-20 14:59:49 +0100

    Create unit test for `dumpVCD` (#3156)
    Check timing of individual signals when tracing multiple domains
    
    If we have multiple signals, each with their own period, that change
    every sample, does the resulting VCD contain all the samples at the
    right times, and do timestamps and value changes strictly alternate?
    
    This test would have caught the issue that PR #3076 fixed, and serves as
    a good basic sanity check of VCD generation.
    
    A set of ADT's is added that represent all parts of a VCD file that
    contain non-trivial data. The ADT's are based on the specified VCD file
    syntax in IEEE Std. 1364-2001 (the Verilog standard), which defines the
    structure of VCD files. The ADT's (and their naming) correspond to the
    names and definitions in section 18.2.1 of the standard; however the
    ADT's are simplified to only those parts we use, and folding some parts
    into other parts. However, given the basic structure of the `VCDFile`
    type, we can trivially extend the types to cover more if we add more
    functionality. This is also why we keep the `[DeclarationCommand]` in
    `VCDFile` even though what we currently output is always fixed and
    direct fields make more sense from that point of view.
    
    `dumpVCD##` is split in such a way that the new `dumpVCD1#` constructs a
    `VCDFile` structure and `dumpVCD0#` turns this into a VCD text file.
    This allows us to unit test `dumpVCD1#` because all interesting
    behavior is there and we don't need to parse a VCD text file to check
    for correctness.

--- clash-prelude/tests/Hedgehog/Extra.hs
+++ clash-prelude/tests/Hedgehog/Extra.hs
@@ -3,13 +3,18 @@
 module Hedgehog.Extra
   ( throwsException
   , throwsDeepException
+  , LockstepWalk(..)
+  , lockstepWalk
+  , combinations
   ) where
 
 import Control.DeepSeq (NFData, force)
 
-import Hedgehog (failure, MonadTest, success)
+import Hedgehog (failure, MonadGen, MonadTest, success)
 import Hedgehog.Internal.Exception (tryEvaluate)
 import Hedgehog.Internal.Source (HasCallStack, withFrozenCallStack)
+
+import qualified Hedgehog.Gen as Gen
 
 throwsException
   :: ( MonadTest m
@@ -31,3 +36,70 @@ throwsDeepException
   -> m ()
 throwsDeepException =
   throwsException . force
+
+{- | Test individual samples of a list through 'lockstepWalk'
+
+@m@ is expected to be a 'MonadTest' monad.
+
+The function gets a boolean indicating if this is the last sample in the list, plus the
+current sample. It should return the function to test the next sample with.
+-}
+newtype LockstepWalk m a
+  = LockstepWalk {unLockstepWalk :: Bool -> a -> m (LockstepWalk m a)}
+
+{- | Walk a lazy list testing multiple properties
+
+For each sample, multiple properties are tested. This way multiple tests can share a
+walk of the list instead of the usual order where the first test would evaluate the
+list fully even before the second test runs.
+-}
+lockstepWalk ::
+  forall m a.
+  (MonadTest m) =>
+  [LockstepWalk m a] ->
+  [a] ->
+  m ()
+lockstepWalk _ [] = pure ()
+lockstepWalk ts (a : as) = do
+  ts0 <- sequence $ map (\t -> unLockstepWalk t (null as) a) ts
+  lockstepWalk ts0 as
+
+{- | Generate combinations of k elements from a list
+
+The generator shrinks towards the start of the list.
+-}
+
+{-
+To generate combinations, if we consider the first element, we note that we can either
+pick it or skip it. From the n choose k combinations, there are (n-1) choose (k-1)
+combinations where we picked the element, and (n-1) choose k combinations where we skipped
+the element. So let's define
+
+pick = (n-1) choose (k-1)
+skip = (n-1) choose k
+
+We only need to know the ratio pick:skip. Working out the binomial coefficients, we come
+to the following equivalent ratio:
+
+pick' = k
+skip' = n-k
+-}
+combinations ::
+  (MonadGen m) =>
+  [a] ->
+  Int ->
+  m [a]
+combinations es k
+  | k < 0 = error $ "combinations: impossible, k < 0. k = " <> show k
+  | otherwise = combinations0 n k es
+ where
+  n = length es
+  combinations0 _ 0 _ = pure []
+  combinations0 n0 k0 es0 | n0 == k0 = pure es0
+  combinations0 n0 k0 (e : es0) =
+    Gen.frequency
+      [ (k0, (e :) <$> combinations0 (n0 - 1) (k0 - 1) es0)
+      , (n0 - k0, combinations0 (n0 - 1) k0 es0)
+      ]
+  combinations0 _ _ [] =
+    error $ "combinations: impossible, k > n. k = " <> show k <> ", n = " <> show n
