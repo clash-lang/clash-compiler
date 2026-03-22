e99f6bf809084c086a991243cb8933ffc7998035
rowanG077 <goemansrowan@gmail.com>
2026-03-22 21:49:16 +0100

    [skip ci] Conflict resolution for nix files

--- nix/overlay-ghc96.nix
+++ nix/overlay-ghc96.nix
@@ -8,47 +8,8 @@ in
   # is basically abandonware it catches fire with brick 1.0+.
   brick = doJailbreak(prev.callHackage "brick" "0.70.1" { });
 
-<<<<<<< HEAD
-  # brick 0.70.1 requires vty < 6.0.
-  vty = prev.callHackage "vty" "5.39" { };
-
-  # Marked as broken in nixpkgs, since it needs a newer hashable than the
-  # .cabal file currently uploaded to hackage.
-  concurrent-supply = doJailbreak (markUnbroken prev.concurrent-supply);
-
-  # Use a branch with changes to support GHC 9.6.1.
-  hint =
-    prev.hint.overrideAttrs (_: {
-      src =
-        pkgs.fetchFromGitHub {
-          owner = "haskell-hint";
-          repo = "hint";
-          rev = "7803c34c8ae1d83c0f7c13fe6b30fcb3abd0ac51";
-          hash = "sha256-ZFusrioxjDmWnDktD1evu7EjPG6brYpmmcaE2NWQKGA=";
-        };
-    });
-||||||| parent of 4e6fc5c5 (Update flake (#3112))
-  vty = prev.callHackage "vty" "5.39" { };
-
-  # Marked as broken in nixpkgs, since it needs on a newer hashable than the
-  # .cabal file currently uploaded to hackage.
-  concurrent-supply = doJailbreak (markUnbroken prev.concurrent-supply);
-
-  # Use a branch with changes to support GHC 9.6.1.
-  hint =
-    prev.hint.overrideAttrs (_: {
-      src =
-        pkgs.fetchFromGitHub {
-          owner = "haskell-hint";
-          repo = "hint";
-          rev = "7803c34c8ae1d83c0f7c13fe6b30fcb3abd0ac51";
-          hash = "sha256-ZFusrioxjDmWnDktD1evu7EjPG6brYpmmcaE2NWQKGA=";
-        };
-    });
-=======
   # brick 0.70.1 requires vty < 6.0.
   vty = doJailbreak (prev.callHackage "vty" "5.39" { });
->>>>>>> 4e6fc5c5 (Update flake (#3112))
 
   # Marked as broken in nixpkgs, since it specifies much older dependencies
   # than the defaults in nixpkgs.
--- nix/overlay-ghc98.nix
+++ nix/overlay-ghc98.nix
@@ -22,50 +22,8 @@ in
   # singletons-th 3.3 requires th-desugar 1.16
   th-desugar = prev.callHackage "th-desugar" "1.16" { };
 
-<<<<<<< HEAD
-  # We need a new tasty-flaky. The one from Hackage doesn't build for some weird
-  # reason..
-  tasty-flaky = prev.callCabal2nix "tasty-flaky" (pkgs.fetchFromGitHub {
-    owner = "LaurentRDC";
-    repo  = "tasty-flaky";
-    rev = "fc31a9d622c1eb60030a50152258a9bef785e365";
-    sha256 = "sha256-irLM3aVMxpBgsM72ArulMXcoLY2glalVkG//Lrj2JBI=";
-  }) {};
-
-  # This version of tasty isn't available in the nix ghc98 package set
-  tasty = prev.callHackageDirect {
-    pkg = "tasty";
-    ver = "1.5.3";
-    sha256 = "sha256-Ogd8J4aHNeL+xmcRWuJeGBNaePyLs5yo1IoMzvWrVPY=";
-  } {};
-
-  # The tests (not the package itself!) require a tasty <1.5, which won't work as we pull in
-  # tasty 1.5.3. Solution: don't test!
-  time-compat = dontCheck prev.time-compat;
-||||||| parent of 4e6fc5c5 (Update flake (#3112))
-  # We need a new tasty-flaky. The one from Hackage doesn't build for some weird
-  # reason..
-  tasty-flaky = prev.callCabal2nix "tasty-flaky" (pkgs.fetchFromGitHub {
-    owner = "LaurentRDC";
-    repo  = "tasty-flaky";
-    rev = "fc31a9d622c1eb60030a50152258a9bef785e365";
-    sha256 = "sha256-irLM3aVMxpBgsM72ArulMXcoLY2glalVkG//Lrj2JBI=";
-  }) {};
-
-  # This version of tasty isn't available in the nix ghc96 package set
-  tasty = prev.callHackageDirect {
-    pkg = "tasty";
-    ver = "1.5.3";
-    sha256 = "sha256-Ogd8J4aHNeL+xmcRWuJeGBNaePyLs5yo1IoMzvWrVPY=";
-  } {};
-
-  # The tests (not the package itself!) require a tasty <1.5, which won't work as we pull in
-  # tasty 1.5.3. Solution: don't test!
-  time-compat = dontCheck prev.time-compat;
-=======
   # Broken on GHC 9.8.4 see clash-ffi cabal file for details
   clash-ffi = overrideCabal prev.clash-ffi (drv: {
     broken = true;
   });
->>>>>>> 4e6fc5c5 (Update flake (#3112))
 }
