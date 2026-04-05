76b8a20aeff3d3561f5694bb9503a7ae6de04b98
rowanG077 <goemansrowan@gmail.com>
2026-04-05 20:26:37 +0200

    Add some simulation tests

--- tests/shouldwork/BitVector/MaybeUnpackUndefined.hs
+++ tests/shouldwork/BitVector/MaybeUnpackUndefined.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE TypeApplications #-}
+
 module MaybeUnpackUndefined where
+
 import Clash.Prelude
 import Clash.Sized.Internal.BitVector
 
-state = maybeUnpack undefined#    :: Vec 3 Bit
+state = maybeUnpack undefined# :: Maybe (Vec 3 Bit)
 topEntity a = register @System state a
