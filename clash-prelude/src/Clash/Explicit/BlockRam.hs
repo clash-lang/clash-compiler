clash-lang/clash-compiler

commit 54a7092ffa817585f7f43d014f2baf506addaed3

Merge pull request #3198 from clash-lang/lucas/ramop-derivations

clash-prelude/src/Clash/Explicit/BlockRam.hs
@@ -429,6 +429,7 @@ where
 
 import Clash.HaskellPrelude
 
+import Control.DeepSeq (NFData)
 import Control.Exception (catch, throw)
 import Control.Monad (forM_)
 import Control.Monad.ST (ST, runST)
@@ -460,7 +461,7 @@ import Clash.Sized.Index (Index)
 import Clash.Sized.Unsigned (Unsigned)
 import Clash.Sized.Vector (Vec, replicate, iterateI)
 import Clash.XException
- (maybeIsX, NFDataX(deepErrorX), defaultSeqX, fromJustX, undefined,
+ (ShowX, maybeIsX, NFDataX(deepErrorX), defaultSeqX, fromJustX, undefined,
  XException (..), seqX, errorX)
 import Clash.XException.MaybeX (MaybeX(..), andX)
 
@@ -1169,7 +1170,7 @@ data RamOp n a
   -- ^ Write data to address
   | RamNoOp
   -- ^ No operation
-  deriving (Generic, NFDataX, Show)
+  deriving (Eq, Generic, NFData, NFDataX, Show, ShowX)
 
 instance (AutoReg a, KnownNat n) => AutoReg (RamOp n a) where
   autoReg clk rst en initVal input =

commit 002e156d4a3b53ed94d38baa39dbc855dd212248

Add `BitPack RamOp`
Fixed fallout from #3198

clash-prelude/src/Clash/Explicit/BlockRam.hs
@@ -406,6 +406,8 @@ This concludes the short introduction to using 'blockRam'.
 -- as to why we need this.
 {-# OPTIONS_GHC -fno-cpr-anal #-}
 
+{-# OPTIONS_GHC -fconstraint-solver-iterations=12 #-}
+
 module Clash.Explicit.BlockRam
   ( -- * Block RAM synchronized to an arbitrary clock
     blockRam
@@ -448,7 +450,7 @@ import Unsafe.Coerce (unsafeCoerce)
 import Clash.Annotations.Primitive
   (Primitive(InlineYamlPrimitive), HDL(..), hasBlackBox)
 import Clash.Class.AutoReg (AutoReg(autoReg))
-import Clash.Class.BitPack (bitToBool, msb)
+import Clash.Class.BitPack (BitPack, bitToBool, msb)
 import Clash.Class.Num (SaturationMode(SatBound), satSucc)
 import Clash.Explicit.BlockRam.Model (TdpbramModelConfig(..), tdpbramModel)
 import Clash.Explicit.Signal (KnownDomain, Enable, register, fromEnable, andEnable)
@@ -1170,7 +1172,7 @@ data RamOp n a
   -- ^ Write data to address
   | RamNoOp
   -- ^ No operation
-  deriving (Eq, Generic, NFData, NFDataX, Show, ShowX)
+  deriving (Eq, Generic, NFData, NFDataX, Show, ShowX, BitPack)
 
 instance (AutoReg a, KnownNat n) => AutoReg (RamOp n a) where
   autoReg clk rst en initVal input =