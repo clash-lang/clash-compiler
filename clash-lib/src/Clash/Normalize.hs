3d8412322c133e38d6842b7e9e16afdc6f21a042
Martijn Bastiaan <martijn@qbaylogic.com>
2026-04-25 20:11:29 +0200

    Run `bindConstantVar` after `flattenLet` in `flatten`
    Fixes #3041

--- clash-lib/src/Clash/Normalize.hs
+++ clash-lib/src/Clash/Normalize.hs
@@ -402,6 +402,7 @@ flattenCallTree (CBranch (nm,(Binding nm' sp inl pr tm r)) used) = do
       topdownSucR (apply "inlineCleanup" inlineCleanup) >->
       bottomupR (apply "caseCon" caseCon) >-> -- https://github.com/clash-lang/clash-compiler/issues/3159 / #3204
       bottomupR (apply "flattenLet" flattenLet) >-> -- https://github.com/clash-lang/clash-compiler/issues/3185
+      bottomupR (apply "bindConstantVar" bindConstantVar) >-> -- https://github.com/clash-lang/clash-compiler/issues/3041
       topdownSucR (apply "topLet" topLet)
 
     goCheap c@(CLeaf   (nm2,(Binding _ _ inl2 _ e _)))
