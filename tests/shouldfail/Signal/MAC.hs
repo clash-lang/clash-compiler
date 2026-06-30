47b51dc03f99855b8abf636f61f2169c9271f1a5
Leon Schoorl <leon@qbaylogic.com>
2026-06-30 15:26:24 +0200

    fixup! Remove bignums from tests where possible

--- tests/shouldfail/Signal/MAC.hs
+++ tests/shouldfail/Signal/MAC.hs
@@ -3,12 +3,12 @@ module MAC where
 import Clash.Prelude
 
 topEntity
-  :: Integer
+  :: Int
   -> Clock System
   -> Reset System
   -> Enable System
-  -> (Signal System Integer, Signal System Integer)
-  -> Signal System Integer
+  -> (Signal System Int, Signal System Int)
+  -> Signal System Int
 topEntity i = exposeClockResetEnable (macT <^> i)
 
 macT s (x,y) = (s',o)
