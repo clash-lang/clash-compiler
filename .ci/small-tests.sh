29dc2bf089513299379577a1dbd21cdf38cd98ad
jaschutte <34577095+jaschutte@users.noreply.github.com>
2026-08-12 15:57:52 +0200

    Rework CI core count and setup
    Reworks how the CI retrieves the amount of cores available on the
    system, as well as define a few setup and small-test scripts

---
+++ .ci/small-tests.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -xou pipefail
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+CI_DIR=$GIT_ROOT/.ci
+
+if [ ! -f $GIT_ROOT/cabal.project.local ]; then
+    echo "Requires gh-setup.sh to be invoked first, refusing to do that manually as to not create unintended effects"
+    exit 1;
+fi
+
+THREADS=$($CI_DIR/effective_cpus.sh)
+
+# Check for EOL whitespace
+grep -E ' $' -n -r . --include=*.{hs,hs-boot,sh} --exclude-dir=dist-newstyle
+if [[ $? == 0 ]]; then
+    echo "EOL whitespace detected. See ^"
+    exit 1;
+fi
+
+# Check whether version numbers in
+# clash-{prelude{,-hedgehog},lib{,-hedgehog},ghc} are the same
+cabal_files="$GIT_ROOT/clash-prelude/clash-prelude.cabal $GIT_ROOT/clash-prelude-hedgehog/clash-prelude-hedgehog.cabal $GIT_ROOT/clash-lib/clash-lib.cabal $GIT_ROOT/clash-lib-hedgehog/clash-lib-hedgehog.cabal $GIT_ROOT/clash-ghc/clash-ghc.cabal"
+versions=$(grep "^[vV]ersion" $cabal_files | grep -Eo '[0-9]+(\.[0-9]+)+')
+
+if [[ $(echo $versions | tr ' ' '\n' | wc -l) == 5 ]]; then
+    if [[ $(echo $versions | tr ' ' '\n' | uniq | wc -l) != 1 ]]; then
+        echo "Expected all distributions to have the same version number. Found: $versions"
+        exit 1;
+    fi
+else
+    echo "Expected to find version number in all distributions. Found: $versions";
+    exit 1;
+fi
+
+#Test if cabal can generate a build plan using the index state as specified in cabal.project
+# We should not have cabal.project.local in place for this
+rm $GIT_ROOT/cabal.project.local
+cabal v2-build -j$THREADS --dry-run all > /dev/null || (echo "Maybe state index should be updated?"; false)
+cp $CI_DIR/cabal.project.local $GIT_ROOT
