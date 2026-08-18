9bacb9cf11918c747e7748659e7a4dba43664a4e
Peter Lebbing <peter@digitalbrains.com>
2026-08-18 18:23:13 +0200

    Catch errors in CI scripts

--- .ci/small-tests.sh
+++ .ci/small-tests.sh
@@ -2,6 +2,7 @@
 set -xou pipefail
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
+[[ $? -ne 0 ]] && exit 1
 CI_DIR=$GIT_ROOT/.ci
 
 if [ ! -f $GIT_ROOT/cabal.project.local ]; then
@@ -10,6 +11,7 @@ if [ ! -f $GIT_ROOT/cabal.project.local ]; then
 fi
 
 THREADS=$($CI_DIR/effective_cpus.sh)
+[[ $? -ne 0 ]] && exit 1
 
 # Check for EOL whitespace
 grep -E ' $' -n -r . --include=*.{hs,hs-boot,sh} --exclude-dir=dist-newstyle
@@ -22,6 +24,7 @@ fi
 # clash-{prelude{,-hedgehog},lib{,-hedgehog},ghc} are the same
 cabal_files="$GIT_ROOT/clash-prelude/clash-prelude.cabal $GIT_ROOT/clash-prelude-hedgehog/clash-prelude-hedgehog.cabal $GIT_ROOT/clash-lib/clash-lib.cabal $GIT_ROOT/clash-lib-hedgehog/clash-lib-hedgehog.cabal $GIT_ROOT/clash-ghc/clash-ghc.cabal"
 versions=$(grep "^[vV]ersion" $cabal_files | grep -Eo '[0-9]+(\.[0-9]+)+')
+[[ $? -ne 0 ]] && exit 1
 
 if [[ $(echo $versions | tr ' ' '\n' | wc -l) == 5 ]]; then
     if [[ $(echo $versions | tr ' ' '\n' | uniq | wc -l) != 1 ]]; then
@@ -35,6 +38,6 @@ fi
 
 #Test if cabal can generate a build plan using the index state as specified in cabal.project
 # We should not have cabal.project.local in place for this
-mv $GIT_ROOT/cabal.project.local $GIT_ROOT/cabal.project.local.disabled
+mv $GIT_ROOT/cabal.project.local $GIT_ROOT/cabal.project.local.disabled || exit $?
 cabal v2-build -j$THREADS --dry-run all > /dev/null || { echo "Maybe state index should be updated?"; exit 1; }
-mv $GIT_ROOT/cabal.project.local.disabled $GIT_ROOT/cabal.project.local
+mv $GIT_ROOT/cabal.project.local.disabled $GIT_ROOT/cabal.project.local || exit $?
