a1f5d6c33e5a33910c2511d70f850cc1633c6ca4
Rowan Goemans <goemansrowan@gmail.com>
2026-09-07 19:41:28 +0200

    Add UnaryClass testcase
    (cherry picked from commit 44094f327e85d5131ccb5d7e2cff605fd130d004)

--- tests/Main.hs
+++ tests/Main.hs
@@ -815,6 +815,7 @@ runClashTest = defaultMain
         , runTest "FunctionInstances" def
         , runTest "GADTExistential" def{hdlSim=[]}
         , runTest "LocalPoly" def{hdlSim=[]}
+        , runTest "UnaryClass" def{hdlSim=[]}
         ]
       , clashTestGroup "PrimitiveGuards"
         [ runTest "WarnAlways" def{
