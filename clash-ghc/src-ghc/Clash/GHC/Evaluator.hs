94a1d53bb4a6e06bd142a40552eeadb2cf53e894
Rowan Goemans <goemansrowan@gmail.com>
2026-09-25 01:30:53 +0200

    Handle unsaturated data constructors in evaluator

--- clash-ghc/src-ghc/Clash/GHC/Evaluator.hs
+++ clash-ghc/src-ghc/Clash/GHC/Evaluator.hs
@@ -1,6 +1,6 @@
 {-|
   Copyright   :  (C) 2017-2022, Google Inc.,
-                     2021-2024, QBayLogic B.V.
+                     2021-2026, QBayLogic B.V.
   License     :  BSD2 (see the file LICENSE)
   Maintainer  :  QBayLogic B.V. <devops@qbaylogic.com>
 
@@ -112,7 +112,10 @@ stepVar i m _
   toStr = Text.unpack . unQualName . flip Text.snoc '_' . nameOcc . varName
 
 stepData :: DataCon -> Step
-stepData dc = ghcUnwind (DC dc [])
+stepData dc m tcm =
+  case fst $ splitFunForallTy (dcType dc) of
+    [] -> ghcUnwind (DC dc []) m tcm
+    tys -> newBinder tys (Data dc) m tcm
 
 stepLiteral :: Literal -> Step
 stepLiteral l = ghcUnwind (Lit l)
